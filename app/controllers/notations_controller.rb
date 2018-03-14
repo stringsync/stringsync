5e87d73d48ac69b640d34a6acf2a1b23cfaf9788
Jared Johnson <jared@nuts.com>
2018-03-13 22:21:48 -0400

    Add notations controller and route.

--- app/controllers/notations_controller.rb
+++ app/controllers/notations_controller.rb
@@ -1,2 +1,6 @@
 class NotationsController < ApplicationController::API
+  def index
+    @notations = Notation.all
+    render(json: @notations.to_json, status: 200)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  scope '/api' do
+    resources :notations
+  end
+
+  # invokes fallback to hand off route handling to the client
   get "*path", to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
