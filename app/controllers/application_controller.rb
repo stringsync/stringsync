11c74fbc9333cda65e56343fe0f79278d5519148
Jared Johnson <jared@nuts.com>
2018-03-13 20:56:35 -0400

    Add fallback to the api.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def fallback_index_html
+    render file: "public/index.html"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "*path", to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
