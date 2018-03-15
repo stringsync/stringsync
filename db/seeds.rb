f3787c4dc8465c30a859e3679bf2aea1f9e3e55d
Jared Johnson <jared@nuts.com>
2018-03-15 17:47:25 -0400

    Do not run seeds.rb unless in the development environment.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+return unless Rails.env.development?
+
 require File.join(Rails.root, "lib", "fixture_loader", "fixture_loader.rb")
 
-FixtureLoader.new.seed! if Rails.env.development?
+ApplicationRecord.descendants.each(&:delete_all)
+FixtureLoader.new.seed!
