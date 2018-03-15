e5f189b1345b50c8850ee0e15f858568654bc07c
Jared Johnson <jared@nuts.com>
2018-03-14 20:38:44 -0400

    Add validations to the notation model.

--- app/models/notation.rb
+++ app/models/notation.rb
@@ -1,3 +1,9 @@
 class Notation < ApplicationRecord
   has_attached_file(:thumbnail)
+
+  validates_attachment(:thumbnail,
+    presence: true,
+    content_type: { content_type: "image/jpeg" },
+    size: { in: 0..2.megabytes }
+  )
 end
