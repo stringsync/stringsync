76f3e28f6b716a2d2722f95c79dd56d3d2865e3b
Jared Johnson <jared@nuts.com>
2018-03-18 15:32:48 -0400

    Restructure data dir.

--- client/src/data/api/index.js
+++
@@ -1,2 +0,0 @@
-export * from './notations';
-export * from './tags';
--- client/src/data/index.js
+++ client/src/data/index.js
@@ -1,5 +1,5 @@
-export * from './api';
-export * from './ui';
+export * from './notations';
+export * from './tags';
 export { default as middleware } from './middleware';
 export { default as reducer } from './reducer';
 export { default as store } from './store';
