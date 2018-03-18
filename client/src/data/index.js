f78c4b419395bbec2b2d8ddcfabf8901b29fec9d
Jared Johnson <jared@nuts.com>
2018-03-18 13:56:16 -0400

    Flesh out data index.

---
+++ client/src/data/index.js
@@ -0,0 +1,5 @@
+export * from './api';
+export * from './ui';
+export { default as middleware } from './middleware';
+export { default as reducer } from './reducer';
+export { default as store } from './store';
