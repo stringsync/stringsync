6acf25e7e2aac5b10d11e1acb43b9db0c55be15c
Jared Johnson <jared@nuts.com>
2018-03-17 22:56:52 -0400

    Simplify app.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import { compose, withHandlers } from 'recompose';
 
-const enhance = compose(
-  withHandlers({
-    handleButtonClick: props => event => {
-      window.auth.oAuthSignIn({ provider: 'facebook' })
-    }
-  })
-)
-
-const App = enhance(props => (
+const App = props => (
   <div className="App">
-    {JSON.stringify(window.auth.user)}
-    <button onClick={props.handleButtonClick}>Login</button>
+    App
   </div>
-));
+);
 
 export default App;
