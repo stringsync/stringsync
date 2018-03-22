346a0940ba761d745032b9605b7962ec288c1ed1
Jared Johnson <jared@nuts.com>
2018-03-22 08:15:39 -0400

    Add react-router-dom to the project.

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { withRouter, BrowserRouter } from 'react-router-dom';
 
 const scrollToTop = () => window.scrollTo(null, 0);
 
 const App = props => (
-  <div className="App">
-    App
-  </div>
+  <Provider store={props.store}>
+    <BrowserRouter>
+      <div className="App">
+        App
+      </div>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
