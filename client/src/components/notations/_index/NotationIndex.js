cffbe2d666d53b51a361fb5b290b09cbe652120e
Jared Johnson <jared@nuts.com>
2018-03-25 11:34:31 -0400

    Filter index notations by query.

--- client/src/components/notations/_index/NotationIndex.js
+++ client/src/components/notations/_index/NotationIndex.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { compose, withProps, lifecycle } from 'recompose';
+import { compose, withProps, withState, lifecycle } from 'recompose';
 import { notationsActions } from 'data';
 import { indexIncludedObjects, camelCaseKeys } from 'utilities';
 import { NotationGrid } from './';
@@ -15,6 +15,22 @@ const enhance = compose(
       setNotations: notations => dispatch(notationsActions.notations.index.set(notations))
     })
   ),
+  withState('query', 'setQuery', ''),
+  withProps(props => {
+    const query = props.query.toUpperCase();
+    const queriedNotations = props.notations.filter(({ attributes, relationships }) => (
+      [
+        attributes.artistName.toUpperCase(),
+        attributes.songName.toUpperCase(),
+        relationships.transcriber.attributes.name.toUpperCase(),
+        ...relationships.tags.map(tag => tag.attributes.name.toUpperCase())
+      ].some(tag => tag.includes(query))
+    ));
+
+    return {
+      queriedNotations
+    }
+  }),
   withProps(props => ({
     /**
      * Transforms the data from the notations index endpoint into notation objects
@@ -59,9 +75,7 @@ const enhance = compose(
 
 const NotationIndex = enhance(props => (
   <div>
-    <NotationGrid
-      notations={props.notations}
-    />
+    <NotationGrid notations={props.queriedNotations} />
   </div>
 ));
 
