0df0810ac702afc5303f6416b70a74b7b4c85bc2
Jared Johnson <jared@nuts.com>
2018-03-26 19:27:15 -0400

    Create NotFound route and component.

---
+++ client/src/components/not-found/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    Not Found
+  </div>
+);
+
+export default NotFound;
---
+++ client/src/components/not-found/NotFound.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import NotFound from './';
+import { assertRender } from 'test';
+
+assertRender(NotFound);
