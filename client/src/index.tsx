24a004d73cfbe5a79f1b88c1d63e448d7ad6d2ff
Jared Johnson <jared@nuts.com>
2018-03-28 22:22:35 -0400

    Resolve root correctly in client.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import { Root } from 'root';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Root />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
---
+++ client/src/root/Root.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const Root: React.SFC = props => (
+  <div>
+    Alright, I'll take your word for it.
+  </div>
+);
+
+export default Root;
