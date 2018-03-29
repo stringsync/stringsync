44e644bcb3583945b4d92958bde82d137279bd07
Jared Johnson <jared@nuts.com>
2018-03-28 22:56:33 -0400

    Continue to migrate back to typescript.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Root } from 'root';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import { registerServiceWorker } from 'utilities';
 
-ReactDOM.render(
-  <Root />,
-  document.getElementById('root') as HTMLElement
-);
+declare global {
+  interface Window {
+    ss: {
+      env: string,
+      notification: void,
+      message: void,
+    };
+  }
+}
+
+ReactDOM.render(<Root />, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
