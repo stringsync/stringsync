5bd549dab97be96959fc2e27921b652fa0823279
Jared Johnson <jared@nuts.com>
2018-04-03 19:42:26 -0400

    Create the ViewportSync component.

--- client/src/root/ViewportSync.tsx
+++ client/src/root/ViewportSync.tsx
@@ -1,7 +1,30 @@
 import * as React from 'react';
+import { compose, createSink } from 'recompose';
+import { connect } from 'react-redux';
+import withSizes from 'react-sizes';
 
-const ViewportSync = () => (
-  <div>ViewportSync</div>
+interface ViewportSyncProps {
+  queriedWidth: number;
+  storedWidth: number;
+  setViewportWidth: (width: number) => void;
+}
+
+interface Size {
+  width: number;
+  height: number;
+}
+
+const enhance = compose(
+  withSizes((size: Size) => ({
+    queriedWidth: size.width
+  })),
+  createSink((props: ViewportSyncProps) => {
+    if (props.storedWidth !== props.queriedWidth) {
+      props.setViewportWidth(props.queriedWidth);
+    }
+  })
 );
+
+const ViewportSync = enhance(() => null);
 
 export default ViewportSync;
