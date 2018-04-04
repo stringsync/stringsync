9be819c9e6d1fab3f99380c88a31ee6107acef8d
Jared Johnson <jared@nuts.com>
2018-04-04 08:25:14 -0400

    Reverting back to javascript.

--- client/src/data/notations/notationsActions.ts
+++ client/src/data/notations/notationsActions.ts
@@ -1,6 +1,7 @@
 import { createActions } from 'redux-actions';
 
 export interface NotationsPayload {
+  readonly fetchedAt: number;
   readonly notations: Notation[];
 }
 
@@ -8,8 +9,10 @@ export interface NotationPayload {
   readonly notation: Notation;
 }
 
-export const notationActions = createActions<NotationsPayload | NotationPayload>({
-  SET_NOTATIONS_INDEX: (notations: Notation[]): NotationsPayload => Object.freeze({ notations }),
+export const notationsActions = createActions<NotationsPayload | NotationPayload>({
+  SET_NOTATIONS_INDEX: (notations: Notation[], fetchedAt: number): NotationsPayload => (
+    Object.freeze({ notations, fetchedAt })
+  ),
   SET_NOTATIONS_SHOW: (notation: Notation): NotationPayload => Object.freeze({ notation }),
   SET_NOTATIONS_EDIT: (notation: Notation): NotationPayload => Object.freeze({ notation }),
   UPDATE_NOTATIONS_EDIT: (notation: Notation): NotationPayload => Object.freeze({ notation })
