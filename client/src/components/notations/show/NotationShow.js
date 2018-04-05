2395452db179bdbbc20c3f489f3096940feee17c
Jared Johnson <jared@nuts.com>
2018-04-05 07:51:07 -0400

    Make skeleton for the NotationShow component.

--- client/src/components/notations/show/NotationShow.js
+++ client/src/components/notations/show/NotationShow.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Affix, Button } from 'antd';
+import styled from 'react-emotion';
+
+const Outer = styled('div')`
+  display: flex;
+  flex-flow: column;
+  overflow: hidden;
+  width: 100%;
+`;
+
+const Top = styled('div')`
+`;
+
+const Middle = styled('div')`
+`;
+
+const Bottom = styled('div')`
+`;
+
 
 const NotationShow = props => (
-  <div>
-    NotationShow
-  </div>
+  <Outer id="notation-show">
+    <Top>
+      NotationShowVideo
+      <Affix
+        target={() => document.getElementById('notation-show')}
+        offsetTop={2}
+        onChange={}
+      >
+        Fretboard
+        Piano
+      </Affix>
+    </Top>
+    <Middle>
+      ScrollElement
+      Score
+    </Middle>
+    <Bottom>
+      NotationControls
+    </Bottom>
+  </Outer>
 );
 
 export default NotationShow;
