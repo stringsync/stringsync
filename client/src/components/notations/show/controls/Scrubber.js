0485e71a08b46da825048cb1d7e68d924a3aa4b1
Jared Johnson <jared@nuts.com>
2018-04-24 20:28:50 -0400

    Make the scrubber move with the timeKeeper.currentTimeMs.

--- client/src/components/notations/show/controls/Scrubber.js
+++ client/src/components/notations/show/controls/Scrubber.js
@@ -1,27 +1,48 @@
 import React from 'react';
-import { compose, withHandlers } from 'recompose';
+import { compose, withHandlers, withProps, withState } from 'recompose';
 import { Slider } from 'antd';
 import { connect } from 'react-redux';
+import { withRaf } from 'enhancers';
+import { RafSpec } from 'services';
 
 const enhance = compose(
   connect(
     state => ({
       videoPlayer: state.video.player,
-      isVideoActive: state.video.isActive
+      isVideoActive: state.video.isActive,
+      durationMs: state.notations.show.attributes.durationMs
     })
   ),
+  withState('value', 'setValue', 0),
   withHandlers({
     handleChange: props => value => {
+      props.setValue(value);
     },
     handleAfterChange: props => value => {
+      props.setValue(value);
     }
-  })
+  }),
+  withHandlers({
+    handleRafLoop: props => () => {
+      const value = 100 * window.ss.maestro.timeKeeper.currentTimeMs / props.durationMs;
+
+      // Guard against NaN
+      if (value === value) {
+        props.setValue(value);
+      }
+    }
+  }),
+  withRaf(
+    () => window.ss.rafLoop,
+    props => new RafSpec('Scrubber.handleRafLoop', 1, props.handleRafLoop)
+  )
 );
 
 const Scrubber = enhance(props => (
   <Slider
     onChange={props.handleChange}
     onAfterChange={props.handleAfterChange}
+    value={props.value}
     step={0.01}
     style={{ margin: '0 4px 0 4px' }}
   />
