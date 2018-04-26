73326afd8b4b3bf2715bf5af9cbbb5fc9e932f2b
Jared Johnson <jared@nuts.com>
2018-04-26 14:08:54 -0400

    Compute numFrets in Fretboard and pass to Frets components.

--- client/src/components/fretboard/Fretboard.js
+++ client/src/components/fretboard/Fretboard.js
@@ -3,26 +3,57 @@ import { Overlap, Layer } from 'components';
 import { Frets, GuitarStrings } from './';
 import styled from 'react-emotion';
 import woodTextureSrc from 'assets/wood-texture.jpg';
+import { compose, withProps } from 'recompose';
+import { connect } from 'react-redux';
+
+const enhance = compose(
+  connect(
+    state => ({
+      viewportType: state.viewport.type
+    })
+  ),
+  withProps(props => {
+    let numFrets;
+
+    switch(props.viewportType) {
+      case 'MOBILE':
+        numFrets = 15;
+        break;
+      case 'TABLET':
+        numFrets = 17;
+        break;
+      case 'DESKTOP':
+        numFrets = 19;
+        break;
+      default:
+        numFrets = 19;
+        break;
+    };
+
+    return { numFrets };
+  })
+);
 
 const Outer = styled('div')`
   background: url(${props => woodTextureSrc});
   background-color: black;
+  width: 100%;
 `;
 
 /**
  * Sets up the layout for the Frets and GuitarStrings components
  */
-const Fretboard = () => (
+const Fretboard = enhance(props => (
   <Outer>
     <Overlap>
       <Layer>
-        <Frets />
+        <Frets numFrets={props.numFrets} />
       </Layer>
       <Layer>
         <GuitarStrings />
       </Layer>
     </Overlap>
   </Outer>
-);
+));
 
 export default Fretboard;
--- client/src/components/fretboard/frets/Frets.js
+++ client/src/components/fretboard/frets/Frets.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { compose, setPropTypes } from 'recompose';
+import PropTypes from 'prop-types';
 
-const Frets = () => (
+const enhance = compose(
+  setPropTypes({
+    numFrets: PropTypes.number
+  })
+);
+
+const Frets = enhance(props => (
   <div>
     Frets
   </div>
-);
+));
 
 export default Frets;
