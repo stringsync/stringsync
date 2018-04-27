0e6372e14489b1628a37b2afbf9fdf2234d21e91
Jared Johnson <jared@nuts.com>
2018-04-27 13:26:47 -0400

    Simplify Scale.test.

--- client/src/services/music/scale/Scale.test.js
+++ client/src/services/music/scale/Scale.test.js
@@ -1,13 +1,12 @@
 import { Note } from 'services';
 import { Scale, ScaleDegree, scales } from './';
 
-const CHROMATIC_DEGREES = Object.freeze(['1', 'b2', '2', 'b3', '3', '4', 'b5', '5', 'b6', '6', 'b7', '7']);
 const KEYS = Note.ALL_LITERALS;
 
 test('Scale factory', () => {
   KEYS.forEach(key => {
-    Object.keys(scales).forEach(scale => {
-      Scale.for(key, scale);
+    Object.keys(scales).forEach(scaleName => {
+      Scale.for(key, scaleName);
     });
   });
 });
