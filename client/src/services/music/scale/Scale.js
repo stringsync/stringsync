e7c5d479477a04d90a4b2499348c035441b041fc
Jared Johnson <jared@nuts.com>
2018-04-30 19:28:39 -0400

    Test Scale.prototype.notes accepts an array of octaves.

--- client/src/services/music/scale/Scale.js
+++ client/src/services/music/scale/Scale.js
@@ -1,5 +1,6 @@
 import { Note } from 'services';
 import { ScaleDegree, scales } from './';
+import { flatMap, uniqWith } from 'lodash';
 
 /**
  * The purpose of this class is to transform a key and an array of ScaleDegree instances
@@ -39,28 +40,26 @@ class Scale {
   /**
    * The primary purpose of the Scale service.
    * Returns an array of note objects that correspond to the key, degrees, and octaves.
-   * The notes will be in the same order as the degrees.
+   * The notes will be in the sort order dictated by Note.sort.
    * 
    * @param {Array<number>}
    * @return {Note[]}
    */
   notes(octaves = [4]) {
     const root = new ScaleDegree('1', this);
-    const notes = [];
 
-    // Used for the break condition in the while loop to construct the return value.
-    const targetOctaves = octaves.sort();
+    const notes = flatMap(this.degrees, degree => (
+      octaves.map(octave => {
+        // The seed is the note used to start the scale for a given octave
+        const seed = this.key.clone;
+        seed.octave = octave;
+        return seed.step(root.distance(degree));
+      })
+    ));
 
-    while (targetOctaves.length > 0) {
-      const octave = targetOctaves.shift();
-      
-    }
-
-    return this.degrees.map(degree => {
-      const numHalfSteps = root.distance(degree);
-      return this.key.step(numHalfSteps);
-    });
+    return Note.sort(notes);
   }
 }
 
 export default Scale;
+window.Scale = Scale;
