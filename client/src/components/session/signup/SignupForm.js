8a39ef07a7b56fd658871562e854984c1e018b36
Jared Johnson <jared@nuts.com>
2018-05-06 15:07:56 -0400

    Add session errors to the SignupForm component.

--- client/src/components/session/signup/SignupForm.js
+++ client/src/components/session/signup/SignupForm.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'react-emotion';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
-import { compose, setDisplayName } from 'recompose';
+import { compose, setDisplayName, withState } from 'recompose';
 import { Link } from 'react-router-dom';
+import { SessionErrors } from '../';
 
 const enhance = compose(
   Form.create(),
-  setDisplayName('SignupForm')
+  withState('loading', 'setLoading', false),
+  withState('errors', 'setErrors', [])
 );
 
 const SignupButton = styled(Button) `
@@ -14,48 +16,51 @@ const SignupButton = styled(Button) `
 `;
 
 const SignupForm = enhance(props => (
-  <Form onSubmit={this.handleSubmit}>
-    <Form.Item>
-      {props.form.getFieldDecorator('username', {
-        rules: [{ required: true, message: 'username cannot be blank' }],
-      })(
-        <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="username" />
-      )}
-    </Form.Item>
-    <Form.Item>
-      {props.form.getFieldDecorator('email', {
-        rules: [{ required: true, message: 'email cannot be blank' }],
-      })(
-        <Input prefix={<Icon type="inbox" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="email" />
-      )}
-    </Form.Item>
-    <Form.Item>
-      {props.form.getFieldDecorator('password', {
-        rules: [{ required: true, message: 'password cannot be blank' }],
-      })(
-        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="password" />
-      )}
-    </Form.Item>
-    <Form.Item>
-      {props.form.getFieldDecorator('passwordConfirmation', {
-        rules: [{ required: true, message: 'password cannot be blank' }],
-      })(
-        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="confirm password" />
-      )}
-    </Form.Item>
-    <Form.Item>
-      {props.form.getFieldDecorator('remember', {
-        valuePropName: 'checked',
-        initialValue: true,
-      })(
-        <Checkbox>remember me</Checkbox>
-      )}
-      <SignupButton type="primary" htmlType="submit">
-        signup
-      </SignupButton>
-      or <Link to="/login">login</Link>
-    </Form.Item>
-  </Form>
+  <div>
+    <Form onSubmit={this.handleSubmit}>
+      <Form.Item>
+        {props.form.getFieldDecorator('username', {
+          rules: [{ required: true, message: 'username cannot be blank' }],
+        })(
+          <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="username" />
+        )}
+      </Form.Item>
+      <Form.Item>
+        {props.form.getFieldDecorator('email', {
+          rules: [{ required: true, message: 'email cannot be blank' }],
+        })(
+          <Input prefix={<Icon type="inbox" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="email" />
+        )}
+      </Form.Item>
+      <Form.Item>
+        {props.form.getFieldDecorator('password', {
+          rules: [{ required: true, message: 'password cannot be blank' }],
+        })(
+          <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="password" />
+        )}
+      </Form.Item>
+      <Form.Item>
+        {props.form.getFieldDecorator('passwordConfirmation', {
+          rules: [{ required: true, message: 'password cannot be blank' }],
+        })(
+          <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="confirm password" />
+        )}
+      </Form.Item>
+      <Form.Item>
+        {props.form.getFieldDecorator('remember', {
+          valuePropName: 'checked',
+          initialValue: true,
+        })(
+          <Checkbox>remember me</Checkbox>
+        )}
+        <SignupButton type="primary" htmlType="submit">
+          signup
+        </SignupButton>
+        or <Link to="/login">login</Link>
+      </Form.Item>
+    </Form>
+    <SessionErrors errors={props.errors} />
+  </div>
 ));
 
 export default SignupForm;
