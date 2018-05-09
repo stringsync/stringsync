808c77e4aa52aab0455cb0958ee99d8ab78e1949
Jared Johnson <jared@nuts.com>
2018-05-08 20:02:43 -0400

    Document AuthRoute.

--- client/src/root/AuthRoute.js
+++ client/src/root/AuthRoute.js
@@ -11,15 +11,21 @@ const enhance = compose(
   )
 );
 
+/**
+ * This component is used to conditionally allow the mounting of a component solely
+ * based on state.session.signedIn.
+ * Specifying requireSignIn as true will require the user to be signed in to mount the component.
+ * Specifying requireSignIn as false will require the user NOT to be signed in to mount the component.
+ */
 const AuthRoute = enhance(({ component: Component, requireSignIn, isSignedIn, ...restProps }) => (
   <Route
     {...restProps}
     render={
       props => {
         if (requireSignIn) {
-          return isSignedIn ? <Component {...props} /> : <Redirect to="/login" />
+          return isSignedIn ? <Component {...props} /> : <Redirect to="/login" />;
         } else {
-          return isSignedIn ? <Redirect to="/" /> : <Component {...props} />
+          return isSignedIn ? <Redirect to="/" /> : <Component {...props} />;
         }
       }
     }
