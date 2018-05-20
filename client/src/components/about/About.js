10022f57b6adfc4964c0483f689f9e76ffaef1f9
Jared Johnson <jared@nuts.com>
2018-05-20 09:24:58 -0400

    Start working on the about page.

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -1,66 +1,49 @@
 import React from 'react';
 import styled from 'react-emotion';
-import guitarSrc from 'assets/guitar-2048x1152.jpg';;
+import guitarSrc from 'assets/guitar-2048x1152.jpg';
+import sonarGuitarSrc from 'assets/sonar-guitar.svg';
+import { Icon, Row, Col } from 'antd';
+import { connect } from 'react-redux';
+import { compose, } from 'recompose';
 
-const Outer = styled('div') `
-  max-width: 100%;
-  overflow-x: hidden;
-  background: white;
-`;
-
-const Inner = styled('div')`
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 12px;
-`;
-
-const MainImgContainer = styled('div')`
-  max-height: 600px;
-  overflow: hidden;
-`;
-
-const ResponsiveImg = styled('img')`
-  display: block;
-  width: 100%;
-  max-width: 100%;
-  height: auto;
-`;
-
-const Logo = styled('h1')`
-  font-size: 48px;
-  font-weight: 200;
-  letter-spacing: 6px;
-  margin-bottom: 0;
-  color: ${props => props.theme.primaryColor};
-  margin-bottom: 0;
-`;
-
-const TitleRow = styled('div')`
-  margin-top: 24px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const Title = styled('h1') `
-  font-size: 36px;
-  font-weight: 200;
-  margin-bottom: 0;
-`;
-
-const About = () => (
-  <Outer>
-    <MainImgContainer>
-      <ResponsiveImg src={guitarSrc} alt="guitar" />
-    </MainImgContainer>
-    <Inner>
-      <TitleRow>
-        <Logo className="main-title">StringSync</Logo>
-        <Title>&nbsp;learn guitar quickly</Title>
-      </TitleRow>
-    </Inner>
-  </Outer>
+const enhance = compose(
+  connect(
+    state => ({
+      viewportType: state.viewport.type
+    })
+  )
 );
+
+const Outer = styled('div')`
+`;
+
+const Banner = styled(Row)`
+  background: ${props => props.theme.primaryColor};
+  min-height: ${props => props.height}px;
+  width: 100%;
+`;
+
+const MainWhiteText = styled('h1')`
+  font-size: 1em;
+  width: 50%;
+  color: ${props => props.theme.tertiaryColor};
+  text-align: center;
+`;
+
+const About = enhance(() => (
+  <Outer>
+    <Banner
+      type="flex"
+      align="middle"
+      justify="center"
+      height={256}
+    >
+      <MainWhiteText>
+        We created StringSync to help you learn guitar differently
+      </MainWhiteText>
+    </Banner>
+    
+  </Outer>
+));
 
 export default About;
