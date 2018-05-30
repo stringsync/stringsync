2af5210eb2f8c6df0c3bca6e94e2ddea990fba71
Jared Johnson <jared@nuts.com>
2018-05-30 08:30:44 -0400

    Start initial work on VextabEncoder.

--- client/src/services/vextab/encoding/VextabEncoder.js
+++ client/src/services/vextab/encoding/VextabEncoder.js
@@ -1,3 +1,11 @@
+class VextabEncodingError extends Error {}
+
+const error = struct => {
+  new VextabEncodingError(`could not handle struct: ${JSON.stringify(struct)}`)
+}
+
+// Performs the reverse operation of the VextabDecoder. That is, from an array of VextabStructs,
+// encode them into a 
 class VextabEncoder {
   static encode(vextabStructs) {
     return new VextabEncoder(vextabStructs).toString();
@@ -14,8 +22,63 @@ class VextabEncoder {
    */
   toString() {
     this.structs.map(struct => {
-      console.log(struct);
+      switch(struct.element) {
+        case 'tabstave':
+          return this._encodeTabstave(struct);
+        default:
+          throw error(struct);
+      }
     });
+  }
+
+  /**
+   * Encodes a tabstave. A tabstave is identified by a vextabStruct having a property called
+   * element, and that property value is equal to 'tabstave'.
+   * 
+   * @param {VextabStruct} tabstave
+   * @return {string[]}
+   * @private
+   */
+  _encodeTabstave(tabstave) {
+    return [
+      'tabstave',
+      this._encodeOptions(tabstave.options),
+      this._encodeNotes(tabstave.notes),
+      this._encodeText(tabstave.text)
+    ]
+  }
+
+  /**
+   * Encodes a tabstave's options. Options are identified by being the options key-value pair
+   * in a tabstave struct. See VextabEncoder.prototype._encodeTabstave.
+   * 
+   * @param {VextabStruct} options 
+   * @return {string[]}
+   * @private
+   */
+  _encodeOptions(options) {
+
+  }
+
+  /**
+   * Encodes a tabstave's notes. Notes are identified by being the notes key-value pair in a
+   * tabstave struct. See VextabEncoder.prototype._encodeTabstave.
+   * 
+   * @param {VextabStruct} notes 
+   * @return {string[]}
+   * @private
+   */
+  _encodeNotes(notes) {
+
+  }
+
+  /**
+   * 
+   * @param {VextabStruct} text
+   * @return {string[]}
+   */
+  _encodeText(text) {
+
   }
 }
 
