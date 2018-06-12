48ce2f94260cff95fb194f77f005387a989c819d
Jared Johnson <jared@nuts.com>
2018-06-12 08:14:54 -0400

    Configure app.

---
+++ client/config-overrides.js
@@ -0,0 +1,33 @@
+// https://ant.design/docs/react/use-in-typescript
+const tsImportPluginFactory = require('ts-import-plugin')
+const { getLoader } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
+module.exports = function override(config, env) {
+  const tsLoader = getLoader(
+    config.module.rules,
+    rule =>
+      rule.loader &&
+      typeof rule.loader === 'string' &&
+      rule.loader.includes('ts-loader')
+  );
+
+  tsLoader.options = {
+    getCustomTransformers: () => ({
+      before: [tsImportPluginFactory({
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true
+      })]
+    })
+  };
+
+  config = rewireLess.withLoaderOptions({
+    modifyVars: {
+      "@primary-color": "#FC354C",
+      "@brand-primary": "@primary-color"
+    },
+  })(config, env);
+
+  return config;
+};
