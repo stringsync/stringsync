656d26168d689ddb0cfd1e290ccef6aa5bd8b16e
Jared Johnson <jared@nuts.com>
2018-06-13 08:16:20 -0400

    Attempt typings at react-sizes.

--- client/src/data/viewport/getViewportType.ts
+++ client/src/data/viewport/getViewportType.ts
@@ -1,15 +1,18 @@
 import * as withSizes from 'react-sizes';
 
+// FIXME: Fix the typings for withSizes.
+const { isMobile, isTablet } = withSizes as any;
+
 /**
  * Returns a string that describes the viewport
  * 
  * @param {number} width 
  * @returns {string}
  */
-const getViewportType = width => {
-  if (withSizes.isMobile({ width })) {
+const getViewportType = (width: number): string => {
+  if (isMobile({ width })) {
     return 'MOBILE';
-  } else if (withSizes.isTablet({ width })) {
+  } else if (isTablet({ width })) {
     return 'TABLET';
   } else {
     return 'DESKTOP';
