3bc19d2ad4ea4e5b2daa23f2ac19b2b9bf3d388a
Jared Johnson <jared@nuts.com>
2018-06-16 15:42:12 -0400

    Begin to work on the NotationIndex.

--- client/src/modules/notations/index/NotationIndex.tsx
+++ client/src/modules/notations/index/NotationIndex.tsx
@@ -1,7 +1,31 @@
 import * as React from 'react';
+import styled from 'react-emotion';
+import { BackTop } from 'antd';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
+import { fetchAllNotations } from 'data';
+
+const enhance = compose(
+  connect(
+    (state: StringSync.Store.IState) => ({
+      notations: state.notations.index,
+      viewportType: state.viewport.type
+    }),
+    dispatch => ({
+      fetchAllNotations: () => fetchAllNotations()(dispatch)
+    })
+  )
+);
+
+const Outer = styled('div')`
+  overflow-x: hidden;
+  max-width: 1200px;
+  margin: 0 auto;
+  margin-top: 24px;
+`;
 
 export const NotationIndex: React.SFC = () => (
-  <div>
-    NotationIndex
-  </div>
+  <Outer>
+    <BackTop />
+  </Outer>
 );
