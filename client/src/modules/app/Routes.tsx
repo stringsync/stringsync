7b3f9f2bae68164adda0c3ee79f9b8dce0d7d3e8
Jared Johnson <jared@nuts.com>
2018-06-16 13:04:57 -0400

    Flesh out routes for notations.

--- client/src/modules/app/Routes.tsx
+++ client/src/modules/app/Routes.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { AuthRoute } from './AuthRoute';
-import { NotationIndex } from 'modules/notations';
+import { Notation, NotationIndex } from 'modules/notations';
+import { NotFound } from 'components';
 
 export const Routes = () => (
   <Switch>
     <Route exact={true} path="/" component={NotationIndex} />
+    <Route path="/n" component={Notation} />
+    <Route component={NotFound} />
   </Switch>
 );
