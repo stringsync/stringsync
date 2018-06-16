a8473b2a16358df70c4e24bf8e1abf5243339518
Jared Johnson <jared@nuts.com>
2018-06-16 09:28:13 -0400

    Create the Root component.

---
+++ client/src/constants/THEME.ts
@@ -0,0 +1,9 @@
+const THEME = Object.freeze({
+  borderColor: '#EFEFEF',
+  primaryColor: '#FC354C',
+  quaternaryColor: '#161616',
+  secondaryColor: '#6CABBA',
+  tertiaryColor: '#F4F4F4'
+});
+
+export default THEME;
--- client/src/modules/root/Root.tsx
+++ client/src/modules/root/Root.tsx
@@ -5,15 +5,24 @@ import { BrowserRouter } from 'react-router-dom';
 import { LocaleProvider } from 'antd';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'emotion-theming';
+import THEME from 'constants/theme';
 
 interface IRootProps {
   store: Store<StringSync.StoreState>
 }
 
 const Root: React.SFC<IRootProps> = props => (
-  <div>
-    Hello, world!
-  </div>
+  <Provider store={props.store}>
+    <BrowserRouter>
+      <LocaleProvider locale={enUS}>
+        <ThemeProvider theme={THEME}>
+          <div>
+            henlo
+          </div>
+        </ThemeProvider>
+      </LocaleProvider>
+    </BrowserRouter> 
+  </Provider>
 );
 
 export default Root;
