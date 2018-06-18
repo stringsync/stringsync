cb37946137f0f38dc06a0c00015704d9eed916c6
Jared Johnson <jared@nuts.com>
2018-06-18 07:50:22 -0400

    Create MiniNotationDetail component.

---
+++ client/src/modules/notations/show/controls/MiniNotationDetail.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { compose } from 'recompose';
+import styled from 'react-emotion';
+import { connect } from 'react-redux';
+
+interface IInnerProps {
+  songName: string;
+  artistName: string;
+  thumbnailUrl: string;
+}
+
+const enhance = compose<IInnerProps, {}>(
+  connect(
+    (state: StringSync.Store.IState) => {
+      const { songName, artistName, thumbnailUrl } = state.notations.show;
+      return { songName, artistName, thumbnailUrl };
+    }
+  )
+);
+
+const Outer = styled('div')`
+  display: flex;
+`;
+
+const Detail = styled('div')`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 12px;
+`;
+
+const Thumbnail = styled('img')`
+  min-width: 36px;
+  min-height: 36px;
+  width: 36px;
+  height: 36px;
+`;
+
+const ArtistName = styled('span')`
+  color: #999;
+  font-size: 10px;
+  line-height: 18px;
+  height: 18px;
+  overflow: hidden;
+`;
+
+const SongName = styled('span')`
+  font-size: 10px;
+  line-height: 18px;
+  height: 18px;
+  overflow: hidden;
+`;
+
+export const MiniNotationDetail = enhance(props => (
+  <Outer>
+    <Thumbnail src={props.thumbnailUrl} />
+    <Detail>
+      <ArtistName>{props.artistName}</ArtistName>
+      <SongName>{props.songName}</SongName>
+    </Detail>
+  </Outer>
+));
