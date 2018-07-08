a31753ea8ef70764faa766b8c5db197351b78817
Jared Johnson <jared@nuts.com>
2018-07-08 09:32:24 -0400

    Make maestro extend AbstractObservable.

--- client/src/services/maestro/Maestro.ts
+++ client/src/services/maestro/Maestro.ts
@@ -1,15 +1,18 @@
 import { TimeKeeper } from 'services';
+import { AbstractObservable } from '../../utilities/AbstractObservable';
 
 /**
  * This class's purpose is to provide a single interface for callers to invoke an update on
  * all of the backend models. If a caller asynchronously invokes an update while the Maestro
  * instance is still computing, it will be ignored.
  */
-export class Maestro {
+export class Maestro extends AbstractObservable {
   public timeKeeper: TimeKeeper;
   public isUpdating: boolean;
 
   constructor(timeKeeper: TimeKeeper) {
+    super();
+
     this.timeKeeper = timeKeeper;
     this.isUpdating = false;
   }
