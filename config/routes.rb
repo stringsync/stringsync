9924f1508dd935f3ad90951e28d393bd1f782646
jaredjj3 <jared@nuts.com>
2018-08-12 16:05:48 -0400

    Use the overrides omniauth controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for "User", at: "auth"
+  mount_devise_token_auth_for "User", at: "auth", controllers: {
+    omniauth_callbacks: "overrides/omniauth_callbacks",
+  }
 
   scope :api, defaults: { format: :json } do
     scope :v1 do
