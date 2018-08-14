1777ca43ac2421f27ae7e4db52b1a6d5ab3a1b96
jaredjj3 <jared@nuts.com>
2018-08-13 20:09:48 -0400

    Render loopCaretLoop.

--- client/src/models/vextab/renderers/LoopCaretRenderer.ts
+++ client/src/models/vextab/renderers/LoopCaretRenderer.ts
@@ -54,8 +54,14 @@ export class LoopCaretRenderer {
     this.setStyles(line);
   }
 
+  // TODO: Clean up this function.
   public render(maestro: Maestro): void {
     this.clear();
+    const { vextab } = maestro;
+
+    if (!vextab) {
+      return;
+    }
 
     const specs = this.renderSpec(maestro);
 
@@ -87,6 +93,98 @@ export class LoopCaretRenderer {
         this.rendereredLines.push(line);
       }
     });
+
+    // fill to the right, unless firstLine === lastLine
+    const firstLine = specs[0].line;
+
+    // fill to the left, unless firstLine === lastLine
+    const lastLine = specs[1].line;
+
+    // fill entire lines
+    const inBetweenLines: Line[] = [];
+
+    if (firstLine !== lastLine) {
+      let probeLine = vextab.links.next(firstLine) as Line | void;
+      while (probeLine && probeLine !== lastLine) {
+        inBetweenLines.push(probeLine);
+        probeLine = vextab.links.next(probeLine) as Line | void;
+      }
+    }
+
+    // There are only two cases we need to handle:
+    //   The firstLine and lastLine are the same line. Only fill in between the carets.
+    //   The firstLine and lastLines are different. Fill the first line all the way to the right,
+    //   the inBewteenLines completely, and the lastLine all the way to the left.
+    if (firstLine === lastLine) {
+      const { canvas } = this.store.fetch(firstLine);
+
+      if (!canvas) {
+        return;
+      }
+
+      const ctx = canvas.getContext('2d');
+
+      if (!ctx) {
+        return;
+      }
+
+      ctx.clearRect(0, 0, canvas.width, LoopCaretRenderer.CARET_HEIGHT);
+      ctx.rect(specs[0].x, 0, specs[1].x - specs[0].x, LoopCaretRenderer.CARET_HEIGHT);
+      ctx.fill();
+    } else {
+      // firstLine
+      let canvas = this.store.fetch(firstLine).canvas;
+
+      if (!canvas) {
+        return;
+      }
+
+      let ctx = canvas.getContext('2d');
+
+      if (!ctx) {
+        return;
+      }
+
+      ctx.clearRect(0, 0, canvas.width, LoopCaretRenderer.CARET_HEIGHT);
+      ctx.rect(specs[0].x, 0, canvas.width, LoopCaretRenderer.CARET_HEIGHT);
+      ctx.fill();
+
+      // inBetweenLines
+      inBetweenLines.forEach(line => {
+        const lineCanvas = this.store.fetch(line).canvas;
+
+        if (!lineCanvas) {
+          return;
+        }
+
+        const lineCtx = lineCanvas.getContext('2d');
+
+        if (!lineCtx) {
+          return;
+        }
+
+        lineCtx.clearRect(0, 0, lineCanvas.width, LoopCaretRenderer.CARET_HEIGHT);
+        lineCtx.rect(0, 0, lineCanvas.width, LoopCaretRenderer.CARET_HEIGHT);
+        lineCtx.fill();
+      });
+
+      // lastLine
+      canvas = this.store.fetch(lastLine).canvas;
+
+      if (!canvas) {
+        return;
+      }
+
+      ctx = canvas.getContext('2d');
+
+      if (!ctx) {
+        return;
+      }
+
+      ctx.clearRect(0, 0, canvas.width, LoopCaretRenderer.CARET_HEIGHT);
+      ctx.rect(0, 0, specs[1].x, LoopCaretRenderer.CARET_HEIGHT);
+      ctx.fill();
+    }
   }
 
   public clear(): void {
