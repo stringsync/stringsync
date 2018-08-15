0f8a08efce003e37f7144787da6bcf0abff79bd6
jaredjj3 <jared@nuts.com>
2018-08-15 11:49:11 -0400

    Enhance the DirectiveExtractor documentation.

--- client/src/models/vextab/renderers/DirectiveExtractor.ts
+++ client/src/models/vextab/renderers/DirectiveExtractor.ts
@@ -3,9 +3,13 @@
  * extract process is as follows:
  * 
  *   - For each tab_note
- *     - If a tab_note has an annotation that starts with "JSON=", consider it a directive.
+ *     - If a tab_note has an annotation that starts with "JSON=", consider that annotation
+ *       a directive.
+ *     - Remove that annotation so it doesn't render as text.
  *     - Create the Vexflow and StringSync data structures necessary for the directive.
  *       Insert them into the tab_notes array and Vextab (respectively) as necessary.
+ *     - Create a directive object that a directive handler will act on later.
+ *     - Add a directive reference to the StringSync data structure.
  */
 export class DirectiveExtractor {
 
