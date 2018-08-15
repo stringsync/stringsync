43e550ece879986ce2679733122fc4a24af64590
jaredjj3 <jared@nuts.com>
2018-08-15 11:25:57 -0400

    Add documentation for the DirectiveExtractor class.

--- client/src/models/vextab/renderers/DirectiveExtractor.ts
+++ client/src/models/vextab/renderers/DirectiveExtractor.ts
@@ -1,3 +1,12 @@
+/**
+ * This class is used to extract the custom directives from a Vexflow Artist's stave. The
+ * extract process is as follows:
+ * 
+ *   - For each tab_note
+ *     - If a tab_note has an annotation that starts with "JSON=", consider it a directive.
+ *     - Create the Vexflow and StringSync data structures necessary for the directive.
+ *       Insert them into the tab_notes array and Vextab (respectively) as necessary.
+ */
 export class DirectiveExtractor {
 
 }
