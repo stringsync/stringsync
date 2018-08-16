1fc3175d954e14c6fe74cca0caf3660ab445c094
jaredjj3 <jared@nuts.com>
2018-08-16 19:52:51 -0400

    Do not require a chord to be more than one note.

--- client/src/models/music/chord/Chord.ts
+++ client/src/models/music/chord/Chord.ts
@@ -23,10 +23,6 @@ export class Chord extends AbstractVexWrapper {
   constructor(notes: Note[]) {
     super();
 
-    if (notes.length <= 1) {
-      throw new Error('expected more than one note to construct a chord');
-    }
-
     this.id = id();
     this.notes = notes;
     this.renderer = new NoteRenderer(this);
