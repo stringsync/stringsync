45ba59a691d31649111e4e2755039baade1b9f27
jaredjj3 <jared@nuts.com>
2018-08-22 11:46:30 -0400

    Flesh out the notations/edit/controls/details components.

---
+++ client/src/modules/notations/edit/controls/details/Chord.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { compose } from 'recompose';
+
+const enhance = compose(
+
+);
+
+export const Chord = enhance(() => (
+  <div>foo</div>
+));
---
+++ client/src/modules/notations/edit/controls/details/Rest.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { compose } from 'recompose';
+
+const enhance = compose(
+
+);
+
+export const Rest = enhance(() => (
+  <div>foo</div>
+));
