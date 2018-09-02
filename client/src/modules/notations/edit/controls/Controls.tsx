b01aee1886ddadd2d4c2866312a373f076c4defe
jaredjj3 <jared@nuts.com>
2018-09-02 08:55:19 -0400

    Add legacy control toggles to the edit view.

--- client/src/modules/notations/edit/controls/Controls.tsx
+++ client/src/modules/notations/edit/controls/Controls.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { Selector } from './selector';
 import styled from 'react-emotion';
-import { Divider, Form } from 'antd';
+import { Divider, Form, Collapse } from 'antd';
 import { Primary } from './Primary';
 import { Selected } from './Selected';
 import { TimeEditors } from './TimeEditors';
 import { VextabStringEditor } from './VextabStringEditor';
+import CollapsePanel from 'antd/lib/collapse/CollapsePanel';
 
 const Outer = styled('div')`
   padding: 12px 16px;
@@ -15,10 +16,16 @@ export const Controls: React.SFC = () => (
   <Outer>
     <Divider orientation="left">MAIN</Divider>
     <Form layout="inline">
-      <Primary />
-      <TimeEditors />
-      <Selector />
-      <VextabStringEditor />
+      <Collapse bordered={false}>
+        <Collapse.Panel key="1" header="new">
+          <Primary />
+          <TimeEditors />
+          <Selector />
+        </Collapse.Panel>
+        <CollapsePanel key="2" header="old">
+          <VextabStringEditor />
+        </CollapsePanel>
+      </Collapse>
     </Form>
     <Divider />
     <Selected />
