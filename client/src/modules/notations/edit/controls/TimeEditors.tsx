a55440ff214bf6bb6ff3dc09fb0e1cf4f4f4f8e4
jaredjj3 <jared@nuts.com>
2018-09-02 09:21:20 -0400

    Add bpm to time editors.

--- client/src/modules/notations/edit/controls/TimeEditors.tsx
+++ client/src/modules/notations/edit/controls/TimeEditors.tsx
@@ -8,11 +8,14 @@ interface IConnectProps {
   deadTimeMs: number;
   durationMs: number;
   notationId: number;
+  bpm: number;
+  updateBpm: (notationId: number, bpm: number) => void;
   updateDeadTimeMs: (notationId: number, deadTimeMs: number) => void;
   updateDurationMs: (notationId: number, durationMs: number) => void;
 }
 
 interface IHandlerProps extends IConnectProps {
+  handleBpmChange: (e: number | string) => void;
   handleDeadTimeMsChange: (e: number | string) => void;
   handleDurationMsChange: (e: number | string) => void;
 }
@@ -20,11 +23,15 @@ interface IHandlerProps extends IConnectProps {
 const enhance = compose<IHandlerProps, {}>(
   connect(
     (state: Store.IState) => ({
+      bpm: state.notation.bpm,
       deadTimeMs: state.notation.deadTimeMs,
       durationMs: state.notation.durationMs,
       notationId: state.notation.id
     }),
     (dispatch: Dispatch) => ({
+      updateBpm: (notationId: number, bpm: number) => (
+        dispatch(updateNotation(notationId, { bpm }) as any)
+      ),
       updateDeadTimeMs: (notationId: number, deadTimeMs: number) => (
         dispatch(updateNotation(notationId, { dead_time_ms: deadTimeMs }) as any)
       ),
@@ -34,6 +41,15 @@ const enhance = compose<IHandlerProps, {}>(
     })
   ),
   withHandlers({
+    handleBpmChange: (props: IConnectProps) => (value: number | string) => {
+      const bpm = typeof value === 'string' ? parseInt(value, 10) : value;
+
+      if (isNaN(bpm)) {
+        return;
+      }
+
+      props.updateBpm(props.notationId, bpm);
+    },
     handleDeadTimeMsChange: (props: IConnectProps) => (value: number | string) => {
       const deadTimeMs = typeof value === 'string' ? parseInt(value, 10) : value;
 
@@ -69,6 +85,13 @@ export const TimeEditors = enhance(props => (
         onChange={props.handleDurationMsChange}
         value={props.durationMs}
         step={100}
+      />
+    </Form.Item>
+    <Form.Item label="bpm">
+      <InputNumber
+        onChange={props.handleBpmChange}
+        value={props.bpm}
+        step={0.5}
       />
     </Form.Item>
   </Form.Item>
--- client/src/modules/notations/studio/Studio.tsx
+++ client/src/modules/notations/studio/Studio.tsx
@@ -1,7 +1,150 @@
 import * as React from 'react';
+import * as $ from 'jquery';
+import styled from 'react-emotion';
+import { Affix } from 'antd';
+import { Fretboard, Score, Piano, MaestroController, Overlap, Layer } from 'components';
+import { compose, lifecycle, withProps } from 'recompose';
+import { connect } from 'react-redux';
+import { fetchNotation, VideoActions, UiActions, NotationActions } from 'data';
+import { RouteComponentProps } from 'react-router-dom';
+import { ViewportTypes } from 'data/viewport/getViewportType';
+import { StudioVideo } from './StudioVideo';
+import { Menu } from 'modules/notations/menu';
+import { VideoControls } from 'modules/video-controls';
+import { Element as ScrollElement, scroller } from 'react-scroll';
 
-export const Studio: React.SFC = () => (
-  <div>
-    NotationStudio
-  </div>
+type OuterProps = RouteComponentProps<{ id: string }>;
+
+interface IConnectProps extends OuterProps {
+  notation: Notation.INotation;
+  viewportWidth: number;
+  viewportType: ViewportTypes;
+  isNotationMenuVisible: boolean;
+  fetchNotation: (id: number) => Notation.INotation;
+  focusScrollElement: (scrollElement: string) => void;
+  setNotation: (notation: Notation.INotation) => void;
+  setVideo: (video: Video.IVideo) => void;
+  resetNotation: () => void;
+  resetVideo: () => void;
+  resetUi: () => void;
+  setFretboardVisibility: (fretboardVisibility: boolean) => void;
+}
+
+interface IInnerProps extends IConnectProps {
+  scoreWidth: number;
+}
+
+const enhance = compose<IInnerProps, OuterProps>(
+  connect(
+    (state: Store.IState) => ({
+      isNotationMenuVisible: state.ui.isNotationMenuVisible,
+      notation: state.notation,
+      viewportType: state.viewport.type,
+      viewportWidth: state.viewport.width,
+    }),
+    dispatch => ({
+      fetchNotation: (id: number) => dispatch(fetchNotation(id) as any),
+      focusScrollElement: (scrollElement: string) => dispatch(UiActions.focusScrollElement(scrollElement)),
+      resetNotation: () => dispatch(NotationActions.resetNotation()),
+      resetUi: () => dispatch(UiActions.reset()),
+      resetVideo: () => dispatch(VideoActions.resetVideo()),
+      setFretboardVisibility: (visibility: boolean) => dispatch(UiActions.setFretboardVisibility(visibility)),
+      setVideo: (video: Video.IVideo) => dispatch(VideoActions.setVideo(video))
+    })
+  ),
+  withProps((props: IConnectProps) => ({
+    scoreWidth: 810
+  })),
+  lifecycle<IInnerProps, {}>({
+    async componentDidMount() {
+      $('body').addClass('no-scroll');
+
+      this.props.resetNotation();
+      this.props.resetVideo();
+      this.props.resetUi();
+      this.props.setFretboardVisibility(this.props.viewportType !== 'MOBILE');
+
+      const id = parseInt(this.props.match.params.id, 10);
+
+      try {
+        await this.props.fetchNotation(id);
+
+        const { video } = this.props.notation;
+        if (video) {
+          this.props.setVideo(video)
+        }
+      } catch (error) {
+        console.error(error);
+        window.ss.message.error('something went wrong');
+      }
+    },
+    componentWillUpdate(nextProps) {
+      if (this.props.viewportWidth !== nextProps.viewportWidth) {
+        this.props.focusScrollElement('app-top');
+        scroller.scrollTo('app-top', {});
+      }
+    },
+    componentWillUnmount() {
+      scroller.scrollTo('app-top', {});
+      $('body').removeClass('no-scroll');
+    }
+  })
 );
+
+const Outer = styled('div')`
+  width: 820px;
+  height: 810px;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  margin: 0 auto;
+  margin-top: 20px;
+`;
+
+// FIXME: This component is needed to hack the fretboard
+const Dummy = styled('div')`
+  height: 0.1px;
+  background: black;
+  width: 100%;
+`;
+
+/**
+ * Sets layout for the NotationShow page and fetches the notation from the router.
+ */
+export const Studio = enhance(props => (
+  <Outer id="notation-show">
+    <ScrollElement name="notation-show-top" />
+    <MaestroController
+      bpm={props.notation.bpm}
+      durationMs={props.notation.durationMs}
+      deadTimeMs={props.notation.deadTimeMs}
+    />
+    <Overlap>
+      <Layer zIndex={10}>
+        <div>
+          <StudioVideo />
+          <ScrollElement name="notation-show-score" />
+          <Dummy />
+          <Affix offsetTop={2}>
+            <div>
+              <Fretboard />
+              <Piano />
+            </div>
+          </Affix>
+        </div>
+        <div>
+          <Score
+            editMode={false}
+            dynamic={true}
+            notation={props.notation}
+            width={props.scoreWidth}
+          />
+        </div>
+      </Layer>
+      <Layer zIndex={11}>
+        <Menu />
+        <VideoControls />
+      </Layer>
+    </Overlap>
+  </Outer>
+));
