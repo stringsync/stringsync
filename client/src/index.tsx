fb2c5b23f0aff1dd4b736b0f21e4354d72e028f4
jaredjj3 <jared@nuts.com>
2018-11-06 17:04:38 -0500

    Render App.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { App } from './modules/app';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
