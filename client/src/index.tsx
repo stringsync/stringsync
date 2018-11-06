3690447a6bdb462d987fe47bdd653c686b009678
jaredjj3 <jared@nuts.com>
2018-11-06 16:53:05 -0500

    Register service worker in root.

---
+++ client/src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.register();
