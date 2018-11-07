db0f32c7ee758b518e56022000ed2ac6c0a97598
jaredjj3 <jared@nuts.com>
2018-11-06 21:12:29 -0500

    Add emotion to the project.

--- client/craco.config.js
+++ client/craco.config.js
@@ -32,7 +32,16 @@ module.exports = {
           'style': true
         },
         'ant'
-      ],
+      ], [
+        'emotion',
+        {
+          // sourceMap is on by default but source maps are dead code eliminated in production
+          'sourceMap': true,
+          'autoLabel': process.env.NODE_ENV !== 'production',
+          'labelFormat': '[local]',
+          'cssPropOptimization': true
+        }
+      ]
     ],
   },
   plugins: [
--- client/src/modules/app/App.tsx
+++ client/src/modules/app/App.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 import { Button } from 'antd';
+import styled from 'react-emotion';
+
+const StyledDiv = styled.div`
+  color: blue;
+`;
 
 export const App = () => (
   <div>
+    <StyledDiv>Foobar</StyledDiv>
     <Button type="primary">App</Button>
   </div>
 );
