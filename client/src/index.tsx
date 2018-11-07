f70e18503f15f89c3731597683e689d7b49ca963
jaredjj3 <jared@nuts.com>
2018-11-06 20:38:35 -0500

    Remove less testing files.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { App } from './modules/app';
 import * as serviceWorker from './serviceWorker';
 import 'antd/dist/antd.less';
-import './test.less';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
