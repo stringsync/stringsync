b36e2a951c1cbd60f2de14d25da7586e81925978
Jared Johnson <jared@nuts.com>
2018-11-07 20:49:10 -0500

    Rename App component to Root.

--- client/src/modules/root/Root.test.tsx
+++ client/src/modules/root/Root.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './';
+import { Root } from './';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Root />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
---
+++ client/src/modules/root/Root.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export const Root = () => (
+  <div>
+    Root
+  </div>
+);
