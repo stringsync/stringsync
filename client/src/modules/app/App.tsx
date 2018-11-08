5c5eeabba16f678a7ba98e5b0f12e6900e33b509
Jared Johnson <jared@nuts.com>
2018-11-07 23:46:53 -0500

    Scroll to top on route change.

--- client/src/modules/app/App.tsx
+++ client/src/modules/app/App.tsx
@@ -1,5 +1,20 @@
 import * as React from 'react';
+import { withRouter, RouteComponentProps } from 'react-router';
+import { compose, lifecycle, withHandlers } from 'recompose';
 
-export const App = () => (
-  <div>foo</div>
+const scrollToTop = () => window.scrollTo(0, 0);
+
+const enhance = compose(
+  withRouter,
+  lifecycle<RouteComponentProps, {}>({
+    componentDidMount(): void {
+      this.props.history.listen(scrollToTop);
+    }
+  })
 );
+
+export const App = enhance(() => (
+  <main>
+    app
+  </main>
+));
--- client/src/theme.js
+++ client/src/theme.js
@@ -1,5 +1,7 @@
 const theme = Object.freeze({
-  '@primary-color': '#FC354C'
+  '@primary-color': '#FC354C',
+  '@secondary-color': '#6CABBA',
+  '@tertiary-color': '#F4F4F4'
 });
 
 module.exports = theme;
