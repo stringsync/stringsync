121b14fc91b631e727e3921cc83d5b2b8fd69845
Jared Johnson <jared@nuts.com>
2018-11-10 08:52:01 -0500

    Try to fix babel bug in travis.

--- client/craco.config.js
+++ client/craco.config.js
@@ -2,6 +2,20 @@ const injectAntdTheme = require('./plugins/injectAntdTheme');
 const theme = require('./src/theme');
 
 module.exports = {
+  babel: {
+    plugins: [
+      [
+        'import',
+        {
+          'libraryName': 'antd',
+          'libraryDirectory':
+          'es',
+          'style': true
+        },
+        'ant'
+      ]
+    ],
+  },
   plugins: [
     {
       plugin: {
