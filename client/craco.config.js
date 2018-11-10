15333a92ef4eb93be4a58d8e0fa527a13ef888de
Jared Johnson <jared@nuts.com>
2018-11-10 08:39:42 -0500

    Test travis ci bug.

--- client/craco.config.js
+++ client/craco.config.js
@@ -2,20 +2,6 @@ const injectAntdTheme = require('./plugins/injectAntdTheme');
 const theme = require('./src/theme');
 
 module.exports = {
-  babel: {
-    plugins: [
-      [
-        'import',
-        {
-          'libraryName': 'antd',
-          'libraryDirectory':
-          'es',
-          'style': true
-        },
-        'ant'
-      ]
-    ],
-  },
   plugins: [
     {
       plugin: {
