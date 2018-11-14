0a20fe83e25bd7dea454275d7756cc506312bab3
Jared Johnson <jared@nuts.com>
2018-11-14 09:17:46 -0500

    Finish notation index transitions.

--- client/src/modules/notations/index/grid/Img.tsx
+++ client/src/modules/notations/index/grid/Img.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import styled from 'react-emotion';
 import { Skeleton } from 'antd';
+import { compose, lifecycle } from 'recompose';
+import { Transition } from 'react-transition-group';
+
+const DURATION_MS = 150;
 
 interface IProps {
   src: string;
@@ -9,40 +13,50 @@ interface IProps {
   onLoad: () => void;
 }
 
-const SkeletonOuter = styled('div')<{ hidden: boolean }>`
-  padding: 24px;
-  display: ${props => props.hidden ? 'none' : 'block'};
+const enhance = compose<IProps, IProps>(
+  lifecycle<IProps, {}, {}>({
+    componentDidMount(): void {
+      // preload image
+      const img = new Image();
+      img.onload = this.props.onLoad;
+      img.src = this.props.src;
+    }
+  })
+);
+
+const Outer = styled('div')<{ loading: boolean }>`
+  padding: ${props => props.loading ? 24 : 0}px;
 `;
 
-const StyledImg = styled('img')<{ hidden: boolean, zIndex: number }>`
-  display: ${props => props.hidden ? 'none' : 'block'};
-  z-index: ${props => props.zIndex};
+const StyledImg = styled('img')`
   width: 100%;
   height: 100%;
+  opacity: 0;
+  transition: opacity ${DURATION_MS}ms ease-in-out;
 `;
+
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered: { opacity: 1 },
+};
 
 /**
  * This component will conditionally show the placeholder until
  * the image loads. This avoids showing abrupt animations when
  * the image is loading.
  */
-export const Img: React.SFC<IProps> = (props => (
-  <div>
-    <SkeletonOuter hidden={!props.loading}>
-      <Skeleton
-        active={true}
-        loading={props.loading}
-        paragraph={{ rows: 7 }}
-      >
-        <div />
-      </Skeleton>
-    </SkeletonOuter>
-    <StyledImg
-      src={props.src}
-      alt={props.alt}
-      zIndex={1}
-      hidden={props.loading}
-      onLoad={props.onLoad}
-    />
-  </div>
-));
+export const Img = enhance((props => (
+  <Outer loading={props.loading}>
+    <Transition in={!props.loading} timeout={DURATION_MS}>
+      {state => (
+        <Skeleton
+          active={true}
+          loading={props.loading}
+          paragraph={{ rows: 7 }}
+        >
+          <StyledImg src={props.src} alt={props.alt} style={{ ...transitionStyles[state] }} />
+        </Skeleton>
+      )}
+    </Transition>
+  </Outer>
+)));
