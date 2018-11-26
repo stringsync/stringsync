dc25d4628ef6e864c792975622b37ef2d2cc0d83
Jared Johnson <jared@nuts.com>
2018-11-25 19:55:53 -0500

    Render a Vextab on the Score component.

--- client/src/components/score/Score.tsx
+++ client/src/components/score/Score.tsx
@@ -1,24 +1,53 @@
 import * as React from 'react';
-import { lifecycle, compose } from 'recompose';
-import { OpenSheetMusicDisplay } from 'opensheetmusicdisplay';
-import xml from './example.xml';
+import { compose, withHandlers } from 'recompose';
+import VT from 'vextab/releases/vextab-div.js';
+
+const { Flow, Artist, VexTab } = VT;
 
 interface IProps {
   src: string;
 }
 
-const enhance = compose<IProps, IProps>(
-  lifecycle({
-    async componentDidMount() {
-      const osmd = new OpenSheetMusicDisplay('score');
-      await osmd.load(xml);
-      osmd.render();
-    }
+interface IHandlerProps {
+  handleDivRef: any;
+}
+
+type InnerProps = IHandlerProps & IProps;
+
+const enhance = compose<InnerProps, IProps>(
+  withHandlers(() => {
+    let div: HTMLCanvasElement;
+
+    return {
+      handleDivRef: () => ref => {
+        if (!ref) {
+          return;
+        }
+
+        div = ref;
+        div.setAttribute('width', '680');
+        div.setAttribute('scale', '1.0');
+        div.setAttribute('editor', 'true');
+        div.setAttribute('editor_width', '680');
+
+        const renderer = new Flow.Renderer(div, Flow.Renderer.Backends.SVG);
+        const artist = new Artist(10, 10, 600);
+        const vextab = new VexTab(artist);
+
+        try {
+          // Parse VexTab music notation passed in as a string.
+          vextab.parse('tabstave notation=true\n notes :q 4/4\n');
+
+          // Render notation onto canvas.
+          artist.render(renderer);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    };
   })
 );
 
-export const Score = enhance(() => (
-  <div>
-    <div id="score" style={{ width: '800px' }} />
-  </div>
+export const Score = enhance(props => (
+  <div ref={props.handleDivRef} />
 ));
