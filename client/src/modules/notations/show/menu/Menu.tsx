aeb7949f9d0cc6ae9c7147190a60c2b7edcf8a50
Jared Johnson <jared@nuts.com>
2018-12-01 19:41:15 -0500

    Toggle fretboard visibility on notation show menu.

--- client/src/modules/notations/show/menu/Menu.tsx
+++ client/src/modules/notations/show/menu/Menu.tsx
@@ -1,40 +1,73 @@
 import * as React from 'react';
-import { compose } from 'recompose';
-import { Drawer } from 'antd';
+import { compose, withHandlers } from 'recompose';
+import { Drawer, Checkbox } from 'antd';
 import { connect } from 'react-redux';
 import { IStore } from '../../../../@types/store';
 import { NotationMenuActions } from '../../../../data/notation-menu/notationMenuActions';
+import styled from 'react-emotion';
+import { CheckboxChangeEvent } from 'antd/lib/checkbox';
 
 interface IStateProps {
   visible: boolean;
+  fretboardVisible: boolean;
 }
 
 interface IDispatchProps {
   hide: () => void;
+  setFretboardVisibility: (fretboardVisible: boolean) => void;
 }
 
 type ConnectProps = IStateProps & IDispatchProps;
 
-const enhance = compose<ConnectProps, {}>(
+interface IHandlerProps {
+  handleFretboardVisibilityChange: (event: CheckboxChangeEvent) => void;
+}
+
+type InnerProps = ConnectProps & IHandlerProps;
+
+const enhance = compose<InnerProps, {}>(
   connect<IStateProps, IDispatchProps, {}, IStore>(
     state => ({
-      visible: state.notationMenu.visible
+      visible: state.notationMenu.visible,
+      fretboardVisible: state.notationMenu.fretboardVisible
     }),
     dispatch => ({
-      hide: () => dispatch(NotationMenuActions.hide())
+      hide: () => dispatch(NotationMenuActions.hide()),
+      setFretboardVisibility: (fretboardVisibility: boolean) => {
+        dispatch(NotationMenuActions.setFretboardVisibility(fretboardVisibility));
+      }
     })
-  )
+  ),
+  withHandlers<ConnectProps, IHandlerProps>({
+    handleFretboardVisibilityChange: props => event => {
+      props.setFretboardVisibility(event.target.checked);
+    }
+  })
 );
+
+const MenuItem = styled('li')`
+  margin-top: 8px;
+  padding: 16px;
+  cursor: pointer;
+`;
 
 export const Menu = enhance(props => (
   <Drawer
-    title="Foo Bar"
+    title="settings"
     placement="right"
-    closable={false}
     onClose={props.hide}
     visible={props.visible}
   >
-    <p>foo</p>
-    <p>bar</p>
+    <ul>
+      <MenuItem>
+        <Checkbox
+          defaultChecked={true}
+          checked={props.fretboardVisible}
+          onChange={props.handleFretboardVisibilityChange}
+        >
+          fretboard
+        </Checkbox>
+      </MenuItem>
+    </ul>
   </Drawer>
 ));
