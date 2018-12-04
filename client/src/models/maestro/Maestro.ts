ffa1dc28f1dfbf52cba2f752fc8b0b12cbb4ff89
Jared Johnson <jared@nuts.com>
2018-12-04 18:38:05 -0500

    Add baseline data tracking for Maestro.

--- client/src/models/maestro/Maestro.ts
+++ client/src/models/maestro/Maestro.ts
@@ -1,7 +1,18 @@
+import { Score } from '../score';
+import { IPlayer } from '../../@types/youtube';
+
 /**
  * This class is concerned with determining the position within a Score instance
  * given a time.
  */
 export class Maestro {
+  public readonly score: Score;
+  public readonly bpm: number;
+  public readonly player: IPlayer;
 
+  constructor(score: Score, bpm: number, player: IPlayer) {
+    this.score = score;
+    this.bpm = bpm;
+    this.player = player;
+  }
 }
