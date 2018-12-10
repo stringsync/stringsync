71a6cf604db19eb10f762fb500a352a00a5b6ef2
Jared Johnson <jared@nuts.com>
2018-12-10 09:31:06 -0500

    Create renderCaret callback in Caret.

--- client/src/components/score/Caret.tsx
+++ client/src/components/score/Caret.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
-import { compose, branch, renderNothing, lifecycle, ReactLifeCycleFunctionsThisArguments } from 'recompose';
+import { compose, branch, renderNothing, lifecycle } from 'recompose';
 import { withMaestro, IWithMaestroProps } from '../../enhancers/withMaestro';
-import { connect } from 'react-redux';
-import { IStore } from '../../@types/store';
 import { interpolate } from '../../utils/interpolate';
-import { ISpec } from '../../models/maestro/Maestro';
-import { msToTick } from '../../utils/conversions';
+import { ISpec, Maestro } from '../../models/maestro/Maestro';
 import { get } from 'lodash';
-import { loop } from '../../enhancers/loop';
 
 interface IProps {
   visible: boolean;
@@ -21,33 +17,42 @@ interface IStateProps {
 
 type InnerProps = IProps & IWithMaestroProps & IStateProps;
 
+const renderCaret = (maestro: Maestro) => {
+  const { spec } = maestro;
+
+  if (!spec) {
+    return;
+  }
+
+  const t0 = spec.start.tick;
+  const t1 = spec.stop.tick;
+  const x0 = spec.start.note.graphic.getBBox().x;
+  const x1 = spec.stop.note.graphic.getBBox().x;
+  const t = maestro.currentTick;
+
+  const line = get(spec.start.note.measure, 'line');
+
+  if (!line) {
+    throw new Error('expected note to be hydrated with a measure');
+  }
+
+  const x = interpolate({ x: t0, y: x0 }, { x: t1, y: x1 }, t);
+  const y = line.graphic.getBBox().y;
+
+  maestro.score.caret.render(x, y);
+};
+
+const maestroListener = Object.freeze({
+  name: 'renderCaret',
+  callback: renderCaret
+});
+
 const enhance = compose<InnerProps, IProps>(
   withMaestro,
   branch<InnerProps>(
     props => !props.visible || !props.maestro || !props.maestro.spec,
     renderNothing
-  ),
-  loop((props: any) => {
-    const { maestro } = props;
-    const { spec } = maestro;
-
-    const t0 = spec.start.tick;
-    const t1 = spec.stop.tick;
-    const x0 = spec.start.note.graphic.getBBox().x;
-    const x1 = spec.stop.note.graphic.getBBox().x;
-    const t = maestro.currentTick;
-
-    const line = get(spec.start.note.measure, 'line');
-
-    if (!line) {
-      throw new Error('expected note to be hydrated with a measure');
-    }
-
-    const x = interpolate({ x: t0, y: x0 }, { x: t1, y: x1 }, t);
-    const y = line.graphic.getBBox().y;
-
-    props.maestro.score.caret.render(x, y);
-  })
+  )
 );
 
 export const Caret = enhance(() => null);
