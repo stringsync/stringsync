ac02c088290043b57024c322ee3bf860f582eb6d
Jared Johnson <jared@nuts.com>
2018-12-19 19:04:44 -0500

    Add form elements to the NotationEdit.

--- client/src/modules/notations/edit/Editor.tsx
+++ client/src/modules/notations/edit/Editor.tsx
@@ -1,8 +1,28 @@
 import * as React from 'react';
 import { compose } from 'recompose';
+import styled from 'react-emotion';
+import { InputNumber, Form, Input } from 'antd';
 
 const enhance = compose(
-
 );
 
-export const Editor = enhance(props => <div>Editor</div>);
+const Outer = styled('div')`
+  margin: 24px;
+  padding-bottom: 72px;
+`;
+
+export const Editor = enhance(props => (
+  <Outer>
+    <Form>
+      <Form.Item label="dead time (ms)">
+        <InputNumber />
+      </Form.Item>
+      <Form.Item label="bpm">
+        <InputNumber />
+      </Form.Item>
+      <Form.Item label="vextab string">
+        <Input.TextArea autosize={{ minRows: 5 }} />
+      </Form.Item>
+    </Form>
+  </Outer>
+));
