2790588dcd33fe2edb3fb5eda7046b20dfe35cbc
Jared Johnson <jared@nuts.com>
2018-12-22 14:56:06 -0500

    Enable two way state setting for Scrubber.

--- client/src/components/video/controls/Scrubber.tsx
+++ client/src/components/video/controls/Scrubber.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Slider } from 'antd';
 import styled from 'react-emotion';
-import { compose, withState, branch, renderNothing } from 'recompose';
+import { compose, withState, withHandlers } from 'recompose';
 import { IPlayer } from '../../../@types/youtube';
 import { connect } from 'react-redux';
 import { IStore } from '../../../@types/store';
 import { subscribeMaestro } from '../../../enhancers/subscribeMaestro';
+import { msToS } from '../../../utils/conversions';
+import { SliderValue } from 'antd/lib/slider';
 
 interface IStateProps {
   player: IPlayer | null;
@@ -13,12 +15,21 @@ interface IStateProps {
   durationMs: number;
 }
 
-interface ISliderValueProps {
+interface IInnerStateProps {
   sliderValue: number;
+  scrubbing: boolean;
+  playAfterScrub: boolean;
   setSliderValue: (sliderValue: number) => void;
+  setScrubbing: (isScrubbing: boolean) => void;
+  setPlayAfterScrub: (playAfterScrub: boolean) => void;
 }
 
-type InnerProps = IStateProps & ISliderValueProps;
+interface IHandlerProps {
+  scrub: (value: SliderValue) => void;
+  afterScrub: (value: SliderValue) => void;
+}
+
+type InnerProps = IStateProps & IInnerStateProps & IHandlerProps;
 
 const enhance = compose<InnerProps, {}>(
   connect<IStateProps, {}, {}, IStore>(
@@ -29,14 +40,73 @@ const enhance = compose<InnerProps, {}>(
     })
   ),
   withState('sliderValue', 'setSliderValue', 0),
-  branch<InnerProps>(
-    props => typeof props.durationMs !== 'number' || props.durationMs <= 0,
-    renderNothing
-  ),
+  withState('scrubbing', 'setScrubbing', false),
+  withState('playAfterScrub', 'setPlayAfterScrub', false),
+  withHandlers<IStateProps & IInnerStateProps, IHandlerProps>({
+    scrub: props => value => {
+      if (typeof value !== 'number') {
+        return;
+      }
+
+      if (!props.scrubbing && value < 100) {
+        props.setScrubbing(true);
+        props.setPlayAfterScrub(!!props.isVideoActive);
+      }
+
+      if (props.player) {
+        if (props.isVideoActive) {
+          props.player.pauseVideo();
+        }
+
+        const timeS = msToS(props.durationMs * (value / 100));
+
+        if (typeof timeS === 'number' && !isNaN(timeS)) {
+          props.player.seekTo(timeS, true);
+        }
+      }
+
+      props.setSliderValue(value);
+    },
+    afterScrub: props => value => {
+      if (typeof value !== 'number') {
+        return;
+      }
+
+      if (props.player) {
+        const timeS = msToS(props.durationMs * (value / 100));
+
+        if (typeof timeS === 'number' && !isNaN(timeS)) {
+          props.player.seekTo(timeS, true);
+        }
+
+        if (props.playAfterScrub) {
+          props.player.playVideo();
+        }
+      }
+
+      props.setSliderValue(value);
+      props.setPlayAfterScrub(false);
+      props.setScrubbing(false);
+    }
+  }),
   subscribeMaestro<InnerProps>(props => ({
     name: 'updateSliderValue',
     callback: maestro => {
-      props.setSliderValue((maestro.currentTimeMs / props.durationMs) * 100);
+      if (props.scrubbing) {
+        return;
+      }
+
+      const sliderValue = (maestro.currentTimeMs / props.durationMs) * 100;
+
+      const shouldSetSliderValue = (
+        typeof sliderValue === 'number' &&
+        !isNaN(sliderValue) &&
+        props.sliderValue !== sliderValue
+      );
+
+      if (shouldSetSliderValue) {
+        props.setSliderValue(sliderValue);
+      }
     }
   }))
 );
@@ -51,7 +121,10 @@ export const Scrubber = enhance(props => (
       min={0}
       step={0.01}
       max={100}
+      tipFormatter={null}
       value={props.sliderValue}
+      onChange={props.scrub}
+      onAfterChange={props.afterScrub}
     />
   </Outer>
 ));
