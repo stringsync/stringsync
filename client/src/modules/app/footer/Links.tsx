96e791df4b32d638a364f006e8d337c58c9c997b
Jared Johnson <jared@nuts.com>
2018-12-22 15:50:39 -0500

    Remove teach link.

--- client/src/modules/app/footer/Links.tsx
+++ client/src/modules/app/footer/Links.tsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 export const Links = () => (
   <Row type="flex" justify="center" gutter={16}>
     <Col><Link to="/about">about</Link></Col>
-    <Col><Link to="/teach">teach</Link></Col>
     <Col><Link to="/terms">terms</Link></Col>
     <Col><Link to="/contact">contact</Link></Col>
   </Row>
