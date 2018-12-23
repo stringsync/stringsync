3bee0e6fa3217fa0ca5f06db6ecea57be1e3d092
Jared Johnson <jared@nuts.com>
2018-12-23 17:00:49 -0500

    Create test helpers.

---
+++ client/src/test/assertRender.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Root } from 'modules/root';
+import { createStore } from 'data';
+import { xhrMock, localStorageMock } from './mocks';
+import { configure } from 'config';
+
+const assertRender = (Component: React.ComponentClass | React.SFC, props: object = {}): void => {
+  it('renders without crashing', () => {
+    window.XMLHttpRequest = jest.fn(xhrMock);
+    (window as any).localStorage = localStorageMock;
+
+    configure();
+    const div = document.createElement('div');
+    const TestComponent = () => (
+      <Root store={createStore()}>
+        <Component {...props} />
+      </Root>
+    );
+
+    ReactDOM.render(<TestComponent />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+};
+
+export default assertRender;
