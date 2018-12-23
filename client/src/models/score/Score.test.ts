1b561544f7ee47e1e74006709a0e5fa9b560fad8
Jared Johnson <jared@nuts.com>
2018-12-23 18:23:28 -0500

    Add Score test.

---
+++ client/src/models/score/Score.test.ts
@@ -0,0 +1,14 @@
+import { Score } from './Score';
+import { VEXTAB_STRINGS } from '../../test/VEXTAB_STRINGS';
+import { VextabString } from '../vextab-string/VextabString';
+
+VEXTAB_STRINGS.forEach((rawVextabString, ndx) => {
+  const numMeasures = Math.ceil(Math.random() * 5);
+  const vextabString = new VextabString(rawVextabString).asMeasures(numMeasures);
+
+  it(`renders VEXTAB_STRINGS ${ndx} without crashing`, () => {
+    const div = document.createElement('div');
+    const score = new Score(640, div, vextabString);
+    score.render();
+  });
+});
