ffa1293099e2169412ac9e5162d6afa10a30b103
Jared Johnson <jared@nuts.com>
2018-12-24 18:18:25 -0500

    Add logging to notations controller.

--- app/controllers/notations_controller.rb
+++ app/controllers/notations_controller.rb
@@ -25,6 +25,9 @@ class NotationsController < ApplicationController
   end
 
   def create
+    puts "*" * 100
+    puts current_user.inspect
+    puts "*" * 100
     if current_user.try(:has_role?, :teacher)
       @notation = Notation.new(notation_params.merge(transcriber: current_user))
       @notation.tags = Tag.where(id: params.fetch(:notation).fetch(:tag_ids))
