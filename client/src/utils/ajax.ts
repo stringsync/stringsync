4b1ce6d1f63199b88a4a36ea7d8def808f54389b
Jared Johnson <jared@nuts.com>
2018-12-25 15:09:57 -0500

    Modify ajax headers object.

--- client/src/utils/ajax.ts
+++ client/src/utils/ajax.ts
@@ -1,18 +1,18 @@
 import $ from 'jquery';
 
-const beforeSend = (xhr, settings) => {
-  const { ss } = window;
+// const beforeSend = (xhr, settings) => {
+//   const { ss } = window;
 
-  // ensure use of proxy
-  if (ss.env === 'development' && typeof settings.url !== 'undefined') {
-    const url = new URL(`${location.protocol}//${location.host}${settings.url}`);
-    url.host = 'localhost:3001';
-    settings.url = url.toString();
-  }
+//   // ensure use of proxy
+//   if (ss.env === 'development' && typeof settings.url !== 'undefined') {
+//     const url = new URL(`${location.protocol}//${location.host}${settings.url}`);
+//     url.host = 'localhost:3001';
+//     settings.url = url.toString();
+//   }
 
-  ss.auth.appendAuthHeaders(xhr, settings);
-};
+//   ss.auth.appendAuthHeaders(xhr, settings);
+// };
 
 export const ajax = (url: string, settings: JQuery.AjaxSettings = {}): JQuery.jqXHR => (
-  $.ajax(url, { beforeSend, ...settings })
+  $.ajax(url, { headers: window.ss.auth.retrieveData('authHeaders'), ...settings })
 );
