43e94009b88ec3c5396898e6d22559a4c4ebb518
Jared Johnson <jared@nuts.com>
2018-12-26 00:25:58 -0500

    Add NotationDashboard to routes.

--- client/src/modules/routes/Routes.tsx
+++ client/src/modules/routes/Routes.tsx
@@ -6,19 +6,21 @@ import { About } from '../about/About';
 import { Contact } from '../contact/Contact';
 import { Terms } from '../terms/Terms';
 import { Privacy } from '../privacy/Privacy';
+import { NotationDashboard } from '../notations/dashboard/NotationDashboard';
 
 export const Routes = () => (
   <Switch>
-    <Route exact={true} path="/"             component={NotationIndex}  />
-    <Route exact={true} path="/upload"       component={NotationNew}    />
-    <Route exact={true} path="/n/:id/edit"   component={NotationEdit}   />
-    <Route exact={true} path="/n/:id/print"  component={NotationPrint}  />
-    <Route exact={true} path="/n/:id/studio" component={NotationStudio} />
-    <Route exact={true} path="/n/:id"        component={NotationShow}   />
-    <Route exact={true} path="/about"        component={About}          />
-    <Route exact={true} path="/contact"      component={Contact}        />
-    <Route exact={true} path="/terms"        component={Terms}          />
-    <Route exact={true} path="/privacy"      component={Privacy}        />
-    <Route                                   component={NotFound}       />
+    <Route exact={true} path="/"             component={NotationIndex}     />
+    <Route exact={true} path="/upload"       component={NotationNew}       />
+    <Route exact={true} path="/n/:id/edit"   component={NotationEdit}      />
+    <Route exact={true} path="/n/:id/print"  component={NotationPrint}     />
+    <Route exact={true} path="/n/:id/studio" component={NotationStudio}    />
+    <Route exact={true} path="/n/:id"        component={NotationShow}      />
+    <Route exact={true} path="/dashboard"    component={NotationDashboard} />
+    <Route exact={true} path="/about"        component={About}             />
+    <Route exact={true} path="/contact"      component={Contact}           />
+    <Route exact={true} path="/terms"        component={Terms}             />
+    <Route exact={true} path="/privacy"      component={Privacy}           />
+    <Route                                   component={NotFound}          />
   </Switch>
 );
