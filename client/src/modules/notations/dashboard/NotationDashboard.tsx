8426d5750306418dc56078195e690162dfb2d6b2
Jared Johnson <jared@nuts.com>
2018-12-26 00:35:57 -0500

    Fetch all notations on notation dashboard mount.

--- client/src/modules/notations/dashboard/NotationDashboard.tsx
+++ client/src/modules/notations/dashboard/NotationDashboard.tsx
@@ -1,9 +1,38 @@
 import * as React from 'react';
-import { compose } from 'recompose';
+import { compose, lifecycle } from 'recompose';
 import { Lane } from '../../../components/lane/Lane';
+import { INotation } from '../../../@types/notation';
+import { connect } from 'react-redux';
+import { IStore } from '../../../@types/store';
+import { NotationsActions } from '../../../data/notations/notationsActions';
+import { fetchAllNotations } from '../../../data/notation/notationApi';
 
-const enhance = compose(
+interface IStateProps {
+  notations: INotation[];
+}
 
+interface IDispatchProps {
+  setNotations: (notations: INotation[]) => void;
+}
+
+type InnerProps = IStateProps & IDispatchProps;
+
+const enhance = compose<InnerProps, {}>(
+  connect<IStateProps, IDispatchProps, {}, IStore>(
+    state => ({
+      notations: state.notations
+    }),
+    dispatch => ({
+      setNotations: (notations: INotation[]) => dispatch(NotationsActions.setNotations(notations))
+    })
+  ),
+  lifecycle<InnerProps, {}, {}>({
+    async componentDidMount(): Promise<void> {
+      const notations = await fetchAllNotations('all');
+      const sorted = notations.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
+      this.props.setNotations(sorted);
+    }
+  })
 );
 
 export const NotationDashboard = enhance(props => (
