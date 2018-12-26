071a4af135a1d9f0007055eb14e4ad1854fc2c10
Jared Johnson <jared@nuts.com>
2018-12-26 13:22:12 -0500

    Serialize tokens in the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,13 @@ class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
   include DeviseTokenAuth::Concerns::UserOmniauthCallbacks
 
-  devise(*%i(database_authenticatable registerable recoverable rememberable trackable validatable))
-
   # The purpose of roles is to determine if the user is allowed to perform certain actions.
   # The order of role declaration matters. Each role can be considered the same as all the
   # roles preceding it.
   enum(role: %i(student teacher admin))
+
+  devise(*%i(database_authenticatable registerable recoverable rememberable trackable validatable))
+  serialize(:tokens)
 
   has_many(:transcribed_notations, foreign_key: :transcriber_id, class_name: "Notation")
 
