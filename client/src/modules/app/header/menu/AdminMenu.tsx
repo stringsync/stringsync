d730c1d02e102ff81e5954554fc5ca45bf365971
Jared Johnson <jared@nuts.com>
2018-12-26 00:43:50 -0500

    Add dashboard to admin menu.

--- client/src/modules/app/header/menu/AdminMenu.tsx
+++ client/src/modules/app/header/menu/AdminMenu.tsx
@@ -1,4 +1,58 @@
 import * as React from 'react';
-import { TeacherMenu } from './TeacherMenu';
+import { Menu } from 'antd';
+import { Link } from 'react-router-dom';
+import { MENU_PROPS } from './MENU_PROPS';
+import { ITEM_PROPS } from './ITEM_PROPS';
+import { SUB_MENU_PROPS } from './SUB_MENU_PROPS';
+import { Avatar } from '../../../../components/avatar/Avatar';
+import { compose, withHandlers } from 'recompose';
+import { ClickParam } from 'antd/lib/menu';
+import { ISession } from '../../../../@types/session';
+import { withAuth, IWithAuthProps } from '../../../../enhancers/withAuth';
 
-export const AdminMenu = props => <TeacherMenu {...props} />;
+interface IOuterProps {
+  session: ISession;
+}
+
+interface IInnerProps extends IOuterProps {
+  handleMenuClick: (e: ClickParam) => void;
+}
+
+const enhance = compose<IInnerProps, IOuterProps>(
+  withAuth,
+  withHandlers<IOuterProps & IWithAuthProps, any>({
+    handleMenuClick: props => ({ key }) => {
+      if (key === 'sign-out') {
+        props.signOut();
+      }
+    }
+  })
+);
+
+export const AdminMenu = enhance(props => {
+  const { image, name } = props.session;
+
+  return (
+    <Menu
+      {...MENU_PROPS}
+      onClick={props.handleMenuClick}
+    >
+      <Menu.SubMenu
+        {...SUB_MENU_PROPS}
+        title={<Avatar src={image} name={name} />}
+      >
+        <Menu.ItemGroup title={name}>
+          <Menu.Item key="dashboard" {...ITEM_PROPS}>
+            <Link to="/dashboard">dashboard</Link>
+          </Menu.Item>
+          <Menu.Item key="upload" {...ITEM_PROPS}>
+            <Link to="/upload">upload</Link>
+          </Menu.Item>
+          <Menu.Item key="sign-out" {...ITEM_PROPS}>
+            sign out
+          </Menu.Item>
+        </Menu.ItemGroup>
+      </Menu.SubMenu>
+    </Menu>
+  );
+});
