e5166c82db3ecb6ce6e99814844953cb68a1eb55
Jared Johnson <jared@nuts.com>
2019-02-02 17:59:03 -0500

    Create the NotationStudio component.

--- client/src/modules/notations/studio/NotationStudio.tsx
+++ client/src/modules/notations/studio/NotationStudio.tsx
@@ -1,3 +1,33 @@
 import * as React from 'react';
+import { Lane } from '../../../components/lane/Lane';
+import { compose } from 'recompose';
+import { RouteComponentProps } from 'react-router';
+import { withNotation } from '../../../enhancers/withNotation';
 
-export const NotationStudio = () => <div>NotationStudio</div>;
+type RouteProps = RouteComponentProps<{ id: string }>;
+
+const noop = () => null;
+
+const enhance = compose<RouteProps, RouteProps>(
+  withNotation<RouteProps>(
+    props => parseInt(props.match.params.id, 10),
+    noop,
+    props => {
+      const notationId = props.match.params.id;
+      window.ss.message.error(`could not load notation '${notationId}'`);
+      props.history.push('/');
+    },
+    noop
+  )
+);
+
+export const NotationStudio = enhance(() => (
+  <Lane
+    withTopMargin={true}
+    withPadding={true}
+  >
+    <div>Video</div>
+    <div>Fretboard</div>
+    <div>Notation</div>
+  </Lane>
+));
