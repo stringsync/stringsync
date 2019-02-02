2c4451f3b5806a70e118c37962f57decd065f1cd
Jared Johnson <jared@nuts.com>
2019-02-02 18:37:04 -0500

    Render NotationStudio inners.

--- client/src/modules/notations/studio/NotationStudio.tsx
+++ client/src/modules/notations/studio/NotationStudio.tsx
@@ -2,13 +2,20 @@ import * as React from 'react';
 import { Lane } from '../../../components/lane/Lane';
 import { compose } from 'recompose';
 import { RouteComponentProps } from 'react-router';
-import { withNotation } from '../../../enhancers/withNotation';
+import { withNotation, IWithNotationProps } from '../../../enhancers/withNotation';
+import { Video } from '../../../components/video/Video';
+import { get } from 'lodash';
+import { Fretboard } from '../../../components/fretboard/Fretboard';
+import { Score } from '../../../components/score/Score';
+import styled from 'react-emotion';
 
 type RouteProps = RouteComponentProps<{ id: string }>;
 
+type InnerProps = RouteProps & IWithNotationProps;
+
 const noop = () => null;
 
-const enhance = compose<RouteProps, RouteProps>(
+const enhance = compose<InnerProps, RouteProps>(
   withNotation<RouteProps>(
     props => parseInt(props.match.params.id, 10),
     noop,
@@ -21,13 +28,45 @@ const enhance = compose<RouteProps, RouteProps>(
   )
 );
 
-export const NotationStudio = enhance(() => (
+const VideoWrapper = styled('div')`
+  margin: 0 auto;
+
+  iframe {
+    width: 100%;
+  }
+`;
+
+const Box = styled('div')`
+  width: 860px;
+  height: 860px;
+  margin: 0 auto;
+`;
+
+export const NotationStudio = enhance(props => (
   <Lane
     withTopMargin={true}
     withPadding={true}
   >
-    <div>Video</div>
-    <div>Fretboard</div>
-    <div>Notation</div>
+    <Box>
+      <VideoWrapper>
+        <Video
+          kind={get(props.notation.video, 'kind', '')}
+          src={get(props.notation.video, 'src', '')}
+        />
+      </VideoWrapper>
+      <Fretboard numFrets={16} />
+      <Score
+        caret={true}
+        scrollOffset={100 + 200 + 200}
+        deadTimeMs={props.notation.deadTimeMs}
+        songName={props.notation.songName}
+        artistName={props.notation.artistName}
+        transcriberName={get(props.notation.transcriber, 'name', '')}
+        vextabString={props.notation.vextabString}
+        bpm={props.notation.bpm}
+        width={860}
+        fretboardVisible={false}
+      />
+    </Box>
   </Lane>
 ));
