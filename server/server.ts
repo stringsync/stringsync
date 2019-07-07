a0d7871a18bcfbb3e8910a576d4dd6ae1d7078ae
Jared Johnson <jared@jaredjohnson.dev>
2019-07-07 00:14:15 -0400

    Add graphql to the project.

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,36 @@
 import express from 'express';
 import prisma from './prisma';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const rootValue = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 
 const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true,
+  })
+);
 
 app.get('*', async (req, res, next) => {
   const users = await prisma.users();
   res.json(users);
 });
 
-app.listen(8080, () => {
-  console.log('Server start on port 8080');
+app.listen(4000, () => {
+  console.log('Server start on port 4000');
 });
