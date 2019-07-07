ba5599b185306fc4c93bb183b13d113f7da88a9d
Jared Johnson <jared@jaredjohnson.dev>
2019-07-06 23:35:22 -0400

    Remove the dependecy to have prisma on the host machine.

--- server/prisma/generated/prisma-client/index.js
+++ server/prisma/generated/prisma-client/index.js
@@ -1,17 +1,17 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var prisma_lib_1 = require('prisma-client-lib');
-var typeDefs = require('./prisma-schema').typeDefs;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
 
 var models = [
   {
-    name: 'User',
-    embedded: false,
-  },
+    name: "User",
+    embedded: false
+  }
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `http://prisma:4466`,
+  endpoint: `http://prisma:4466/dev`
 });
 exports.prisma = new exports.Prisma();
