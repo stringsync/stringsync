b54cf77444e0d14400e09456dadc863d5f33c162
Jared Johnson <jared@jaredjohnson.dev>
2019-09-11 22:50:37 -0400

    Starts the signup module.

--- web/src/modules/signup/Signup.tsx
+++ web/src/modules/signup/Signup.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Form, Input, Row, Col } from 'antd';
+import styled from 'styled-components';
+
+const StyledRow = styled(Row)`
+  margin-top: 24px;
+`;
 
 interface Props {}
 
 const Signup: React.FC<Props> = (props) => {
-  return <div>Signup</div>;
+  return (
+    <StyledRow type="flex" justify="center" align="middle">
+      <Col xs={14} sm={14} md={12} lg={10} xl={8} xxl={6}>
+        <Form>
+          <Form.Item>
+            <Input />
+          </Form.Item>
+          <Form.Item>
+            <Input />
+          </Form.Item>
+        </Form>
+      </Col>
+    </StyledRow>
+  );
 };
 
 export default Signup;
