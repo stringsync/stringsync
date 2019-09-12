e4dd5ba578587398fbaa4e560551faa0633851e6
Jared Johnson <jared@jaredjohnson.dev>
2019-09-11 20:05:10 -0400

    Creates Landing component.

---
+++ web/src/modules/landing/Landing.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {}
+
+const Landing: React.FC<Props> = (props) => {
+  return <div>Landing</div>;
+};
+
+export default Landing;
--- web/src/modules/routes/Routes.tsx
+++ web/src/modules/routes/Routes.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Library from '../library/Library';
-
+import Landing from '../landing/Landing';
+const Library = React.lazy(() => import('../library/Library'));
 const Signup = React.lazy(() => import('../signup/Signup'));
 const Login = React.lazy(() => import('../login/Login'));
 
 interface Props {}
 
 const Router: React.FC<Props> = (props) => {
-  console.log('Router rerendered');
   return (
     <React.Fragment>
-      <Route path="/" exact component={Library} />
+      <Route path="/" exact component={Landing} />
+      <Route path="/library" component={Library} />
       <Route path="/signup" component={Signup} />
       <Route path="/login" component={Login} />
     </React.Fragment>
