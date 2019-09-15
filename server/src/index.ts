d2c72704bf0fb8fb01a6d9964cb8a9753734f50a
Jared Johnson <jared@jaredjohnson.dev>
2019-09-15 14:04:40 -0400

    Fails attempt at prisma 2.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import { ApolloServer } from 'apollo-server';
 import schema from './graphql/schema';
 
+const PORT = process.env.PORT || 3000;
+
 const server = new ApolloServer({
   schema,
 });
 
-server.listen().then((serverInfo) => {
+server.listen(PORT).then((serverInfo) => {
   console.log(`🚀 Server ready at ${serverInfo.url}`);
 });
