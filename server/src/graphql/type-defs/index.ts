a12a9ce190dbce24d7652c7357770c897570ba4e
Jared Johnson <jared@jaredjohnson.dev>
2019-09-21 13:16:58 -0400

    Fixes Mutation import in type-defs index.

--- server/src/graphql/type-defs/index.ts
+++ server/src/graphql/type-defs/index.ts
@@ -1,11 +1,10 @@
 import Scalars from './Scalars';
+import Mutation from './Mutation';
 import { ITypedef } from 'apollo-server';
 
 import Query from './Query';
 import Book from './Book';
 import User from './User';
-
-import Mutation from './Mutation';
 
 const typeDefs: ITypedef[] = [Scalars, Query, Mutation, Book, User];
 
