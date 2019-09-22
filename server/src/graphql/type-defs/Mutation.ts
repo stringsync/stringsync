4ebb7d6a89d485d306add301c1c16d843fccd352
Jared Johnson <jared@jaredjohnson.dev>
2019-09-22 16:39:18 -0400

    Remove book artifacts from Mutation and Query.

--- server/src/graphql/type-defs/Mutation.ts
+++ server/src/graphql/type-defs/Mutation.ts
@@ -3,6 +3,5 @@ import { gql } from 'apollo-server';
 export default gql`
   type Mutation {
     signup(input: UserInput!): User!
-    addBook(input: BookInput!): Book!
   }
 `;
--- server/src/graphql/type-defs/Query.ts
+++ server/src/graphql/type-defs/Query.ts
@@ -2,7 +2,6 @@ import { gql } from 'apollo-server';
 
 export default gql`
   type Query {
-    books: [Book]
     users: [User]
   }
 `;
