0b344d3fa88269ef16234358aa1b9b64a8855103
Jared Johnson <jared@jaredjohnson.dev>
2019-09-22 16:10:07 -0400

    Remove books from gql schema.

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,20 +1,16 @@
 import { IResolvers } from 'apollo-server';
 
 // Query
-import books from './books';
 import users from './users';
 
 // Mutation
-import addBook from './addBook';
 import signup from './signup';
 
 const resolvers: IResolvers = {
   Query: {
-    books,
     users,
   },
   Mutation: {
-    addBook,
     signup,
   },
 };
--- server/src/graphql/type-defs/index.ts
+++ server/src/graphql/type-defs/index.ts
@@ -3,9 +3,8 @@ import Mutation from './Mutation';
 import { ITypedef } from 'apollo-server';
 
 import Query from './Query';
-import Book from './Book';
 import User from './User';
 
-const typeDefs: ITypedef[] = [Scalar, Query, Mutation, Book, User];
+const typeDefs: ITypedef[] = [Scalar, Query, Mutation, User];
 
 export default typeDefs;
