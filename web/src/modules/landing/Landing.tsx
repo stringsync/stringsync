aca8885d722057fff4d92658e04aa8028afda4f6
Jared Johnson <jared@jaredjohnson.dev>
2019-09-30 20:20:46 -0400

    Rename variable in Landing component.

--- web/src/modules/landing/Landing.tsx
+++ web/src/modules/landing/Landing.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { isMobile as $isMobile } from '../../store/selectors';
+import { isMobile as isMobileSelector } from '../../store/selectors';
 import { useSelector } from 'react-redux';
 
 interface Props {}
 
 const Landing: React.FC<Props> = (props) => {
-  const isMobile = useSelector($isMobile);
+  const isMobile = useSelector(isMobileSelector);
   const msg = isMobile ? 'mobile' : 'not mobile';
   return <div>{msg}</div>;
 };
