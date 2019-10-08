5823c981bb08a08db09fcf0b26343d535929ba25
Jared Johnson <jared@jaredjohnson.dev>
2019-10-08 07:08:29 -0400

    Clean up redux devtools installation in createStore in web.

--- web/src/store/createStore.ts
+++ web/src/store/createStore.ts
@@ -21,17 +21,10 @@ const reducer = combineReducers({
 
 const preloadedState = getPreloadedState();
 
-const isReduxDevtoolsAvailable = () => {
-  return REDUX_DEVTOOLS_KEY in window;
-};
-
 const createStore = () => {
   const apollo = createApolloClient();
   const middlewares = [thunk.withExtraArgument({ apollo })];
-  let reduxDevtools = compose;
-  if (process.env.NODE_ENV !== 'production' && isReduxDevtoolsAvailable()) {
-    reduxDevtools = (window as any)[REDUX_DEVTOOLS_KEY];
-  }
+  let reduxDevtools = (window as any)[REDUX_DEVTOOLS_KEY] || compose;
 
   return doCreateStore(
     reducer,
