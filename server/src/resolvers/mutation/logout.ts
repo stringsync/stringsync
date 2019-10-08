5759c27e424295d368c4d36fcf6814cd2b8f7cc3
Jared Johnson <jared@jaredjohnson.dev>
2019-10-07 21:42:27 -0400

    Remove console.log in logout resolver.

--- server/src/resolvers/mutation/logout.ts
+++ server/src/resolvers/mutation/logout.ts
@@ -3,7 +3,6 @@ import { LogoutPayloadType } from '../types';
 import { JWT_COOKIE_NAME } from '../../util/createJwt';
 
 export const logout: FieldResolver<LogoutPayloadType> = (parent, args, ctx) => {
-  console.log('logging out');
   ctx.res.clearCookie(JWT_COOKIE_NAME);
   return { ok: true };
 };
