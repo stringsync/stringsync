63eb6fbbd9f3f3eab4194fee8c17eaa008dd0aae
Jared Johnson <jared@jaredjohnson.dev>
2019-10-08 22:27:54 -0400

    Fix constant naming in compareRole util.

--- web/src/util/compareRole.ts
+++ web/src/util/compareRole.ts
@@ -1,10 +1,10 @@
 export type Roles = 'student' | 'teacher' | 'admin';
 
-const RoleHiearchy: Roles[] = ['student', 'teacher', 'admin'];
+const ROLE_HIEARCHY: Roles[] = ['student', 'teacher', 'admin'];
 
 const compareRole = (role1: Roles, role2: Roles) => {
-  const ndx1 = RoleHiearchy.indexOf(role1);
-  const ndx2 = RoleHiearchy.indexOf(role2);
+  const ndx1 = ROLE_HIEARCHY.indexOf(role1);
+  const ndx2 = ROLE_HIEARCHY.indexOf(role2);
 
   if (ndx1 < 0 || ndx2 < 0) {
     throw new Error(`can't compare roles: ${role1}, ${role2}`);
