38041d3a1e0c5e64366a1c11805860abac294b0c
Jared Johnson <jared@jaredjohnson.dev>
2019-10-09 21:52:53 -0400

    Fix lint errors.

--- server/src/util/auth-jwt/setAuthJwtCookie.ts
+++ server/src/util/auth-jwt/setAuthJwtCookie.ts
@@ -1,7 +1,7 @@
 import { Response } from 'express';
 import { JWT_MAX_AGE_MS, JWT_COOKIE_NAME } from '.';
 
-export const setAuthJwtCookie = (jwt: string, res: Response) => {
+export const setAuthJwtCookie = (jwt: string, res: Response): void => {
   res.cookie(JWT_COOKIE_NAME, jwt, {
     httpOnly: true,
     maxAge: JWT_MAX_AGE_MS,
