4e56d90e9030a9c22df717641accffc0c4285745
Jared Johnson <jared@jaredjohnson.dev>
2019-10-11 20:29:35 -0400

    Print lint output only on error.

--- .huskyrc.js
+++ .huskyrc.js
@@ -5,9 +5,9 @@ module.exports = {
     'pre-commit':
       'tsc --noEmit --project server/tsconfig.json &&' +
       'tsc --noEmit --project web/tsconfig.json &&' +
-      'yarn run lint --max-warnings 1 &&' +
-      'yarn prettier --write server/src/** &&' +
-      'yarn prettier --write web/src/** &&' +
-      'yarn prettier --write common/**',
+      'yarn run lint --quiet --max-warnings 1 &&' +
+      'yarn prettier --write server/src/**/* &&' +
+      'yarn prettier --write web/src/**/* &&' +
+      'yarn prettier --write common/**/*',
   },
 };
