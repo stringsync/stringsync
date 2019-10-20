08140f0e9a18755c3665050828709244dab1d4e6
Jared Johnson <jared@jaredjohnson.dev>
2019-10-20 08:15:31 -0400

    Get scripts container to migrate then watch files.

--- server/sequelize.config.js
+++ server/sequelize.config.js
@@ -23,4 +23,12 @@ module.exports = {
     host: process.env.DB_HOST,
     port: 5432,
   },
+  migration: {
+    dialect: 'postgres',
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: 5432,
+  },
 };
