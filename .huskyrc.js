f86a0f1354bf99f53ea110636ebba418138c0d02
Jared Johnson <jared@jaredjohnson.dev>
2019-10-20 13:37:03 -0400

    Make yarn precommit hook and use in huskyrc.

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,10 +2,6 @@
 
 module.exports = {
   hooks: {
-    'pre-commit':
-      'yarn tsc --noEmit --project server/tsconfig.json &&' +
-      'yarn tsc --noEmit --project web/tsconfig.json &&' +
-      'ss lint -s &&' +
-      'ss prettier -s -f',
+    'pre-commit': 'yarn precommit',
   },
 };
