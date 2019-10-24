74516539a440069e5b22dbbb75ab93f497f5cd91
Jared Johnson <jared@jaredjohnson.dev>
2019-10-24 06:49:16 -0400

    Test getErrorFormatter.

---
+++ server/src/modules/server/getErrorFormatter.test.ts
@@ -0,0 +1,53 @@
+import { GraphQLError } from 'graphql';
+import { getErrorFormatter } from './getErrorFormatter';
+
+const NONPROD_ENVS = ['development', 'test', 'foobar'];
+const PROD_ENV = 'production';
+const SECRET_SERVER_DETAILS = 'do not want clients to see this in prod';
+const SANITIZED_SERVER_DETAILS = 'something went wrong';
+const NON_INTERNAL_SERVER_ERROR = new GraphQLError(SECRET_SERVER_DETAILS);
+const INTERNAL_SERVER_ERROR = new GraphQLError(
+  SECRET_SERVER_DETAILS,
+  undefined,
+  undefined,
+  undefined,
+  undefined,
+  undefined,
+  { code: 'INTERNAL_SERVER_ERROR' }
+);
+
+test('getErrorFormatter does not sanitize internal errors in nonprod', (done) => {
+  for (const env of NONPROD_ENVS) {
+    const errorFormatter = getErrorFormatter(env);
+    expect(errorFormatter(INTERNAL_SERVER_ERROR).message).toEqual(
+      SECRET_SERVER_DETAILS
+    );
+  }
+  done();
+});
+
+test('getErrorFormatter does not sanitize non internal errors in nonprod', (done) => {
+  for (const env of NONPROD_ENVS) {
+    const errorFormatter = getErrorFormatter(env);
+    expect(errorFormatter(NON_INTERNAL_SERVER_ERROR).message).toEqual(
+      SECRET_SERVER_DETAILS
+    );
+  }
+  done();
+});
+
+test('getErrorFormatter sanitizes internal server errors in prod', (done) => {
+  const errorFormatter = getErrorFormatter(PROD_ENV);
+  expect(errorFormatter(INTERNAL_SERVER_ERROR).message).toEqual(
+    SANITIZED_SERVER_DETAILS
+  );
+  done();
+});
+
+test('getErrorFormatter does not sanitize non internal server errors in prod', (done) => {
+  const errorFormatter = getErrorFormatter(PROD_ENV);
+  expect(errorFormatter(NON_INTERNAL_SERVER_ERROR).message).toEqual(
+    SECRET_SERVER_DETAILS
+  );
+  done();
+});
--- server/src/modules/server/getErrorFormatter.ts
+++ server/src/modules/server/getErrorFormatter.ts
@@ -1,14 +1,14 @@
 import { get } from 'lodash';
 import { GraphQLError } from 'graphql';
 
-const shouldHideErrorDetails = (env: string, e: GraphQLError) => {
+const shouldSanitizeError = (env: string, e: GraphQLError) => {
   const isProduction = env === 'production';
   const isInternalError = get(e, 'extensions.code') === 'INTERNAL_SERVER_ERROR';
   return isProduction && isInternalError;
 };
 
 export const getErrorFormatter = (env: string) => (e: GraphQLError) => {
-  if (shouldHideErrorDetails(env, e)) {
+  if (shouldSanitizeError(env, e)) {
     return { message: 'something went wrong' };
   }
   return e;
