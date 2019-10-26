ef7f424bae0f571775a62ff857e6b75a331d41a7
Jared Johnson <jared@jaredjohnson.dev>
2019-10-26 09:53:05 -0400

    Add getEncryptedPassword test.

---
+++ server/src/modules/encrypted-password/getEncryptedPassword.test.ts
@@ -0,0 +1,24 @@
+import { getEncryptedPassword } from './getEncryptedPassword';
+import bcrypt from 'bcrypt';
+
+const PASSWORD = 'qwerty123456';
+
+test('getEncryptedPassword is not idempotent', async (done) => {
+  const encryptedPassword1 = await getEncryptedPassword(PASSWORD);
+  const encryptedPassword2 = await getEncryptedPassword(PASSWORD);
+
+  expect(encryptedPassword1).not.toBe(encryptedPassword2);
+  done();
+});
+
+test('getEncryptedPassword can be decrypted by bcrypt', async (done) => {
+  const encryptedPassword1 = await getEncryptedPassword(PASSWORD);
+  const encryptedPassword2 = await getEncryptedPassword(PASSWORD);
+
+  const isPassword1 = await bcrypt.compare(PASSWORD, encryptedPassword1);
+  const isPassword2 = await bcrypt.compare(PASSWORD, encryptedPassword2);
+
+  expect(isPassword1).toBe(true);
+  expect(isPassword2).toBe(true);
+  done();
+});
