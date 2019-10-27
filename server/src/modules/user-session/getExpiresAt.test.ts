ff63d184fe0621d73e3bdb49fcbe672d3d19a27b
Jared Johnson <jared@jaredjohnson.dev>
2019-10-27 19:52:43 -0400

    Test getExpiresAt.

---
+++ server/src/modules/user-session/getExpiresAt.test.ts
@@ -0,0 +1,10 @@
+import { getExpiresAt } from './getExpiresAt';
+
+test('adds 14 days to the reference date', (done) => {
+  const from = new Date('2019-01-01');
+  const expected = new Date('2019-01-15').getTime();
+
+  const actual = getExpiresAt(from).getTime();
+  expect(actual).toBe(expected);
+  done();
+});
