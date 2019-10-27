087a2c5e0819c85fe9ac343a32c0a353ecb7c137
Jared Johnson <jared@jaredjohnson.dev>
2019-10-27 16:08:34 -0400

    Test createServer.

---
+++ server/src/modules/server/createServer.test.ts
@@ -0,0 +1,50 @@
+import { ApolloServer } from 'apollo-server';
+import { getRequestContextCreator } from '../request-context';
+import { getErrorFormatter } from './getErrorFormatter';
+import { createServer } from './createServer';
+import { getConfig } from '../config';
+
+jest.mock('apollo-server');
+jest.mock('./getErrorFormatter', () => ({
+  getErrorFormatter: jest.fn(),
+}));
+jest.mock('../request-context', () => ({
+  getRequestContextCreator: jest.fn(),
+}));
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+test('wraps the ApolloServer constructor', (done) => {
+  const config = getConfig(process.env);
+  const db = Symbol('db') as any;
+  const schema = Symbol('schema') as any;
+
+  const apolloServer = { apolloServer: Symbol('apollo-server') };
+  (ApolloServer as jest.Mock).mockImplementationOnce(() => apolloServer);
+
+  const errorFormatter = Symbol('error-formatter');
+  (getErrorFormatter as jest.Mock).mockReturnValueOnce(errorFormatter);
+
+  const requestContextCreator = Symbol('request-context-creator');
+  (getRequestContextCreator as jest.Mock).mockReturnValueOnce(
+    requestContextCreator
+  );
+
+  const server = createServer(db, schema, config);
+
+  expect(ApolloServer).toBeCalledWith({
+    schema,
+    context: requestContextCreator,
+    formatError: errorFormatter,
+    cors: {
+      origin: config.CLIENT_URI,
+      credentials: true,
+    },
+  });
+  expect(getRequestContextCreator).toBeCalledWith(db);
+  expect(getErrorFormatter).toBeCalledWith(config.NODE_ENV);
+  expect(server).toBe(apolloServer);
+  done();
+});
