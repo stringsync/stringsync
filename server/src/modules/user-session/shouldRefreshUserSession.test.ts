3eb296909f58449c9e28af656c9cb307213512b6
Jared Johnson <jared@jaredjohnson.dev>
2019-10-27 20:17:53 -0400

    Test shouldRefreshUserSession.

---
+++ server/src/modules/user-session/shouldRefreshUserSession.test.ts
@@ -0,0 +1,19 @@
+import { shouldRefreshUserSession } from './shouldRefreshUserSession';
+
+test('returns false if difference < 5 minutes', (done) => {
+  const issuedAt = new Date('2019-01-01T00:00:00.000Z');
+  const requestedAt = new Date('2019-01-01T00:04:99.999Z');
+
+  const actual = shouldRefreshUserSession(requestedAt, issuedAt);
+  expect(actual).toBe(false);
+  done();
+});
+
+test('returns true if difference >= 5 minutes', (done) => {
+  const issuedAt = new Date('2019-01-01T00:00:00.000Z');
+  const requestedAt = new Date('2019-01-01T00:05:00.000Z');
+
+  const actual = shouldRefreshUserSession(requestedAt, issuedAt);
+  expect(actual).toBe(true);
+  done();
+});
