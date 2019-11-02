d174d4ab60680de12d5874f9fd91e1480ccd4f9f
Jared Johnson <jared@jaredjohnson.dev>
2019-11-02 14:17:44 -0400

    Test alignOneToOne dedups values and handles missing values.

--- server/src/modules/align/alignOneToOne.test.ts
+++ server/src/modules/align/alignOneToOne.test.ts
@@ -10,28 +10,58 @@ const VAL3 = 'VAL3';
 
 const identity = <T>(x: T) => x;
 const noop: any = () => {};
+const getKey = (value: string) => {
+  switch (value) {
+    case VAL1:
+      return KEY1;
+    case VAL2:
+      return KEY2;
+    case VAL3:
+      return KEY3;
+    default:
+      return '';
+  }
+};
 
 test('aligns keys with values', (done) => {
   const keys = [KEY1, KEY2, KEY3];
   const values = [VAL3, VAL1, VAL2];
 
-  const alignedValues = alignOneToOne(keys, values, {
-    getKey: (value) => {
-      switch (value) {
-        case VAL1:
-          return KEY1;
-        case VAL2:
-          return KEY2;
-        case VAL3:
-          return KEY3;
-        default:
-          return '';
-      }
-    },
+  const actual = alignOneToOne(keys, values, {
+    getKey,
     getUniqueIdentifier: identity,
     getMissingValue: noop,
   });
 
-  expect(alignedValues).toEqual([VAL1, VAL2, VAL3]);
+  expect(actual).toEqual([VAL1, VAL2, VAL3]);
+  done();
+});
+
+test('dedups values', (done) => {
+  const keys = [KEY1, KEY2, KEY3];
+  const values = [VAL3, VAL1, VAL2, VAL1, VAL2];
+
+  const actual = alignOneToOne(keys, values, {
+    getKey,
+    getUniqueIdentifier: identity,
+    getMissingValue: noop,
+  });
+
+  expect(actual).toEqual([VAL1, VAL2, VAL3]);
+  done();
+});
+
+test('handles missing values', (done) => {
+  const keys = [KEY1, KEY2, KEY3];
+  const values = [VAL3, VAL1];
+  const getMissingValue = () => Symbol.for(VAL2);
+
+  const actual = alignOneToOne(keys, values, {
+    getKey,
+    getUniqueIdentifier: identity,
+    getMissingValue,
+  });
+
+  expect(actual).toEqual([VAL1, Symbol.for(VAL2), VAL3]);
   done();
 });
