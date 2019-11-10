6c51b7268ce2d73477b2c786e3aacb21bf878946
Jared Johnson <jared@jaredjohnson.dev>
2019-11-10 15:31:57 -0500

    Remove unecessary typedef in batchGetNotationsFromUserIds.

--- server/src/data-loaders/notations/batchGetNotationsFromUserIds.ts
+++ server/src/data-loaders/notations/batchGetNotationsFromUserIds.ts
@@ -7,7 +7,7 @@ export const batchGetNotationsFromUserIds = (db: Db) => async (
   userIds: string[]
 ) => {
   // userId can have many notations
-  const notations = userIds.map<Notation[]>((userId) => {
+  const notations = userIds.map((userId) => {
     return [{ id: userId }];
   });
   return alignOneToMany(userIds, flatten(notations), {
