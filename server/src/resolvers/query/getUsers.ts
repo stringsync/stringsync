e4603eaba4032b1122a4c46df9b5bfa008aa07df
Jared Johnson <jared@jaredjohnson.dev>
2019-11-10 15:33:36 -0500

    Map Users to POJOs in getUsers resolver.

--- server/src/resolvers/query/getUsers.ts
+++ server/src/resolvers/query/getUsers.ts
@@ -1,8 +1,10 @@
 import { FieldResolver } from '..';
 import { User } from 'common/types';
 import { guardAuthenticated } from '../../guards';
+import { toUserPojo } from '../../db';
 
 export const getUsers: FieldResolver<User[]> = async (parent, args, ctx) => {
   guardAuthenticated(ctx);
-  return await ctx.db.models.User.findAll();
+  const rawUsers = await ctx.db.models.User.findAll({ raw: true });
+  return rawUsers.map(toUserPojo);
 };
