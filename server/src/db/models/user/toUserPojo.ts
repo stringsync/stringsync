ec9c553dbd58b3a33a1012b6710717e42b6858b4
Jared Johnson <jared@jaredjohnson.dev>
2019-11-10 15:29:12 -0500

    Create toUserPojo.

---
+++ server/src/db/models/user/toUserPojo.ts
@@ -0,0 +1,14 @@
+import { User } from 'common/types';
+
+type UserLike = {
+  [P in keyof User]: User[P];
+};
+
+export const toUserPojo = (userLike: UserLike): User => ({
+  id: userLike.id,
+  createdAt: userLike.createdAt,
+  email: userLike.email,
+  role: userLike.role,
+  updatedAt: userLike.updatedAt,
+  username: userLike.username,
+});
