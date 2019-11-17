1786947ffe51c780c0b767eeb7365dff8fc15632
Jared Johnson <jared@jaredjohnson.dev>
2019-11-17 10:01:01 -0500

    Return raw users in getUser and getUsers.

--- server/src/db/models/user/getUser.ts
+++ server/src/db/models/user/getUser.ts
@@ -1,15 +1,8 @@
 import { Db } from '../../types';
-import { toUserPojo } from './toUserPojo';
 
 export const getUser = async (db: Db, id: string) => {
   if (!id) {
     return null;
   }
-
-  const user = await db.models.User.findOne({ where: { id } });
-  if (!user) {
-    return null;
-  }
-
-  return toUserPojo(user);
+  return await db.models.User.findOne({ raw: true, where: { id } });
 };
--- server/src/db/models/user/getUsers.ts
+++ server/src/db/models/user/getUsers.ts
@@ -1,7 +1,5 @@
-import { toUserPojo } from '../..';
 import { Db } from '../../types';
 
 export const getUsers = async (db: Db) => {
-  const users = await db.models.User.findAll({ raw: true });
-  return users.map(toUserPojo);
+  return await db.models.User.findAll({ raw: true });
 };
