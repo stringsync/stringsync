1796bfab55a36d1dae7466e2a39aabebc2291cbf
Jared Johnson <jared@jaredjohnson.dev>
2019-11-17 09:51:02 -0500

    Refactor getUserResolver test to be less prescriptive.

--- server/src/resolvers/query/getUserResolver.test.ts
+++ server/src/resolvers/query/getUserResolver.test.ts
@@ -1,21 +1,30 @@
 import { getUserResolver } from './getUserResolver';
 import { GetUserInput } from 'common/types';
-import { getTestCtxProvider } from '../../testing';
+import { getTestCtxProvider, getUserFixtures } from '../../testing';
+
+const USER_FIXTURES = getUserFixtures();
+const USER1 = USER_FIXTURES.student1;
+const USER2 = USER_FIXTURES.student2;
 
 const provideTestCtx = getTestCtxProvider();
 
 it(
-  'loads the user from the dataLoader',
-  provideTestCtx({}, async (ctx) => {
-    const mockLoad = jest
-      .fn()
-      .mockImplementationOnce((key: string) => Symbol.for(key));
-    ctx.dataLoaders.usersById.load = mockLoad;
-
-    const input: GetUserInput = { id: 'fake-id' };
+  'returns the user from the db that matches the id',
+  provideTestCtx({ User: [USER1, USER2] }, async (ctx) => {
+    const input: GetUserInput = { id: USER1.id };
     const user = await getUserResolver(undefined, { input }, ctx);
 
-    expect(mockLoad).toBeCalledTimes(1);
-    expect(user).toBe(Symbol.for('fake-id'));
+    expect(user).not.toBeNull();
+    expect(user!.id).toBe(USER1.id);
+  })
+);
+
+it(
+  'returns null if the user does not exist',
+  provideTestCtx({}, async (ctx) => {
+    const input: GetUserInput = { id: USER1.id };
+    const user = await getUserResolver(undefined, { input }, ctx);
+
+    expect(user).toBeNull();
   })
 );
