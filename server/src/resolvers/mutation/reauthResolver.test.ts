c88d4502778a2d08cc5101f7f71fae9a341eca83
Jared Johnson <jared@jaredjohnson.dev>
2019-12-01 17:40:02 -0500

    Test reauthResolver throws an error when user session does not exist.

--- server/src/resolvers/mutation/reauthResolver.test.ts
+++ server/src/resolvers/mutation/reauthResolver.test.ts
@@ -1,5 +1,10 @@
 import { reauthResolver } from './reauthResolver';
-import { getTestCtxProvider } from '../../testing';
+import { getTestCtxProvider, getFixtures } from '../../testing';
+import { getRawUserByEmailOrUsername } from '../../db';
+
+const FIXTURES = getFixtures();
+const USER = FIXTURES.User.student1;
+const USER_SESSION = FIXTURES.UserSession.student1Session;
 
 const provideTestCtx = getTestCtxProvider();
 
@@ -27,6 +32,21 @@ it(
   'throws an error when no token',
   provideTestCtx({}, async (ctx) => {
     ctx.auth.token = '';
+    await expect(reauthResolver(undefined, {}, ctx)).rejects.toThrowError(
+      'invalid or expired credentials'
+    );
+  })
+);
+
+it(
+  'throws an error when user session does not exist',
+  provideTestCtx({ User: [USER] }, async (ctx) => {
+    const user = await getRawUserByEmailOrUsername(ctx.db, USER.username);
+
+    ctx.auth.isLoggedIn = true;
+    ctx.auth.user = user;
+    ctx.auth.token = USER_SESSION.token;
+
     await expect(reauthResolver(undefined, {}, ctx)).rejects.toThrowError(
       'invalid or expired credentials'
     );
