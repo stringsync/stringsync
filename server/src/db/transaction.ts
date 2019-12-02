69c06c7939bd7606c2a0e759793a0e03de4853eb
Jared Johnson <jared@jaredjohnson.dev>
2019-12-02 07:18:38 -0500

    Create transaction utility.

---
+++ server/src/db/transaction.ts
@@ -0,0 +1,14 @@
+import { TRANSACTION_NAMESPACE } from './constants';
+import { Db } from './types';
+import { Transaction } from 'sequelize';
+
+type AutoCallback<T> = (t: Transaction) => PromiseLike<T>;
+
+/**
+ * Utility method that flattens transactions. When a transaction
+ * already exists, it uses it. Otherwise, it creates a new transaction.
+ */
+export const transaction = <T>(db: Db, task: AutoCallback<T>) => {
+  const currTransaction = TRANSACTION_NAMESPACE.get('transaction');
+  return currTransaction ? task(currTransaction) : db.transaction(task);
+};
