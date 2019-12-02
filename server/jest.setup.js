5d156ccb2e97c8b777e716bdd8930b6ec3079043
Jared Johnson <jared@jaredjohnson.dev>
2019-12-02 07:32:25 -0500

    Add retry logic to server jest setup.

--- server/jest.setup.js
+++ server/jest.setup.js
@@ -1,13 +1,26 @@
 const { execSync } = require('child_process');
 
-module.exports = async () => {
+const MAX_NUM_RETRIES = 5;
+
+const resetDb = async (numRetries = 0) => {
   const env = process.env.NODE_ENV;
   if (env !== 'test') {
     throw new Error(`cannot run jest setup in NODE_ENV=${env || ''}`);
   }
 
   // `yarn tsc:db` is called in pretest script
-  execSync('yarn sequelize db:drop');
-  execSync('yarn sequelize db:create');
-  execSync('yarn sequelize db:migrate');
+  try {
+    execSync('yarn sequelize db:drop');
+    execSync('yarn sequelize db:create');
+    execSync('yarn sequelize db:migrate');
+  } catch (e) {
+    if (numRetries >= MAX_NUM_RETRIES) {
+      throw e;
+    }
+    console.error(e);
+    await setTimeout(() => {}, 1000);
+    resetDb(numRetries + 1);
+  }
 };
+
+module.exports = resetDb;
