0229e39b96a2b1b17764761349a4775211ef1473
Jared Johnson <jared@jaredjohnson.dev>
2019-12-07 22:12:24 -0500

    Test signupResolver return value and persistence behavior.

--- server/src/resolvers/mutation/signupResolver.test.ts
+++ server/src/resolvers/mutation/signupResolver.test.ts
@@ -2,6 +2,53 @@ import { signupResolver } from './signupResolver';
 import { useTestCtx } from '../../testing';
 import { SignupInput } from 'common/types';
 import { UserInputError } from 'apollo-server';
+import { isPassword } from '../../password';
+
+it(
+  'returns the newly created user',
+  useTestCtx({}, {}, async (ctx) => {
+    const email = 'foo@gmail.com';
+    const password = 'password';
+    const username = 'foo';
+
+    const input: SignupInput = {
+      email,
+      password,
+      username,
+    };
+
+    const { user } = await signupResolver(undefined, { input }, ctx);
+    expect(user.email).toBe(email);
+    expect(user.username).toBe(username);
+  })
+);
+
+it(
+  'creates user in the db',
+  useTestCtx({}, {}, async (ctx) => {
+    const email = 'foo@gmail.com';
+    const password = 'password';
+    const username = 'foo';
+
+    const input: SignupInput = {
+      email,
+      password,
+      username,
+    };
+
+    const { user } = await signupResolver(undefined, { input }, ctx);
+
+    const userModel = await ctx.db.models.User.findByPk(user.id);
+    expect(userModel).not.toBeNull();
+    const isExpectedPassword = await isPassword(
+      password,
+      userModel!.encryptedPassword
+    );
+    expect(isExpectedPassword).toBe(true);
+    expect(userModel!.email).toBe(email);
+    expect(userModel!.username).toBe(username);
+  })
+);
 
 it(
   'transforms validation errors to UserInputErrors',
