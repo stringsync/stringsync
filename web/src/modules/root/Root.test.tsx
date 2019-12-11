c2c45d620e2c2bad8882c536ad024e9c464d6926
Jared Johnson <jared@jaredjohnson.dev>
2019-12-10 21:49:24 -0500

    Update packages to accommodate jest testing in the web project.

--- web/src/modules/root/Root.test.tsx
+++ web/src/modules/root/Root.test.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Root from './Root';
 import createStore from '../../store/createStore';
+import { render } from '@testing-library/react';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
   const store = createStore();
-  ReactDOM.render(<Root store={store} />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const { container } = render(<Root store={store} />);
+  expect(container).toBeInTheDocument();
 });
