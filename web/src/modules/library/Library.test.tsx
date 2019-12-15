e62faeb1dccfbb7c8fa09c8225eee5fc656e3ccc
Jared Johnson <jared@jaredjohnson.dev>
2019-12-14 19:14:00 -0500

    Simplify Library test.

--- web/src/modules/library/Library.test.tsx
+++ web/src/modules/library/Library.test.tsx
@@ -3,13 +3,21 @@ import Library from './Library';
 import createStore from '../../store/createStore';
 import Root from '../root/Root';
 import { render } from '@testing-library/react';
+import { Store } from '../../store';
 
-it('renders without crashing', () => {
-  const store = createStore();
-  const { container } = render(
+let store: Store;
+let component: JSX.Element;
+
+beforeEach(() => {
+  store = createStore();
+  component = (
     <Root store={store}>
       <Library />
     </Root>
   );
+});
+
+it('renders without crashing', () => {
+  const { container } = render(component);
   expect(container).toBeInTheDocument();
 });
