b66c20fc24e4e947e44ede88afecdc125d44c6a7
Jared Johnson <jared@jaredjohnson.dev>
2019-12-25 17:40:22 -0500

    Test Menu shows a signup button when logged out.

--- web/src/hocs/with-layout/Menu.test.tsx
+++ web/src/hocs/with-layout/Menu.test.tsx
@@ -15,6 +15,30 @@ it('shows a login button when logged out', () => {
     }
   );
 
-  const { getByText } = render(<TestComponent />);
+  const { container, getByText } = render(<TestComponent />);
+
   expect(getByText('login')).toBeInTheDocument();
+  expect(
+    container.querySelectorAll<HTMLAnchorElement>('a[href="/login"]').length
+  ).toBeGreaterThanOrEqual(1);
+});
+
+it('shows a signup button when logged out', () => {
+  const { TestComponent } = getTestComponent(
+    Menu,
+    {},
+    {
+      auth: {
+        isLoggedIn: false,
+        isPending: false,
+      },
+    }
+  );
+
+  const { container, getByText } = render(<TestComponent />);
+
+  expect(getByText('signup')).toBeInTheDocument();
+  expect(
+    container.querySelectorAll<HTMLAnchorElement>('a[href="/signup"]').length
+  ).toBeGreaterThanOrEqual(1);
 });
