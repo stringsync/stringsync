37a3298740928f99ae8e4600b1dd69b2bf0f09e0
Jared Johnson <jared@jaredjohnson.dev>
2019-12-29 18:37:44 -0500

    Test Signup has useful links and has required fields.

--- web/src/modules/signup/Signup.test.tsx
+++ web/src/modules/signup/Signup.test.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { getTestComponent } from '../../testing';
 import Signup from './Signup';
 import { render } from '@testing-library/react';
+import { getTestComponent } from '../../testing';
 
 it('renders without crashing', () => {
   const { TestComponent } = getTestComponent(Signup, {});
   const { container } = render(<TestComponent />);
   expect(container).toBeInTheDocument();
 });
+
+it.each(['/library', '/login'])('has useful links', (href) => {
+  const { TestComponent } = getTestComponent(Signup, {});
+  const { container } = render(<TestComponent />);
+
+  expect(
+    container.querySelectorAll<HTMLAnchorElement>(`a[href="${href}"]`).length
+  ).toBeGreaterThan(0);
+});
+
+it.each(['email', 'username', 'password'])(
+  'has required fields',
+  (placeholderText) => {
+    const { TestComponent } = getTestComponent(Signup, {});
+    const { getByPlaceholderText } = render(<TestComponent />);
+
+    expect(getByPlaceholderText(placeholderText)).toBeInTheDocument();
+  }
+);
