5eeca9284624180c55f4906e929aa4b5a7897326
Jared Johnson <jared@jaredjohnson.dev>
2020-01-01 11:24:28 -0500

    Test server compareUserRoles.

---
+++ server/src/util/compareUserRoles.test.ts
@@ -0,0 +1,57 @@
+import { compareUserRoles } from './compareUserRoles';
+import { UserRoles } from 'common/types';
+
+it.each([
+  {
+    role1: 'student',
+    role2: 'student',
+    expectation: 0,
+  },
+  {
+    role1: 'student',
+    role2: 'teacher',
+    expectation: -1,
+  },
+  {
+    role1: 'student',
+    role2: 'admin',
+    expectation: -1,
+  },
+  {
+    role1: 'teacher',
+    role2: 'student',
+    expectation: 1,
+  },
+  {
+    role1: 'teacher',
+    role2: 'teacher',
+    expectation: 0,
+  },
+  {
+    role1: 'teacher',
+    role2: 'admin',
+    expectation: -1,
+  },
+  {
+    role1: 'admin',
+    role2: 'student',
+    expectation: 1,
+  },
+  {
+    role1: 'admin',
+    role2: 'teacher',
+    expectation: 1,
+  },
+  {
+    role1: 'admin',
+    role2: 'admin',
+    expectation: 0,
+  },
+] as Array<{
+  role1: UserRoles;
+  role2: UserRoles;
+  expectation: -1 | 0 | 1;
+}>)('compares roles', ({ role1, role2, expectation }) => {
+  const comparison = compareUserRoles(role1, role2);
+  expect(comparison).toBe(expectation);
+});
