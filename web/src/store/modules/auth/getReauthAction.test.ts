6be56938bd9a9e4ea083d341e902be0448957657
Jared Johnson <jared@jaredjohnson.dev>
2019-12-31 19:05:18 -0500

    Test getReauthAction sets the user from the response.

--- web/src/store/modules/auth/getReauthAction.test.ts
+++ web/src/store/modules/auth/getReauthAction.test.ts
@@ -1,15 +1,35 @@
 import { getReauthAction } from './getReauthAction';
 import { getTestStore } from '../../../testing';
 import { REQUEST_AUTH_PENDING } from './constants';
+import { AuthUser } from './types';
 
-it('dispatches an auth pending action', () => {
+it('sets the user from the response', async () => {
+  const { store, apollo } = getTestStore();
+  const user: AuthUser = {
+    id: 'id',
+    username: 'username',
+    email: 'email',
+    role: 'teacher',
+  };
+  jest.spyOn(apollo, 'mutate').mockImplementation(async () => ({
+    data: { reauth: { user } },
+  }));
+
+  await getReauthAction()(store.dispatch, store.getState, { apollo });
+
+  const { auth } = store.getState();
+  expect(auth.user).toEqual(user);
+  expect(auth.isLoggedIn).toBe(true);
+  expect(auth.isPending).toBe(false);
+});
+
+it('dispatches an auth pending action', async () => {
   const { store, apollo } = getTestStore();
   const dispatchSpy = jest.spyOn(store, 'dispatch');
-  const action = getReauthAction();
+  jest.spyOn(apollo, 'mutate').mockImplementation(async () => undefined);
 
-  action(store.dispatch, store.getState, { apollo });
+  await getReauthAction()(store.dispatch, store.getState, { apollo });
 
-  expect(dispatchSpy).toHaveBeenCalledTimes(1);
   expect(dispatchSpy).toHaveBeenCalledWith({
     type: REQUEST_AUTH_PENDING,
   });
