74b972c8f73eacd98551030f4c3c2567839e17ae
Jared Johnson <jared@jaredjohnson.dev>
2020-01-01 11:08:53 -0500

    Test getStore preloads the state.

--- web/src/store/getStore.test.ts
+++ web/src/store/getStore.test.ts
@@ -1,5 +1,17 @@
 import { getStore } from './getStore';
+import { getPreloadedState } from './getPreloadedState';
+import { createApolloClient } from '../util';
 
 it('runs without crashing', () => {
   expect(getStore).not.toThrow();
+});
+
+it('preloads the state', () => {
+  const apollo = createApolloClient();
+  const store = getStore(apollo);
+
+  const state = store.getState();
+  const preloadedState = getPreloadedState();
+
+  expect(state).toEqual(preloadedState);
 });
