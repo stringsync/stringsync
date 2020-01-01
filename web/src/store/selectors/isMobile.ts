4503837c11bcaa07428cf1ab59939f58523c9cff
Jared Johnson <jared@jaredjohnson.dev>
2020-01-01 11:00:19 -0500

    Test isMobile returns if the device is mobile.

--- web/src/store/selectors/isMobile.ts
+++ web/src/store/selectors/isMobile.ts
@@ -1,5 +1,5 @@
 import { RootState } from '../';
 
 export const isMobile = (state: RootState): boolean => {
-  return state.device.device.phone || state.device.device.tablet;
+  return state.device.device.mobile;
 };
