ed39ce487b3d2dcc6872bd4cb05cd93fc7513f39
Jared Johnson <jared@jaredjohnson.dev>
2020-02-08 21:04:03 -0500

    Add comment to useTestReqCtx.

--- server/src/testing/useTestReqCtx.ts
+++ server/src/testing/useTestReqCtx.ts
@@ -4,6 +4,10 @@ import { getMockExpressContext } from './getMockExpressContext';
 import { getReqCtxCreator } from '../ctx';
 import { useTestGlobalCtx } from './useTestGlobalCtx';
 
+/**
+ * The canonical way of creating a request context. All
+ * changes will be rolled back after the test is done.
+ */
 export const useTestReqCtx = <A extends any[]>(
   patch: ReqCtxPatch,
   callback: CtxCallback<A>
