7c0fc5cdcf8f00018c11935376c8ae5e71431bf8
Jared Johnson <jared@jaredjohnson.dev>
2020-02-09 08:21:57 -0500

    Test production branch of getTransporter.

--- server/src/jobs/processors/mail/getTransporter.test.ts
+++ server/src/jobs/processors/mail/getTransporter.test.ts
@@ -3,6 +3,13 @@ import { useTestGlobalCtx } from '../../../testing';
 
 it.each(['test', 'development'])('runs without crashing', (nodeEnv) => {
   useTestGlobalCtx({ config: { NODE_ENV: nodeEnv } }, async (ctx) => {
-    await expect(() => getTransporter(ctx)).not.toThrow();
+    await expect(() => getTransporter(ctx)).resolves.not.toThrow();
   });
 });
+
+it(
+  'throws an error for production',
+  useTestGlobalCtx({ config: { NODE_ENV: 'production' } }, async (ctx) => {
+    await expect(() => getTransporter(ctx)).rejects.toThrow();
+  })
+);
