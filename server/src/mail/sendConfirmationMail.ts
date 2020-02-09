5a905e6b98e525e35d8685c71b26e9a5174f4fe6
Jared Johnson <jared@jaredjohnson.dev>
2020-02-09 12:36:17 -0500

    Create mail module with sendConfirmationMail function.

---
+++ server/src/mail/sendConfirmationMail.ts
@@ -0,0 +1,26 @@
+import { GlobalCtx } from '../ctx';
+import { CONFIRMATION_MAIL } from './constants';
+import url from 'url';
+
+export const sendConfirmationMail = async (
+  email: string,
+  confirmationToken: string,
+  ctx: GlobalCtx
+) => {
+  const confirmHref = url.format({
+    protocol: 'http',
+    hostname: ctx.config.WEB_URI,
+    pathname: 'confirm',
+    query: { confirmationToken },
+  });
+
+  await ctx.queues.MAIL.add(CONFIRMATION_MAIL, {
+    from: 'StringSync <info@stringsync.com>',
+    to: email,
+    html: `
+      <p>
+        Please confirm your email for <a href=${confirmHref}>StringSync</a>.
+      </p>
+    `,
+  });
+};
