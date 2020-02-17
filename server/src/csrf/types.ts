d3e3b3d55889c22084a00be8230f4c8ced4d6a75
Jared Johnson <jared@jaredjohnson.dev>
2020-02-16 19:10:50 -0500

    Implement getCsrfToken resolver.

---
+++ server/src/csrf/types.ts
@@ -0,0 +1,4 @@
+export interface CsrfTokenPayload {
+  sessionToken: string;
+  issuedAt: Date;
+}
--- server/src/resolvers/query/get-csrf-token/getCsrfToken.ts
+++ server/src/resolvers/query/get-csrf-token/getCsrfToken.ts
@@ -1,4 +1,6 @@
 import { ReqCtx } from '../../../ctx';
+import jwt from 'jsonwebtoken';
+import { CsrfTokenPayload } from '../../../csrf';
 
 interface Args {}
 
@@ -7,5 +9,14 @@ export const getCsrfToken = (
   args: Args,
   ctx: ReqCtx
 ): string => {
-  return 'csrftoken';
+  const payload: CsrfTokenPayload = {
+    sessionToken: '',
+    issuedAt: ctx.requestedAt,
+  };
+
+  if (ctx.auth.isLoggedIn) {
+    payload.sessionToken = ctx.auth.token;
+  }
+
+  return jwt.sign(JSON.stringify(payload), ctx.config.CSRF_SECRET);
 };
