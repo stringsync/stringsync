cc7d252d6544d7924226fc9b89d35b99f8888532
Jared Johnson <jared@jaredjohnson.dev>
2020-03-08 16:26:04 -0400

    Cleanup ss up command and remove attached option.

--- bin/scripts/src/commands/up.ts
+++ bin/scripts/src/commands/up.ts
@@ -1,33 +1,30 @@
 import { Command, flags } from '@oclif/command';
-import {
-  ROOT_PATH,
-  getBuildDockerImageCmd,
-  cmd,
-  getDockerComposeCmd,
-  PROJECT_ARG,
-  execSyncFromRootPath,
-} from '../util';
+import { ROOT_PATH, PROJECT_ARG, getDockerComposeFile } from '../util';
+import { spawn } from 'child_process';
 
 export default class Up extends Command {
   static description = 'Spins up a development environment';
 
   static flags = {
     help: flags.help({ char: 'h' }),
-    attach: flags.boolean({ char: 'a' }),
   };
 
   static args = [PROJECT_ARG];
 
   async run() {
-    const { flags, args } = this.parse(Up);
+    const { args } = this.parse(Up);
 
-    execSyncFromRootPath(
-      cmd(
-        getDockerComposeCmd(args.project),
+    spawn(
+      'docker-compose',
+      [
+        '-f',
+        getDockerComposeFile(args.project),
+        '-p',
+        args.project,
         'up',
         '--build',
-        flags.attach ? '' : '-d'
-      )
+      ],
+      { cwd: ROOT_PATH, stdio: 'inherit' }
     );
   }
 }
