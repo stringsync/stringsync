2fe32ea7207e05cdf290e6207b7f74740901561d
Jared Johnson <jared@jaredjohnson.dev>
2020-03-07 21:52:35 -0500

    Remove uneeded props definition.

--- web/src/pages/App/App.tsx
+++ web/src/pages/App/App.tsx
@@ -3,9 +3,7 @@ import { StoreViewportSync } from '../../components/StoreViewportSync';
 import { AuthenticateOnce } from '../../components/AuthenticateOnce';
 import { Routes } from '../../routes/Routes';
 
-interface Props {}
-
-const App: React.FC<Props> = (props) => {
+const App: React.FC = () => {
   return (
     <>
       <StoreViewportSync />
