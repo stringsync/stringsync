cf1132e59f1fde39f8c7e9811f1ff40e38eb453f
Jared Johnson <jared@jaredjohnson.dev>
2020-03-29 20:36:40 -0400

    Revert "Fail server tests purposely."
    This reverts commit ae9e6dddad2e44c6a3f3e6077d0df4d42da0af40.

--- server/src/password/getEncryptedPassword.test.ts
+++ server/src/password/getEncryptedPassword.test.ts
@@ -3,7 +3,6 @@ import { getEncryptedPassword } from './getEncryptedPassword';
 const PASSWORD = 'password';
 
 it('is not idempotent', async () => {
-  fail();
   const encryptedPassword1 = await getEncryptedPassword(PASSWORD);
   const encryptedPassword2 = await getEncryptedPassword(PASSWORD);
 
