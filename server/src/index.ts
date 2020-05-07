d08c0d900fbf5849e0116f62203438091d00bc6b
Jared Johnson <jared@jaredjohnson.dev>
2020-05-07 19:38:44 -0400

    Update dummy to return the time of the db and redis.

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,25 @@ import { getServer } from './server';
 import { getConfig, Config } from './config';
 import { createWorkers } from './jobs';
 import express from 'express';
+import { get } from 'lodash';
 
 const dummy = async (config: Config): Promise<void> => {
+  const ctx = createGlobalCtx(config);
   const app = express();
-  app.get('/', (req, res) => res.send('Hello World!'));
+
+  app.get('/', async (req, res) => {
+    // db time
+    const entries = await (await ctx.db.query('SELECT NOW();')).entries();
+    const dbNow = new Date(get(entries.next().value, '[1][0].now'));
+
+    // redis time
+    const redisTimes = await ctx.redis.time();
+    const redisUs = parseInt(redisTimes[0], 10) * 1000;
+    const redisNow = new Date(redisUs);
+
+    res.send(`dbNow: ${dbNow}\nredisNow: ${redisNow}`);
+  });
+
   app.listen(config.PORT, () => {
     console.log(`App running at http://localhost:${config.PORT}`);
   });
