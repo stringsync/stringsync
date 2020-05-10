92d20bfac5bef951965b7a9f410c86208b80ea8f
Jared Johnson <jared@jaredjohnson.dev>
2020-05-09 21:22:56 -0400

    Remove dummy server role.

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,30 +3,6 @@ import { createGlobalCtx } from './ctx';
 import { getServer } from './server';
 import { getConfig, Config } from './config';
 import { createWorkers } from './jobs';
-import express from 'express';
-import { get } from 'lodash';
-
-const dummy = async (config: Config): Promise<void> => {
-  const ctx = createGlobalCtx(config);
-  const app = express();
-
-  app.get('/', async (req, res) => {
-    // db time
-    const entries = await (await ctx.db.query('SELECT NOW();')).entries();
-    const dbNow = new Date(get(entries.next().value, '[1][0].now'));
-
-    // redis time
-    const redisTimes = await ctx.redis.time();
-    const redisUs = parseInt(redisTimes[0], 10) * 1000;
-    const redisNow = new Date(redisUs);
-
-    res.send(`TEST123, dbNow: ${dbNow}\nredisNow: ${redisNow}`);
-  });
-
-  app.listen(config.PORT, () => {
-    console.log(`App running at http://localhost:${config.PORT}`);
-  });
-};
 
 const server = async (config: Config): Promise<void> => {
   const ctx = createGlobalCtx(config);
@@ -49,9 +25,6 @@ if (require.main === module) {
       break;
     case 'worker':
       worker(config);
-      break;
-    case 'dummy':
-      dummy(config);
       break;
     default:
       throw new TypeError(`config.ROLE not supported: ${config.ROLE}`);
