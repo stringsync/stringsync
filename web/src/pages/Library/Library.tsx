81713893a9693ea81a91c4d192e6f898ede5051f
Jared Johnson <jared@jaredjohnson.dev>
2020-05-19 12:02:06 -0400

    Query hello from frontend.

--- web/src/pages/Library/Library.tsx
+++ web/src/pages/Library/Library.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { compose } from '../../common';
 import { Layouts, withLayout } from '../../hocs';
+import { useEffectOnce } from '../../hooks';
 
 interface Props {}
 
 const enhance = compose(withLayout(Layouts.DEFAULT));
 
 const Library: React.FC<Props> = enhance(() => {
-  return <div data-testid="library">Library</div>;
+  const [result, setResult] = useState({});
+  useEffectOnce(() => {
+    fetch('http://localhost:3000/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: `{ hello }`,
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => setResult(json));
+  });
+
+  return <pre data-testid="library">{JSON.stringify(result, null, 2)}</pre>;
 });
 
 export default Library;
