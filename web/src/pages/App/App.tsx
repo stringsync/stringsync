61d2ae1b3cfeed0b52a78598d400d4e82d216c81
Jared Johnson <jared@jaredjohnson.dev>
2020-05-19 11:50:32 -0400

    Do not authenticate on frontend.

--- web/src/pages/App/App.tsx
+++ web/src/pages/App/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StoreViewportSync } from '../../components/StoreViewportSync';
-import { AuthenticateOnce } from '../../components/AuthenticateOnce';
+// import { AuthenticateOnce } from '../../components/AuthenticateOnce';
 import { Routes } from '../../routes/Routes';
 
 const App: React.FC = () => {
   return (
     <>
       <StoreViewportSync />
-      <AuthenticateOnce />
+      {/*<AuthenticateOnce />*/}
       <Routes />
     </>
   );
