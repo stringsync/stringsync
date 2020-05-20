2bb689031978e397234affadc602f591749244c1
Jared Johnson <jared@jaredjohnson.dev>
2020-05-20 10:19:27 -0400

    Rewrite schema using graphql types instead of graphql language.

--- server/src/resolvers/getSchema.ts
+++ server/src/resolvers/getSchema.ts
@@ -6,13 +6,44 @@ import {
   Kind,
   GraphQLEnumType,
   GraphQLNonNull,
+  GraphQLList,
+  GraphQLInputObjectType,
+  GraphQLOutputType,
+  GraphQLInt,
 } from 'graphql';
 import { GlobalCtx } from '../util/ctx';
 
+/* eslint-disable prefer-const */
+// We use `let` declarations at the beginning of the function so we
+// can use thunks to reference the types independently of
+// declaration order.
+
 export const getSchema = () => {
-  const dateType = new GraphQLScalarType({
+  let dateType: GraphQLScalarType;
+  let userRolesType: GraphQLEnumType;
+  let userType: GraphQLObjectType;
+  let notationType: GraphQLObjectType;
+  let userInputType: GraphQLInputObjectType;
+  let userOutputType: GraphQLObjectType;
+  let usersInputType: GraphQLInputObjectType;
+  let usersOutputType: GraphQLOutputType;
+  let authenticateInputType: GraphQLInputObjectType;
+  let authenticateOutputType: GraphQLOutputType;
+  let signupInputType: GraphQLInputObjectType;
+  let signupOutputType: GraphQLOutputType;
+  let loginInputType: GraphQLInputObjectType;
+  let loginOutputType: GraphQLOutputType;
+  let logoutInputType: GraphQLInputObjectType;
+  let logoutOutputType: GraphQLOutputType;
+  let confirmEmailInputType: GraphQLInputObjectType;
+  let confirmEmailOutputType: GraphQLOutputType;
+  let resendConfirmationEmailInputType: GraphQLInputObjectType;
+  let resendConfirmationEmailOutputType: GraphQLOutputType;
+  let queryType: GraphQLObjectType;
+  let mutationType: GraphQLObjectType;
+
+  dateType = new GraphQLScalarType({
     name: 'Date',
-    description: 'Date custom scalar type',
     parseValue(value: string) {
       return new Date(value);
     },
@@ -20,15 +51,11 @@ export const getSchema = () => {
       return date.getTime();
     },
     parseLiteral(ast) {
-      // Only accept string dates
-      if (ast.kind === Kind.STRING) {
-        return new Date(ast.value);
-      }
-      return null;
+      return ast.kind === Kind.STRING ? new Date(ast.value) : null;
     },
   });
 
-  const userRolesType = new GraphQLEnumType({
+  userRolesType = new GraphQLEnumType({
     name: 'UserRoles',
     values: {
       STUDENT: { value: 'student' },
@@ -37,113 +64,142 @@ export const getSchema = () => {
     },
   });
 
-  const queryType = new GraphQLObjectType<undefined, GlobalCtx>({
+  userType = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+      id: { type: new GraphQLNonNull(GraphQLString) },
+      username: { type: new GraphQLNonNull(GraphQLString) },
+      email: { type: new GraphQLNonNull(GraphQLString) },
+      createdAt: { type: new GraphQLNonNull(dateType) },
+      updatedAt: { type: new GraphQLNonNull(dateType) },
+      confirmedAt: { type: dateType },
+      notations: { type: new GraphQLList(GraphQLString) },
+      role: { type: userRolesType },
+    }),
+  });
+
+  notationType = new GraphQLObjectType({
+    name: 'Notation',
+    fields: () => ({
+      id: { type: GraphQLNonNull(GraphQLString) },
+    }),
+  });
+
+  userInputType = new GraphQLInputObjectType({
+    name: 'UserInput',
+    fields: () => ({
+      id: { type: new GraphQLNonNull(GraphQLString) },
+    }),
+  });
+
+  userOutputType = userType;
+
+  usersInputType = new GraphQLInputObjectType({
+    name: 'UsersInput',
+    fields: () => ({}),
+  });
+
+  usersOutputType = new GraphQLList(userType);
+
+  authenticateInputType = new GraphQLInputObjectType({
+    name: 'AuthenticateInput',
+    fields: () => ({}),
+  });
+
+  authenticateOutputType = userType;
+
+  signupInputType = new GraphQLInputObjectType({
+    name: 'SignupInput',
+    fields: () => ({
+      username: { type: new GraphQLNonNull(GraphQLString) },
+      email: { type: new GraphQLNonNull(GraphQLString) },
+      password: { type: new GraphQLNonNull(GraphQLString) },
+    }),
+  });
+
+  signupOutputType = userType;
+
+  loginInputType = new GraphQLInputObjectType({
+    name: 'LoginInput',
+    fields: () => ({
+      emailOrUsername: { type: new GraphQLNonNull(GraphQLString) },
+      password: { type: new GraphQLNonNull(GraphQLString) },
+    }),
+  });
+
+  loginOutputType = userType;
+
+  logoutInputType = new GraphQLInputObjectType({
+    name: 'LogoutInput',
+    fields: () => ({}),
+  });
+
+  logoutOutputType = userType;
+
+  confirmEmailInputType = new GraphQLInputObjectType({
+    name: 'ConfirmEmailInput',
+    fields: () => ({
+      confirmationToken: { type: new GraphQLNonNull(GraphQLString) },
+    }),
+  });
+
+  confirmEmailOutputType = userType;
+
+  resendConfirmationEmailInputType = new GraphQLInputObjectType({
+    name: 'ResendConfirmation',
+    fields: () => ({
+      email: { type: new GraphQLNonNull(GraphQLString) },
+    }),
+  });
+
+  resendConfirmationEmailOutputType = new GraphQLNonNull(GraphQLString);
+
+  queryType = new GraphQLObjectType<undefined, GlobalCtx>({
     name: 'Query',
-    fields: {
-      hello: {
-        type: new GraphQLNonNull(GraphQLString),
-        resolve: () => {
-          return 'Hello, from the server!';
-        },
+    fields: () => ({
+      user: {
+        args: { input: { type: userInputType } },
+        type: userOutputType,
       },
-    },
+      users: {
+        args: { input: { type: usersInputType } },
+        type: usersOutputType,
+      },
+    }),
+  });
+
+  mutationType = new GraphQLObjectType<undefined, GlobalCtx>({
+    name: 'Mutation',
+    fields: () => ({
+      authenticate: {
+        args: { input: { type: authenticateInputType } },
+        type: authenticateOutputType,
+      },
+      signup: {
+        args: { input: { type: signupInputType } },
+        type: signupOutputType,
+      },
+      login: {
+        args: { input: { type: loginInputType } },
+        type: loginOutputType,
+      },
+      logout: {
+        args: { input: { type: logoutInputType } },
+        type: logoutOutputType,
+      },
+      confirmEmail: {
+        args: { input: { type: confirmEmailInputType } },
+        type: confirmEmailOutputType,
+      },
+      resendConfirmationEmail: {
+        args: { input: { type: resendConfirmationEmailInputType } },
+        type: resendConfirmationEmailOutputType,
+      },
+    }),
   });
 
   return new GraphQLSchema({
-    description: 'GraphQL Schema for StringSync',
     query: queryType,
-    types: [dateType, userRolesType],
+    mutation: mutationType,
   });
 };
-
-// export const getSchema = () => `
-// # Scalar
-// scalar Date
-
-// enum UserRoles {
-//   student
-//   teacher
-//   admin
-// }
-
-// # Types/Inputs
-// type User {
-//   id: String!
-//   username: String!
-//   email: String!
-//   createdAt: Date!
-//   updatedAt: Date!
-//   confirmedAt: Date
-//   notations: [Notation]
-//   role: UserRoles!
-// }
-
-// type Notation {
-//   id: String!
-// }
-
-// input GetUserInput {
-//   id: String!
-// }
-
-// type AuthenticatePayload {
-//   user: User!
-// }
-
-// input SignupInput {
-//   username: String!
-//   email: String!
-//   password: String!
-// }
-
-// type SignupPayload {
-//   user: User!
-// }
-
-// input LoginInput {
-//   emailOrUsername: String!
-//   password: String!
-// }
-
-// type LoginPayload {
-//   user: User!
-// }
-
-// type LogoutPayload {
-//   user: User!
-// }
-
-// input ConfirmEmailInput {
-//   confirmationToken: String!
-// }
-
-// type ConfirmEmailPayload {
-//   user: User!
-// }
-
-// input ResendConfirmationInput {
-//   email: String!
-// }
-
-// type ResendConfirmationPayload {
-//   email: String!
-// }
-
-// # Query
-// type Query {
-//   getUsers: [User]!
-//   getUser(input: GetUserInput!): User
-// }
-
-// # Mutation
-// type Mutation {
-//   signup(input: SignupInput!): SignupPayload!
-//   login(input: LoginInput!): LoginPayload!
-//   logout: LogoutPayload!
-//   authenticate: AuthenticatePayload!
-//   confirmEmail(input: ConfirmEmailInput!): ConfirmEmailPayload!
-//   resendConfirmation(
-//     input: ResendConfirmationInput!
-//   ): ResendConfirmationPayload!
-// }
-// `;
