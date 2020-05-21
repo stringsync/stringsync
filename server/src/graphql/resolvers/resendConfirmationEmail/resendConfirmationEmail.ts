34eef3432267ffa6e389184e7fa8724f76a7a9a1
Jared Johnson <jared@jaredjohnson.dev>
2020-05-21 18:07:57 -0400

    Refactor resendConfirmationEmail.

--- server/src/graphql/resolvers/resendConfirmationEmail/resendConfirmationEmail.ts
+++ server/src/graphql/resolvers/resendConfirmationEmail/resendConfirmationEmail.ts
@@ -5,41 +5,41 @@ import {
   AuthRequirements,
 } from '../../../common';
 import { sendConfirmationMail } from '../../../jobs/mail';
-import { transaction } from '../../../data/db';
 import uuid from 'uuid';
 import { ResolverCtx } from '../../../util/ctx';
 import { IFieldResolver } from 'graphql-tools';
 import { NotFoundError, BadRequestError } from '../../../common/errors';
-import { withAuthRequirement } from '../../middlewares';
+import { withAuthRequirement, withTransaction } from '../../middlewares';
 
 export const middleware = compose(
-  withAuthRequirement(AuthRequirements.LOGGED_IN)
+  withAuthRequirement(AuthRequirements.LOGGED_IN),
+  withTransaction
 );
 
 export const resolver: IFieldResolver<
   undefined,
   ResolverCtx,
   { input: ResendConfirmationEmailInput }
-> = async (src, args, ctx): Promise<ResendConfirmationEmailOutput> =>
-  transaction(ctx.db, async () => {
-    const { email } = args.input;
-    const user = await ctx.db.models.User.findOne({ where: { email } });
+> = async (src, args, ctx): Promise<ResendConfirmationEmailOutput> => {
+  const { email } = args.input;
+  const pk = ctx.req.session.user.id;
+  const user = await ctx.db.models.User.findByPk(pk);
 
-    if (!user) {
-      throw new NotFoundError(`user not found: ${email}`);
-    }
-    if (user.id !== ctx.req.session.user.id) {
-      throw new Error(`must be logged in as ${email}`);
-    }
-    if (user.confirmedAt) {
-      throw new BadRequestError('email already confirmed');
-    }
+  if (!user) {
+    throw new NotFoundError(`user not found: ${email}`);
+  }
+  if (user.email !== email) {
+    throw new Error(`must be logged in as ${email}`);
+  }
+  if (user.confirmedAt) {
+    throw new BadRequestError('email already confirmed');
+  }
 
-    const confirmationToken = uuid.v4();
-    user.update({ confirmationToken });
-    await sendConfirmationMail(email, confirmationToken, ctx);
+  user.confirmationToken = uuid.v4();
+  await user.save();
+  await sendConfirmationMail(user.email, user.confirmationToken, ctx);
 
-    return { email };
-  });
+  return { email };
+};
 
 export const resendConfirmationEmail = middleware(resolver);
