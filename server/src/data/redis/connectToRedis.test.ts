b04342cf3a5be9c11c770e66df935be2abd31745
Jared Johnson <jared@jaredjohnson.dev>
2020-05-23 12:50:49 -0400

    Close redis connection in connectToRedis test.

--- server/src/data/redis/connectToRedis.test.ts
+++ server/src/data/redis/connectToRedis.test.ts
@@ -3,6 +3,12 @@ import { getConfig } from '../../config';
 
 const config = getConfig(process.env);
 
-it('runs without crashing', () => {
-  expect(() => connectToRedis(config)).not.toThrow();
+it('runs without crashing', async () => {
+  const fn = async () => {
+    const redis = connectToRedis(config);
+    await redis.ping();
+    await redis.quit();
+    return Promise.resolve();
+  };
+  await expect(fn()).resolves.not.toThrow();
 });
