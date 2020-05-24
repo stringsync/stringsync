c7ed716a0539fd670515a4b525edc06a91e0e33a
Jared Johnson <jared@jaredjohnson.dev>
2020-05-24 02:04:48 -0400

    Clean up user resolver.

--- server/src/graphql/resolvers/Query/user/user.test.ts
+++ server/src/graphql/resolvers/Query/user/user.test.ts
@@ -1,15 +1,18 @@
 import { resolver } from './user';
 import { tmpResolverCtx, createUser, randStr } from '../../../../testing';
 
-it('returns the user matching the id', () => {
-  const id = randStr(10);
+describe('resolver', () => {
+  it('returns the user matching the id', () => {
+    const id = randStr(10);
+    const input = { input: { id } };
 
-  return tmpResolverCtx(async (ctx, info) => {
-    await createUser(ctx.db, { id });
+    return tmpResolverCtx(async (ctx, info) => {
+      await createUser(ctx.db, { id });
 
-    const user = await resolver(undefined, { input: { id } }, ctx, info);
+      const user = await resolver(undefined, input, ctx, info);
 
-    expect(user).not.toBeNull();
-    expect(user!.id).toBe(id);
+      expect(user).not.toBeNull();
+      expect(user!.id).toBe(id);
+    });
   });
 });
