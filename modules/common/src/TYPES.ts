2058f33c9ea459d53087e88c1af8f8ea027789a3
Jared Johnson <jared@jaredjohnson.dev>
2020-05-31 13:51:15 -0400

    Create common module.

---
+++ modules/common/src/TYPES.ts
@@ -0,0 +1,7 @@
+export const TYPES = {
+  Db: Symbol('Db'),
+  UserRepo: Symbol('UserRepo'),
+  Config: Symbol('Config'),
+  Redis: Symbol('Redis'),
+  HealthCheckerService: Symbol('HealthCheckerService'),
+};
