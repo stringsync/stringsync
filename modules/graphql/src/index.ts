babe0858b61353259065409af20c5ab65f411378
Jared Johnson <jared@jaredjohnson.dev>
2020-05-31 10:41:33 -0400

    Format index.ts.

--- modules/graphql/src/index.ts
+++ modules/graphql/src/index.ts
@@ -5,6 +5,7 @@ import { getApp } from './app';
 const container = getContainer();
 const app = getApp(container);
 const config = container.get<Config>(TYPES.Config);
+
 app.listen(config.PORT, () => {
   console.log(`running at http://localhost:${config.PORT}`);
 });
