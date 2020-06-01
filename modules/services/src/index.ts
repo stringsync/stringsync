79d1e9f0171c6de1737b4d27b4c0c1c1d32aa404
Jared Johnson <jared@jaredjohnson.dev>
2020-06-01 14:27:56 -0400

    Create UserService.

--- modules/services/src/index.ts
+++ modules/services/src/index.ts
@@ -1,1 +1,2 @@
 export * from './health-checker';
+export * from './user';
---
+++ modules/services/src/user/UserService.ts
@@ -0,0 +1,17 @@
+import { inject, injectable } from 'inversify';
+import { TYPES } from '@stringsync/common';
+import { UserRepo } from '@stringsync/repos';
+import * as domain from '@stringsync/domain';
+
+@injectable()
+export class UserService {
+  readonly userRepo: UserRepo;
+
+  constructor(@inject(TYPES.UserRepo) userRepo: UserRepo) {
+    this.userRepo = userRepo;
+  }
+
+  async get(id: string): Promise<domain.User | null> {
+    return await this.userRepo.get(id);
+  }
+}
