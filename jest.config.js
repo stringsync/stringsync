2508fe7c951fc13a021b236fc682ae7f3b072a2a
Jared Johnson <jared@jaredjohnson.dev>
2020-06-03 06:52:56 -0400

    Add jest config to root.

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+    },
+  },
+  roots: ['<rootDir>/modules/'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+};
