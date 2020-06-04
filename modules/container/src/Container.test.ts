b89306223f91e475cd4efd754d08c9d0bd8c5b4a
Jared Johnson <jared@jaredjohnson.dev>
2020-06-04 19:41:52 -0400

    Test getContainer.

--- modules/container/src/Container.test.ts
+++ modules/container/src/Container.test.ts
@@ -5,4 +5,8 @@ describe('instance', () => {
   it('returns a inversify Container instance', () => {
     expect(Container.instance).toBeInstanceOf(InversifyContainer);
   });
+
+  it('returns the same inversify Container instance', () => {
+    expect(Container.instance).toBe(Container.instance);
+  });
 });
