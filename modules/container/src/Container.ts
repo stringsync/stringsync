72877e17a93f57c579edfd1f3274d253924df954
Jared Johnson <jared@jaredjohnson.dev>
2020-06-04 19:55:42 -0400

    Add mechanism to cleanup cache to Container.

--- modules/container/src/Container.ts
+++ modules/container/src/Container.ts
@@ -1,8 +1,11 @@
 import { Container as InverisfyContainer } from 'inversify';
 import { getContainerConfig } from '@stringsync/config';
 import { getContainer } from './getContainer';
+import { Db } from '@stringsync/sequelize';
+import { TYPES } from './constants';
+import { Redis } from 'ioredis';
 
-let cache: InverisfyContainer;
+let cache: InverisfyContainer | undefined;
 
 export class Container {
   static get instance(): InverisfyContainer {
@@ -11,5 +14,23 @@ export class Container {
       cache = getContainer(config);
     }
     return cache;
+  }
+
+  static async reset() {
+    if (!cache) {
+      return;
+    }
+
+    Container.cleanup(cache);
+
+    cache = undefined;
+  }
+
+  private static async cleanup(container: InverisfyContainer) {
+    const redis = container.get<Redis>(TYPES.Redis);
+    const db = container.get<Db>(TYPES.Db);
+
+    redis.disconnect();
+    await db.sequelize.close();
   }
 }
