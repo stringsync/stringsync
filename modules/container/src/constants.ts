29febba3dab41961286d9530fcb6dfa9004e7d5b
Jared Johnson <jared@jaredjohnson.dev>
2020-06-07 10:54:19 -0400

    Remove unused TYPES.

--- modules/container/src/constants.ts
+++ modules/container/src/constants.ts
@@ -1,11 +1,9 @@
 export const TYPES = {
-  Db: Symbol('Db'),
   Connection: Symbol('Connection'),
   ContainerConfig: Symbol('ContainerConfig'),
   HealthCheckerService: Symbol('HealthCheckerService'),
   HealthController: Symbol('HealthController'),
   Redis: Symbol('Redis'),
   UserRepo: Symbol('UserRepo'),
-  UserMemoryRepo: Symbol('UserMemoryRepo'),
   UserService: Symbol('UserService'),
 };
