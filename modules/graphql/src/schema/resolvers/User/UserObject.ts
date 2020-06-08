a01bb9661b0fba786d68a861e163585bde2e25e9
Jared Johnson <jared@jaredjohnson.dev>
2020-06-07 20:38:23 -0400

    Add RestrictedField middleware in UserObject.

--- modules/graphql/src/schema/resolvers/User/UserObject.ts
+++ modules/graphql/src/schema/resolvers/User/UserObject.ts
@@ -1,10 +1,7 @@
 import { ObjectType, Field, ID, registerEnumType, UseMiddleware } from 'type-graphql';
 import { User, UserRole } from '@stringsync/domain';
+import { RestrictedField } from './RestrictedField';
 import { IsDataOwner } from './IsDataOwner';
-import { Branch } from '../../middlewares/Branch';
-import { Identity } from '../../middlewares/Identity';
-import { WithAuthRequirement, WithErrorHandler } from '../../middlewares';
-import { AuthRequirement, ForbiddenError } from '@stringsync/common';
 
 type PublicFacingUser = Omit<User, 'encryptedPassword' | 'confirmationToken' | 'confirmedAt' | 'resetPasswordToken'>;
 
@@ -22,29 +19,23 @@ export class UserObject implements PublicFacingUser {
   updatedAt!: Date;
 
   @Field()
-  username!: string;
+  @RestrictedField(IsDataOwner)
+  email!: string;
 
   @Field()
-  @UseMiddleware(
-    WithErrorHandler((err) => {
-      if (err instanceof ForbiddenError) {
-        throw new ForbiddenError('must be logged in as data owner or admin');
-      }
-      throw err;
-    }),
-    Branch(IsDataOwner, Identity, WithAuthRequirement(AuthRequirement.LOGGED_IN_AS_ADMIN))
-  )
-  email!: string;
+  username!: string;
+
+  @Field((type) => String, { nullable: true })
+  avatarUrl!: string | null;
 
   @Field((type) => UserRole)
   role!: UserRole;
 
   @Field((type) => Date, { nullable: true })
+  @RestrictedField(IsDataOwner)
   confirmedAt!: Date | null;
 
   @Field((type) => Date, { nullable: true })
+  @RestrictedField(IsDataOwner)
   resetPasswordTokenSentAt!: Date | null;
-
-  @Field((type) => String, { nullable: true })
-  avatarUrl!: string | null;
 }
