32cd11d5a02540f46a8ba97c5cbff507994d16d0
Jared Johnson <jared@jaredjohnson.dev>
2020-06-07 20:42:59 -0400

    Add restriction to users resolver.

--- modules/graphql/src/schema/resolvers/User/UserResolver.ts
+++ modules/graphql/src/schema/resolvers/User/UserResolver.ts
@@ -1,10 +1,12 @@
-import { Resolver, Query, Args } from 'type-graphql';
+import { Resolver, Query, Args, UseMiddleware } from 'type-graphql';
 import { injectable, inject } from 'inversify';
 import { TYPES } from '@stringsync/container';
 import { UserService } from '@stringsync/services';
 import { UserObject } from './UserObject';
 import { UserArgs } from './UserArgs';
 import { User } from '@stringsync/domain';
+import { WithAuthRequirement } from '../../middlewares';
+import { AuthRequirement } from '@stringsync/common';
 
 @Resolver()
 @injectable()
@@ -21,6 +23,7 @@ export class UserResolver {
   }
 
   @Query((returns) => [UserObject])
+  @UseMiddleware(WithAuthRequirement(AuthRequirement.LOGGED_IN_AS_ADMIN))
   async users(): Promise<User[]> {
     return await this.userService.findAll();
   }
