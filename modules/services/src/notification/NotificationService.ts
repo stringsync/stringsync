d731f550c2c06adc5fcce62e95f5e8371d2be223
Jared Johnson <jared@jaredjohnson.dev>
2020-06-08 07:46:53 -0400

    Add sendConfirmationEmail action to notification service.

--- modules/services/src/notification/NotificationService.ts
+++ modules/services/src/notification/NotificationService.ts
@@ -1,4 +1,32 @@
-import { injectable } from 'inversify';
+import { injectable, inject } from 'inversify';
+import url from 'url';
+import { TYPES } from '@stringsync/container';
+import { ContainerConfig } from '@stringsync/config';
 
 @injectable()
-export class NotificationService {}
+export class NotificationService {
+  readonly config: ContainerConfig;
+
+  constructor(@inject(TYPES.ContainerConfig) config: ContainerConfig) {
+    this.config = config;
+  }
+
+  async sendConfirmationEmail(to: string, confirmationToken: string): Promise<void> {
+    const confirmHref = url.format({
+      protocol: 'https',
+      hostname: this.config.WEB_URI,
+      pathname: 'confirm-email',
+      query: { confirmationToken },
+    });
+
+    const emailOptions = {
+      from: 'StringSync <info@stringsync.com>',
+      to,
+      html: `
+        <p>
+          Please confirmation your email for <a href=${confirmHref}>StringSync<a/>
+        </p>
+      `,
+    };
+  }
+}
