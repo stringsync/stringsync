723befb47cea622a0a3837daf3d7f67ee6b528e6
Jared Johnson <jared@jaredjohnson.dev>
2020-06-09 17:59:43 -0400

    Update cleanup to be concurrent.

--- modules/container/src/cleanupContainer.ts
+++ modules/container/src/cleanupContainer.ts
@@ -5,11 +5,13 @@ import { TYPES } from './constants';
 
 export const cleanupContainer = async (container: Container): Promise<void> => {
   const connection = container.get<Connection>(TYPES.Connection);
-  await connection.close();
+  const connectionPromise = connection.close();
 
   const redis = container.get<Redis>(TYPES.Redis);
-  await new Promise((resolve) => {
+  const redisPromise = new Promise((resolve) => {
     redis.on('end', resolve);
     redis.quit();
   });
+
+  await Promise.all([connectionPromise, redisPromise]);
 };
