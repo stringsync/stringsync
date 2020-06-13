abeba4e3744c9ccc2efe08557764869bdcba93c0
Jared Johnson <jared@jaredjohnson.dev>
2020-06-13 11:03:07 -0400

    Add NODE_ENV to web config.

--- modules/config/src/getWebConfig.test.ts
+++ modules/config/src/getWebConfig.test.ts
@@ -2,6 +2,7 @@ import { getWebConfig } from './getWebConfig';
 
 it('runs without crashing', () => {
   const env = {
+    NODE_ENV: 'NODE_ENV',
     REACT_APP_SERVER_URI: 'REACT_APP_SERVER_URI',
   };
 
--- modules/config/src/getWebConfig.ts
+++ modules/config/src/getWebConfig.ts
@@ -2,6 +2,7 @@ import { configFactory } from './configFactory';
 import { ConfigKind } from './types';
 
 export const getWebConfig = configFactory({
+  NODE_ENV: ConfigKind.STRING,
   REACT_APP_SERVER_URI: ConfigKind.STRING,
 });
 
