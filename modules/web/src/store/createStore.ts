d5b40eef078bcabcb264b5aa62fc7e06789a027b
Jared Johnson <jared@jaredjohnson.dev>
2020-06-13 15:20:55 -0400

    Add AppStore type to createStore.

--- modules/web/src/store/createStore.ts
+++ modules/web/src/store/createStore.ts
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-export const createStore = () =>
-  configureStore({
+export const createStore = () => {
+  return configureStore({
     reducer: {},
   });
+};
+
+export type AppStore = ReturnType<typeof createStore>;
