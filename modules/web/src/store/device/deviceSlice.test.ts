5129fb139ff11d7b02eed271c087fdc348080f54
Jared Johnson <jared@jaredjohnson.dev>
2020-06-14 09:36:29 -0400

    Test device slice initializes state and sets user agent.

---
+++ modules/web/src/store/device/deviceSlice.test.ts
@@ -0,0 +1,23 @@
+import { deviceSlice, setUserAgent } from './deviceSlice';
+import { configureStore, EnhancedStore } from '@reduxjs/toolkit';
+import { getDevice } from './getDevice';
+
+let store: EnhancedStore;
+
+beforeEach(() => {
+  store = configureStore({
+    reducer: {
+      device: deviceSlice.reducer,
+    },
+  });
+});
+
+it('initializes state', () => {
+  expect(navigator.userAgent).toBeTruthy();
+  expect(store.getState().device).toStrictEqual(getDevice(navigator.userAgent));
+});
+
+it('sets user agent', () => {
+  store.dispatch(setUserAgent({ userAgent: '' }));
+  expect(store.getState().device).toStrictEqual(getDevice(''));
+});
