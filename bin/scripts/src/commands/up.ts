d65fe7fe3ad716c10be539dad455f2c3b2f0064f
Jared Johnson <jared@jaredjohnson.dev>
2020-06-14 09:21:00 -0400

    Support web in the up commands.

--- bin/scripts/src/commands/up.ts
+++ bin/scripts/src/commands/up.ts
@@ -1,6 +1,6 @@
 import { Command, flags } from '@oclif/command';
 import { ROOT_PATH, PROJECT_ARG, getDockerComposeFile } from '../util';
-import { spawn, execSync } from 'child_process';
+import { execSync, spawn } from 'child_process';
 
 export default class Up extends Command {
   static description = 'Spins up a development environment';
@@ -13,6 +13,11 @@ export default class Up extends Command {
 
   async run() {
     const { args } = this.parse(Up);
+
+    if (args.project === 'web') {
+      execSync('yarn web dev', { cwd: ROOT_PATH, stdio: 'inherit' });
+      this.exit();
+    }
 
     spawn('docker-compose', ['-p', args.project, '-f', getDockerComposeFile(args.project), 'up', '--build', '-d'], {
       cwd: ROOT_PATH,
