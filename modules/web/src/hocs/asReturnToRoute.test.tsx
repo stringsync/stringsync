6ae3e0cc5357beb86ef854ae37779d320ed81730
Jared Johnson <jared@jaredjohnson.dev>
2020-06-18 23:47:49 -0400

    Test asReturnToRoute when mounted.

---
+++ modules/web/src/hocs/asReturnToRoute.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { asReturnToRoute } from './asReturnToRoute';
+import { EnhancedStore } from '@reduxjs/toolkit';
+import { RootState, createStore } from '../store';
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Route, Router } from 'react-router';
+import { Provider } from 'react-redux';
+
+const Dummy: React.FC = asReturnToRoute(() => <div data-testid="dummy"></div>);
+const Routes = () => <Route path="/dummy" component={Dummy} />;
+
+let store: EnhancedStore<RootState>;
+
+beforeEach(() => {
+  store = createStore();
+});
+
+it('sets returnToRoute when mounted', () => {
+  const history = createMemoryHistory();
+
+  history.push('/dummy?foo=bar#baz');
+  const { queryByTestId } = render(
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+      </Router>
+    </Provider>
+  );
+
+  expect(queryByTestId('dummy')).toBeInTheDocument();
+  expect(store.getState().history.returnToRoute).toBe('/dummy?foo=bar#baz');
+});
