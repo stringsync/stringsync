d6ceae0dc37226f1cb5c2e6afa1da7c12e66a25a
Jared Johnson <jared@jaredjohnson.dev>
2020-06-20 19:06:18 -0400

    Add notation domain type.

---
+++ modules/domain/src/notation/types.ts
@@ -0,0 +1,12 @@
+export interface Notation {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+  songName: string;
+  artistName: string;
+  deadTimeMs: number;
+  durationMs: number;
+  bpm: number;
+  featured: boolean;
+  transcriberId: number;
+}
