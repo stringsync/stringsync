ef0ce2ea5ed41acb04da1fd18aefa79986b8c443
Jared Johnson <jared@jaredjohnson.dev>
2020-06-19 23:41:22 -0400

    Test AuthSync updates user when logged out and that is swallows errors silently.

--- modules/web/src/components/AuthSync.test.tsx
+++ modules/web/src/components/AuthSync.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { AuthSync } from './AuthSync';
 import { Clients, createClients, UserRoles } from '../clients';
-import { render, waitFor } from '@testing-library/react';
+import { render, waitFor, wait } from '@testing-library/react';
 import { Test } from '../testing';
 import { AppStore, createStore } from '../store';
 import { UserRole } from '@stringsync/domain';
+import { getNullAuthUser } from '../store/auth/getNullAuthUser';
+import { GraphQLError } from 'graphql';
 
 let store: AppStore;
 let clients: Clients;
@@ -14,7 +16,7 @@ beforeEach(() => {
   clients = createClients();
 });
 
-it('updates auth user when successful', async () => {
+it('updates auth user when logged in', async () => {
   const now = new Date();
   const whoamiSpy = jest.spyOn(clients.authClient, 'whoami');
   whoamiSpy.mockResolvedValue({
@@ -46,4 +48,36 @@ it('updates auth user when successful', async () => {
     role: UserRole.TEACHER,
     confirmedAt: now.toJSON(),
   });
+});
+
+it('updates user when logged out', async () => {
+  const whoamiSpy = jest.spyOn(clients.authClient, 'whoami');
+  whoamiSpy.mockResolvedValue({ data: { whoami: null } });
+
+  render(
+    <Test store={store} clients={clients}>
+      <AuthSync />
+    </Test>
+  );
+
+  await waitFor(() => expect(whoamiSpy).toHaveBeenCalledTimes(1));
+
+  expect(store.getState().auth.user).toStrictEqual(getNullAuthUser());
+});
+
+it('swallows errors silently', async () => {
+  const whoamiSpy = jest.spyOn(clients.authClient, 'whoami');
+  whoamiSpy.mockResolvedValue({ data: { whoami: null }, errors: [new GraphQLError('error message 1')] });
+
+  render(
+    <Test store={store} clients={clients}>
+      <AuthSync />
+    </Test>
+  );
+
+  await waitFor(() => expect(whoamiSpy).toHaveBeenCalledTimes(1));
+
+  const state = store.getState();
+  expect(state.auth.user).toStrictEqual(getNullAuthUser());
+  expect(state.auth.errors).toHaveLength(0);
 });
