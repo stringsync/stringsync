cfd54860822493f7bd401b53895d685187e08b9a
Jared Johnson <jared@jaredjohnson.dev>
2020-06-27 12:12:30 -0400

    Add tagRepo to NotationService.

--- modules/services/src/notation/NotationService.ts
+++ modules/services/src/notation/NotationService.ts
@@ -1,12 +1,14 @@
 import { TYPES } from '@stringsync/container';
 import { injectable, inject } from 'inversify';
-import { NotationRepo } from '@stringsync/repos';
+import { NotationRepo, TagRepo } from '@stringsync/repos';
 
 @injectable()
 export class NotationService {
   readonly notationRepo: NotationRepo;
+  readonly tagRepo: TagRepo;
 
-  constructor(@inject(TYPES.NotationRepo) notationRepo: NotationRepo) {
+  constructor(@inject(TYPES.NotationRepo) notationRepo: NotationRepo, @inject(TYPES.TagRepo) tagRepo: TagRepo) {
     this.notationRepo = notationRepo;
+    this.tagRepo = tagRepo;
   }
 }
