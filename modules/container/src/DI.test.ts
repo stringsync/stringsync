f449822d8fdc0577f4e1da8e8d669b29c910539f
Jared Johnson <jared@jaredjohnson.dev>
2020-07-03 01:26:23 -0400

    Test DI can create all symbols.

---
+++ modules/container/src/DI.test.ts
@@ -0,0 +1,21 @@
+import { TYPES } from '@stringsync/container';
+import { Container } from 'inversify';
+import { DI } from './DI';
+
+let container: Container | undefined;
+
+afterEach(async () => {
+  if (container) {
+    await DI.cleanup(container);
+  }
+  container = undefined;
+});
+
+it('returns an instance for each identifier', () => {
+  container = DI.create();
+
+  for (const identifier of Object.values(TYPES)) {
+    const object = container.get(identifier);
+    expect(object).toBeDefined();
+  }
+});
