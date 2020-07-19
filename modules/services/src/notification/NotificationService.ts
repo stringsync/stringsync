80b9d67a28821f08972ca7c7c63b6338cd3b8c89
Jared Johnson <jared@jaredjohnson.dev>
2020-07-19 06:05:20 -0400

    Add reset password email to the NotificationService.

--- modules/services/src/notification/NotificationService.ts
+++ modules/services/src/notification/NotificationService.ts
@@ -3,9 +3,12 @@ import url from 'url';
 import { TYPES } from '@stringsync/container';
 import { ContainerConfig } from '@stringsync/config';
 import { User } from '@stringsync/domain';
+import { BadRequestError } from '@stringsync/common';
 
 @injectable()
 export class NotificationService {
+  static INFO_EMAIL = 'StringSync <info@stringsync.com>';
+
   config: ContainerConfig;
 
   constructor(@inject(TYPES.ContainerConfig) config: ContainerConfig) {
@@ -21,11 +24,34 @@ export class NotificationService {
     });
 
     const emailOptions = {
-      from: 'StringSync <info@stringsync.com>',
+      from: NotificationService.INFO_EMAIL,
       to: user.email,
       html: `
         <p>
-          Please confirmation your email for <a href=${confirmHref}>StringSync<a/>
+          Please confirm your email for <a href="${confirmHref}">StringSync<a/>
+        </p>
+      `,
+    };
+  }
+
+  async sendResetPasswordEmail(user: User): Promise<void> {
+    if (!user.resetPasswordToken) {
+      throw new Error('user must have a reset password token');
+    }
+
+    const resetPasswordHref = url.format({
+      protocol: 'https',
+      hostname: this.config.WEB_URI,
+      pathname: 'reset-password',
+      query: { resetPasswordToken: user.resetPasswordToken },
+    });
+
+    const emailOptions = {
+      from: NotificationService.INFO_EMAIL,
+      to: user.email,
+      html: `
+        <p>
+          Reset your password at <a href="${resetPasswordHref}">StringSync<a/>
         </p>
       `,
     };
