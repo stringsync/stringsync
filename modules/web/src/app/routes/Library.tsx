ed5aa3277ae71b89dbe63eee875c73c8a8ad114e
Jared Johnson <jared@jaredjohnson.dev>
2020-07-26 18:54:57 -0400

    Seed 100 notations to make pagination hand testing easier.

--- modules/web/src/app/routes/Library.tsx
+++ modules/web/src/app/routes/Library.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { compose } from '@stringsync/common';
+import { compose, PageInfo } from '@stringsync/common';
 import { Layout, withLayout } from '../../hocs';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState, AppDispatch } from '../../store';
+import { NotationPreview } from '../../store/library/types';
+import { getNotationPage } from '../../store/library';
+import { useEffectOnce } from '../../hooks';
+
+const PAGE_SIZE = 10;
 
 interface Props {}
 
 const enhance = compose(withLayout(Layout.DEFAULT));
 
 const Library: React.FC<Props> = enhance(() => {
+  const dispatch = useDispatch<AppDispatch>();
+  const notations = useSelector<RootState, NotationPreview[]>((state) => state.library.notations);
+  const pageInfo = useSelector<RootState, PageInfo>((state) => state.library.pageInfo);
+
+  useEffectOnce(() => {
+    if (notations.length === 0 && pageInfo.hasNextPage) {
+      dispatch(getNotationPage({ first: PAGE_SIZE, after: pageInfo.endCursor }));
+    }
+  });
+
   return <div data-testid="library">Library</div>;
 });
 
