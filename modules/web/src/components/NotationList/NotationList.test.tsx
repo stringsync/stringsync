7a065534892c596938a2f829227f245d482e8c2d
Jared Johnson <jared@jaredjohnson.dev>
2020-08-09 12:59:38 -0400

    Test NotationList.

---
+++ modules/web/src/components/NotationList/NotationList.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { randStr } from '@stringsync/common';
+import { UserRole } from '@stringsync/domain';
+import { NotationList } from './NotationList';
+import { render } from '@testing-library/react';
+import { NotationPreview } from '../../store/library/types';
+
+it('renders without crashing', async () => {
+  const now = new Date();
+  const notations: NotationPreview[] = [
+    {
+      id: randStr(8),
+      artistName: randStr(10),
+      createdAt: now,
+      updatedAt: now,
+      songName: randStr(10),
+      thumbnailUrl: randStr(10),
+      tags: [],
+      transcriber: {
+        id: randStr(8),
+        avatarUrl: randStr(10),
+        role: UserRole.TEACHER,
+        username: randStr(8),
+      },
+    },
+  ];
+  const loadMore = jest.fn();
+  const hasNextPage = false;
+
+  const { container } = render(<NotationList notations={notations} loadMore={loadMore} hasNextPage={hasNextPage} />);
+
+  expect(container).toBeInTheDocument();
+});
