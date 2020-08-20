9556899fcdd329b2a8256023cbf447134ff1d267
Jared Johnson <jared@jaredjohnson.dev>
2020-08-19 21:11:24 -0400

    Test findNotationPageQuery runs without crashing.

--- modules/repos/src/queries/findNotationPageQuery.test.ts
+++ modules/repos/src/queries/findNotationPageQuery.test.ts
@@ -4,6 +4,9 @@ import { PagingType } from '@stringsync/common';
 it.each<FindNotationpageQueryArgs>([
   { cursor: 1, pagingType: PagingType.FORWARD, limit: 10, tagIds: [], query: '%query%' },
   { cursor: 1, pagingType: PagingType.BACKWARD, limit: 10, tagIds: [], query: '%query%' },
+  { cursor: 1, pagingType: PagingType.FORWARD, limit: 10, tagIds: [], query: null },
+  { cursor: 1, pagingType: PagingType.BACKWARD, limit: 10, tagIds: null, query: '%query%' },
+  { cursor: 1, pagingType: PagingType.FORWARD, limit: 10, tagIds: null, query: null },
 ])('runs without crashing', (args) => {
   expect(() => findNotationPageQuery(args)).not.toThrow();
 });
