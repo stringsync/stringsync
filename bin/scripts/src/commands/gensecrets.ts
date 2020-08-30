ef4b67dedec5a9aeeabe5847a69d9cd49fa1a49a
Jared Johnson <jared@jaredjohnson.dev>
2020-08-30 10:41:02 -0400

    Check that the secrets file is ignored.

--- bin/scripts/src/commands/gensecrets.ts
+++ bin/scripts/src/commands/gensecrets.ts
@@ -1,10 +1,11 @@
 import { Command, flags } from '@oclif/command';
-import { ROOT_PATH } from '../util';
+import { existsSync, readFile, writeFileSync } from 'fs';
 import path from 'path';
-import { existsSync } from 'fs';
-import { writeFileSync } from 'fs';
+import { ROOT_PATH } from '../util';
 
+const GITIGNORE_FILE_NAME = '.gitignore';
 const FILE_NAME = 'secrets.env';
+const FILE_TEMPLATE = 'ACCESS_KEY_ID=\nSECRET_ACCESS_KEY=\n';
 
 export default class Gensecrets extends Command {
   static description = 'Creates the secrets.env file needed for local development.';
@@ -15,14 +16,29 @@ export default class Gensecrets extends Command {
 
   async run() {
     const filepath = path.join(ROOT_PATH, FILE_NAME);
+    const gitignorePath = path.join(ROOT_PATH, GITIGNORE_FILE_NAME);
 
     if (existsSync(filepath)) {
       this.log(`secrets file already exists: ${filepath}`);
       this.exit();
     }
 
+    let isSecretsFileIgnored = true;
+    readFile(gitignorePath, (err, data) => {
+      if (err) {
+        throw err;
+      }
+      if (!data.includes(FILE_NAME)) {
+        isSecretsFileIgnored = false;
+      }
+    });
+    if (!isSecretsFileIgnored) {
+      this.log(`secrets file is not ignored,  add '${FILE_NAME}' to ${gitignorePath}`);
+      this.exit();
+    }
+
     this.log(`creating secrets file: ${filepath}`);
-    writeFileSync(filepath, '');
+    writeFileSync(filepath, FILE_TEMPLATE);
     this.log('done');
   }
 }
