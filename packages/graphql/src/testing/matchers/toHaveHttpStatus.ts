d03e39141c0b719b669b8fa69a4696272bfca53a
Jared Johnson <jared@jaredjohnson.dev>
2020-09-19 20:39:25 -0400

    Improve toHaveHttpStatus matcher message.

--- packages/graphql/src/testing/matchers/toHaveHttpStatus.ts
+++ packages/graphql/src/testing/matchers/toHaveHttpStatus.ts
@@ -4,8 +4,8 @@ import { Response } from '../types';
 
 const message = (pass: boolean, expected: HttpStatus) => () =>
   pass
-    ? matcherHint('not.toHaveHttpStatus', 'res.statusCode', expected.toString())
-    : matcherHint('.toHaveHttpStatus', 'res.statusCode', expected.toString());
+    ? matcherHint('.not.toHaveHttpStatus', 'received', expected.toString())
+    : matcherHint('.toHaveHttpStatus', 'received', expected.toString());
 
 export const toHaveHttpStatus: jest.CustomMatcher = function(res: Response<any, any>, expected: HttpStatus) {
   const actual = res.statusCode;
