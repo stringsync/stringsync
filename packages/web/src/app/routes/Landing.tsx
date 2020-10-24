2571d44253c38aea9ce45c59cfde145966ece6f9
Jared Johnson <jared@jaredjohnson.dev>
2020-10-24 12:55:04 -0400

    Create Landing page.

--- packages/web/src/app/routes/Landing.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { compose } from '@stringsync/common';
-import { withLayout, Layout } from '../../hocs';
-
-const enhance = compose(withLayout(Layout.DEFAULT));
-
-export const Landing: React.FC = enhance(() => {
-  return <div data-testid="landing">Landing</div>;
-});
