3d0866186d6d41635f0d9a5b8c01ba026eeede0e
Jared Johnson <jared@jaredjohnson.dev>
2020-10-24 14:15:34 -0400

    Use extract-files library for graphql queries in the web package.

--- packages/web/src/clients/graphql/GraphqlClient.ts
+++ packages/web/src/clients/graphql/GraphqlClient.ts
@@ -1,4 +1,5 @@
 import { getWebConfig } from '@stringsync/config';
+import { ExtractableFile, extractFiles } from 'extract-files';
 import { RequestType, Response } from './types';
 
 export class GraphqlClient {
@@ -26,10 +27,39 @@ export class GraphqlClient {
     query: string,
     variables?: V
   ): Promise<Response<T, N>> => {
+    // extract files
+    const extraction = extractFiles(
+      { query, variables },
+      undefined,
+      (value: any): value is ExtractableFile => value instanceof File
+    );
+    const clone = extraction.clone;
+    const fileMap = extraction.files;
+
+    // compute map
+    const map: { [key: string]: string | string[] } = {};
+    const pathGroups = Array.from(fileMap.values());
+    for (let ndx = 0; ndx < pathGroups.length; ndx++) {
+      const paths = pathGroups[ndx];
+      map[ndx] = paths;
+    }
+
+    // create form data
+    const formData = new FormData();
+    formData.append('operations', JSON.stringify(clone));
+    formData.append('map', JSON.stringify(map));
+
+    // append files to form data
+    const files = Array.from(fileMap.keys());
+    for (let ndx = 0; ndx < files.length; ndx++) {
+      const file = files[ndx];
+      formData.append(ndx.toString(), file as Blob);
+    }
+
     const res = await fetch(this.uri, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-      body: JSON.stringify({ query, variables }),
+      headers: { Accept: 'application/json' },
+      body: formData,
       credentials: 'include',
       mode: 'cors',
     });
