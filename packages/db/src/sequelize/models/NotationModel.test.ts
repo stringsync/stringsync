26fefb1189be681e2f9d1c55449fa56ee875a049
Jared Johnson <jared@jaredjohnson.dev>
2020-12-12 19:56:04 -0500

    Fix NotationModel test.

--- packages/db/src/sequelize/models/NotationModel.test.ts
+++ packages/db/src/sequelize/models/NotationModel.test.ts
@@ -1,62 +1,56 @@
-// import { EntityBuilder } from '@stringsync/common';
-// import { TYPES, useTestContainer } from '@stringsync/di';
-// import { Notation, User } from '@stringsync/domain';
-// import { NotationRepo, UserRepo } from '@stringsync/repos';
-// import { NotationModel } from './NotationModel';
+import { useTestContainer } from '@stringsync/di';
+import { EntityBuilder, Notation, User } from '@stringsync/domain';
+import { DB } from '../../DB';
+import { NotationModel } from './NotationModel';
+import { UserModel } from './UserModel';
 
-// const container = useTestContainer();
+useTestContainer(DB);
 
-// let userRepo: UserRepo;
-// let notationRepo: NotationRepo;
+let transcriber: User;
+let notation: Notation;
 
-// let transcriber: User;
-// let notation: Notation;
+beforeEach(async () => {
+  transcriber = await UserModel.create(EntityBuilder.buildRandUser());
+  notation = await NotationModel.create(EntityBuilder.buildRandNotation({ transcriberId: transcriber.id }));
+});
 
-// beforeEach(async () => {
-//   userRepo = container.get<UserRepo>(TYPES.UserRepo);
-//   notationRepo = container.get<NotationRepo>(TYPES.NotationRepo);
+it('permits valid notations', async () => {
+  const notation = NotationModel.build(EntityBuilder.buildRandNotation());
+  await expect(notation.validate()).resolves.not.toThrow();
+});
 
-//   transcriber = await userRepo.create(EntityBuilder.buildRandUser());
-//   notation = await notationRepo.create(EntityBuilder.buildRandNotation({ transcriberId: transcriber.id }));
-// });
+it.each(['Above And Beyond (The Call Of Love)', `no i don't shave my thighs`, `You Can't Come with Me`])(
+  'permits valid song names',
+  async (songName) => {
+    const notation = NotationModel.build(EntityBuilder.buildRandNotation({ songName }));
+    await expect(notation.validate()).resolves.not.toThrow();
+  }
+);
 
-// it('permits valid notations', async () => {
-//   const notation = NotationModel.build(EntityBuilder.buildRandNotation());
-//   await expect(notation.validate()).resolves.not.toThrow();
-// });
+it.each(['; ATTEMPTED SQL INJECTION', '<script>ATTEMPTED XSS</script>'])(
+  'disallows invalid song names',
+  async (songName) => {
+    const notation = NotationModel.build(EntityBuilder.buildRandNotation({ songName }));
+    await expect(notation.validate()).rejects.toThrow();
+  }
+);
 
-// it.each(['Above And Beyond (The Call Of Love)', `no i don't shave my thighs`, `You Can't Come with Me`])(
-//   'permits valid song names',
-//   async (songName) => {
-//     const notation = NotationModel.build(EntityBuilder.buildRandNotation({ songName }));
-//     await expect(notation.validate()).resolves.not.toThrow();
-//   }
-// );
+it.each(['@jaredplaysguitar', 'tekashi69'])('permits valid artist names', async (artistName) => {
+  const notation = NotationModel.build(EntityBuilder.buildRandNotation({ artistName }));
+  await expect(notation.validate()).resolves.not.toThrow();
+});
 
-// it.each(['; ATTEMPTED SQL INJECTION', '<script>ATTEMPTED XSS</script>'])(
-//   'disallows invalid song names',
-//   async (songName) => {
-//     const notation = NotationModel.build(EntityBuilder.buildRandNotation({ songName }));
-//     await expect(notation.validate()).rejects.toThrow();
-//   }
-// );
+it.each(['; ATTEMPTED SQL INJECTION', '<script>ATTEMPTED XSS</script>'])(
+  'disallows invalid artist names',
+  async (artistName) => {
+    const notation = NotationModel.build(EntityBuilder.buildRandNotation({ artistName }));
+    await expect(notation.validate()).rejects.toThrow();
+  }
+);
 
-// it.each(['@jaredplaysguitar', 'tekashi69'])('permits valid artist names', async (artistName) => {
-//   const notation = NotationModel.build(EntityBuilder.buildRandNotation({ artistName }));
-//   await expect(notation.validate()).resolves.not.toThrow();
-// });
-
-// it.each(['; ATTEMPTED SQL INJECTION', '<script>ATTEMPTED XSS</script>'])(
-//   'disallows invalid artist names',
-//   async (artistName) => {
-//     const notation = NotationModel.build(EntityBuilder.buildRandNotation({ artistName }));
-//     await expect(notation.validate()).rejects.toThrow();
-//   }
-// );
-
-// it('fetches the transcriber association', async () => {
-//   const notationDao = await NotationModel.findByPk(notation.id, { include: 'transcriber' });
-//   expect(notationDao).not.toBeNull();
-//   expect(notationDao!.transcriber).not.toBeNull();
-//   expect(notationDao!.transcriber!.id).toBe(transcriber.id);
-// });
+it('fetches the transcriber association', async () => {
+  const notationEntity = await NotationModel.findByPk(notation.id, { include: 'transcriber' });
+  expect(notationEntity).not.toBeNull();
+  expect(notationEntity!.transcriber).not.toBeNull();
+  expect(notationEntity!.transcriber!.id).toBe(transcriber.id);
+});
