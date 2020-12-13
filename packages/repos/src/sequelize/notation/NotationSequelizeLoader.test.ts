03add6bfcc20fdbee3296a9627bcc7062b20746b
Jared Johnson <jared@jaredjohnson.dev>
2020-12-12 20:33:03 -0500

    Fix NotationSequelizeLoader test.

--- packages/repos/src/sequelize/notation/NotationSequelizeLoader.test.ts
+++ packages/repos/src/sequelize/notation/NotationSequelizeLoader.test.ts
@@ -1,70 +1,85 @@
-// import { EntityBuilder, randStr } from '@stringsync/common';
-// import { TYPES, useTestContainer } from '@stringsync/di';
-// import { Notation, User } from '@stringsync/domain';
-// import { isPlainObject, sortBy } from 'lodash';
-// import { UserSequelizeRepo } from '../user';
-// import { NotationSequelizeLoader, NotationSequelizeRepo } from './';
+import { randStr } from '@stringsync/common';
+import { Container, useTestContainer } from '@stringsync/di';
+import { EntityBuilder, Notation, User } from '@stringsync/domain';
+import { isPlainObject, sortBy } from 'lodash';
+import { REPOS } from '../../REPOS';
+import { REPOS_TYPES } from '../../REPOS_TYPES';
+import { UserSequelizeLoader, UserSequelizeRepo } from '../user';
+import { NotationSequelizeLoader, NotationSequelizeRepo } from './';
 
-// const container = useTestContainer();
+const TYPES = { ...REPOS_TYPES };
 
-// let notationLoader: NotationSequelizeLoader;
+const ref = useTestContainer(REPOS);
 
-// let transcriber1: User;
-// let transcriber2: User;
-// let notation1: Notation;
-// let notation2: Notation;
-// let notation3: Notation;
+let container: Container;
 
-// beforeEach(async () => {
-//   notationLoader = container.get<NotationSequelizeLoader>(TYPES.NotationSequelizeLoader);
+let notationLoader: NotationSequelizeLoader;
 
-//   const userRepo = container.get<UserSequelizeRepo>(TYPES.UserSequelizeRepo);
-//   const notationRepo = container.get<NotationSequelizeRepo>(TYPES.NotationSequelizeRepo);
+let transcriber1: User;
+let transcriber2: User;
+let notation1: Notation;
+let notation2: Notation;
+let notation3: Notation;
 
-//   [transcriber1, transcriber2] = await userRepo.bulkCreate([
-//     EntityBuilder.buildRandUser(),
-//     EntityBuilder.buildRandUser(),
-//   ]);
-//   [notation1, notation2, notation3] = await notationRepo.bulkCreate([
-//     EntityBuilder.buildRandNotation({ transcriberId: transcriber1.id }),
-//     EntityBuilder.buildRandNotation({ transcriberId: transcriber1.id }),
-//     EntityBuilder.buildRandNotation({ transcriberId: transcriber2.id }),
-//   ]);
-// });
+beforeEach(async () => {
+  container = ref.container;
 
-// describe('findById', () => {
-//   it('finds a notation by id', async () => {
-//     const notation = await notationLoader.findById(notation1.id);
-//     expect(notation).not.toBeNull();
-//     expect(notation!.id).toBe(notation1.id);
-//   });
+  container.rebind<NotationSequelizeRepo>(TYPES.NotationRepo).to(NotationSequelizeRepo);
+  container.rebind<NotationSequelizeLoader>(TYPES.NotationLoader).to(NotationSequelizeLoader);
+  container.rebind<UserSequelizeRepo>(TYPES.UserRepo).to(UserSequelizeRepo);
+  container.rebind<UserSequelizeLoader>(TYPES.UserLoader).to(UserSequelizeLoader);
+});
 
-//   it('returns null for missing notations', async () => {
-//     const notation = await notationLoader.findById(randStr(10));
-//     expect(notation).toBeNull();
-//   });
+beforeEach(async () => {
+  notationLoader = container.get<NotationSequelizeLoader>(TYPES.NotationLoader);
 
-//   it('returns a plain object', async () => {
-//     const notation = await notationLoader.findById(notation1.id);
-//     expect(isPlainObject(notation)).toBe(true);
-//   });
-// });
+  const userRepo = container.get<UserSequelizeRepo>(TYPES.UserRepo);
+  const notationRepo = container.get<NotationSequelizeRepo>(TYPES.NotationRepo);
 
-// describe('findByTranscriberId', () => {
-//   it('finds a notation by transcriberId', async () => {
-//     const notations = await notationLoader.findAllByTranscriberId(transcriber1.id);
-//     expect(notations).toHaveLength(2);
-//     expect(sortBy(notations, 'id')).toStrictEqual(sortBy([notation1, notation2], 'id'));
-//   });
+  [transcriber1, transcriber2] = await userRepo.bulkCreate([
+    EntityBuilder.buildRandUser(),
+    EntityBuilder.buildRandUser(),
+  ]);
+  [notation1, notation2, notation3] = await notationRepo.bulkCreate([
+    EntityBuilder.buildRandNotation({ transcriberId: transcriber1.id }),
+    EntityBuilder.buildRandNotation({ transcriberId: transcriber1.id }),
+    EntityBuilder.buildRandNotation({ transcriberId: transcriber2.id }),
+  ]);
+});
 
-//   it('returns an empty array for a missing transcriber', async () => {
-//     const notations = await notationLoader.findAllByTranscriberId(randStr(10));
-//     expect(notations).toStrictEqual([]);
-//   });
+describe('findById', () => {
+  it('finds a notation by id', async () => {
+    const notation = await notationLoader.findById(notation1.id);
+    expect(notation).not.toBeNull();
+    expect(notation!.id).toBe(notation1.id);
+  });
 
-//   it('returns plain objects', async () => {
-//     const notations = await notationLoader.findAllByTranscriberId(transcriber1.id);
-//     expect(notations).toHaveLength(2);
-//     expect(notations.every(isPlainObject)).toBe(true);
-//   });
-// });
+  it('returns null for missing notations', async () => {
+    const notation = await notationLoader.findById(randStr(10));
+    expect(notation).toBeNull();
+  });
+
+  it('returns a plain object', async () => {
+    const notation = await notationLoader.findById(notation1.id);
+    expect(isPlainObject(notation)).toBe(true);
+  });
+});
+
+describe('findByTranscriberId', () => {
+  it('finds a notation by transcriberId', async () => {
+    const notations = await notationLoader.findAllByTranscriberId(transcriber1.id);
+    expect(notations).toHaveLength(2);
+    expect(sortBy(notations, 'id')).toStrictEqual(sortBy([notation1, notation2], 'id'));
+  });
+
+  it('returns an empty array for a missing transcriber', async () => {
+    const notations = await notationLoader.findAllByTranscriberId(randStr(10));
+    expect(notations).toStrictEqual([]);
+  });
+
+  it('returns plain objects', async () => {
+    const notations = await notationLoader.findAllByTranscriberId(transcriber1.id);
+    expect(notations).toHaveLength(2);
+    expect(notations.every(isPlainObject)).toBe(true);
+  });
+});
