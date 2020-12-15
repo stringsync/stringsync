3f4d272a4ba182ba865de17e0c23d75db9b5340c
Jared Johnson <jared@jaredjohnson.dev>
2020-12-14 21:17:12 -0500

    Add cache from option to build command.

--- bin/scripts/src/commands/build.ts
+++ bin/scripts/src/commands/build.ts
@@ -11,28 +11,40 @@ export default class Build extends Command {
     dev: flags.boolean({ char: 'd', default: false }),
     test: flags.boolean({ char: 't', default: false }),
     prod: flags.boolean({ char: 'p', default: false }),
+    cacheFrom: flags.string({ default: '' }),
   };
 
   async run() {
     const { flags } = this.parse(Build);
 
-    this.build('Dockerfile.base', 'stringsync-base', flags.tag);
+    this.build('Dockerfile.base', 'stringsync-base', flags.tag, flags.cacheFrom);
 
     if (flags.test) {
-      this.build('Dockerfile.test', 'stringsync-test', flags.tag);
+      this.build('Dockerfile.test', 'stringsync-test', flags.tag, flags.cacheFrom);
     }
     if (flags.dev) {
-      this.build('Dockerfile.dev', 'stringsync-dev', flags.tag);
+      this.build('Dockerfile.dev', 'stringsync-dev', flags.tag, flags.cacheFrom);
     }
     if (flags.prod) {
-      this.build('Dockerfile.prod', 'stringsync', flags.tag);
+      this.build('Dockerfile.prod', 'stringsync', flags.tag, flags.cacheFrom);
     }
   }
 
-  private build(dockerfile: string, image: string, tag: string) {
-    execSync(`docker build . -f ./docker/${dockerfile} -t ${image}:${tag}`, {
-      cwd: ROOT_PATH,
-      stdio: 'inherit',
-    });
+  private build(dockerfile: string, image: string, tag: string, cacheFrom: string) {
+    execSync(
+      [
+        'docker',
+        'build .',
+        `-f ./docker/${dockerfile}`,
+        `-t ${image}:${tag}`,
+        cacheFrom ? `--cache-from ${cacheFrom}` : '',
+      ]
+        .filter((part) => part)
+        .join(' '),
+      {
+        cwd: ROOT_PATH,
+        stdio: 'inherit',
+      }
+    );
   }
 }
