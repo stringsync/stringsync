5c49647081926750f4de16feb134de403b74b08a
Jared Johnson <jared@jaredjohnson.dev>
2020-12-15 21:04:45 -0500

    Add describe blocks to tests in the domain package.

--- packages/domain/src/testing/EntityBuilder.test.ts
+++ packages/domain/src/testing/EntityBuilder.test.ts
@@ -1,50 +1,52 @@
 import { randStr } from '@stringsync/common';
 import { EntityBuilder } from './EntityBuilder';
 
-describe('buildRandUser', () => {
-  it('runs without crashing', () => {
-    expect(EntityBuilder.buildRandUser).not.toThrow();
+describe('EntityBuilder', () => {
+  describe('buildRandUser', () => {
+    it('runs without crashing', () => {
+      expect(EntityBuilder.buildRandUser).not.toThrow();
+    });
+
+    it('can accept attrs', () => {
+      const username = randStr(10);
+      const user = EntityBuilder.buildRandUser({ username });
+      expect(user.username).toBe(username);
+    });
   });
 
-  it('can accept attrs', () => {
-    const username = randStr(10);
-    const user = EntityBuilder.buildRandUser({ username });
-    expect(user.username).toBe(username);
-  });
-});
+  describe('buildRandNotation', () => {
+    it('runs without crashing', () => {
+      expect(EntityBuilder.buildRandNotation).not.toThrow();
+    });
 
-describe('buildRandNotation', () => {
-  it('runs without crashing', () => {
-    expect(EntityBuilder.buildRandNotation).not.toThrow();
-  });
-
-  it('can accept attrs', () => {
-    const songName = randStr(10);
-    const notation = EntityBuilder.buildRandNotation({ songName });
-    expect(notation.songName).toBe(songName);
-  });
-});
-
-describe('buildRandTag', () => {
-  it('runs without crashing', () => {
-    expect(EntityBuilder.buildRandTag).not.toThrow();
+    it('can accept attrs', () => {
+      const songName = randStr(10);
+      const notation = EntityBuilder.buildRandNotation({ songName });
+      expect(notation.songName).toBe(songName);
+    });
   });
 
-  it('can accept attrs', () => {
-    const name = randStr(10);
-    const tag = EntityBuilder.buildRandTag({ name });
-    expect(tag.name).toBe(name);
-  });
-});
+  describe('buildRandTag', () => {
+    it('runs without crashing', () => {
+      expect(EntityBuilder.buildRandTag).not.toThrow();
+    });
 
-describe('buildRandTagging', () => {
-  it('runs without crashing', () => {
-    expect(EntityBuilder.buildRandTagging).not.toThrow();
+    it('can accept attrs', () => {
+      const name = randStr(10);
+      const tag = EntityBuilder.buildRandTag({ name });
+      expect(tag.name).toBe(name);
+    });
   });
 
-  it('can accept attrs', () => {
-    const notationId = randStr(10);
-    const tagging = EntityBuilder.buildRandTagging({ notationId });
-    expect(tagging.notationId).toBe(notationId);
+  describe('buildRandTagging', () => {
+    it('runs without crashing', () => {
+      expect(EntityBuilder.buildRandTagging).not.toThrow();
+    });
+
+    it('can accept attrs', () => {
+      const notationId = randStr(10);
+      const tagging = EntityBuilder.buildRandTagging({ notationId });
+      expect(tagging.notationId).toBe(notationId);
+    });
   });
 });
