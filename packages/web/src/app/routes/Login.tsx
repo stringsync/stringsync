ff1969c49abbb9f85d9f0d85a292947be4316926
Jared Johnson <jared@jaredjohnson.dev>
2021-01-18 09:52:49 -0500

    Simplify Login component.

--- packages/web/src/app/routes/Login.tsx
+++ packages/web/src/app/routes/Login.tsx
@@ -1,5 +1,5 @@
 import { Button, Form, Input, message } from 'antd';
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
@@ -11,33 +11,28 @@ const Center = styled.div`
   text-align: center;
 `;
 
+type FormValues = {
+  usernameOrEmail: string;
+  password: string;
+};
+
 export const Login: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
   const errors = useSelector<RootState, string[]>((state) => state.auth.errors);
   const isAuthPending = useSelector<RootState, boolean>((state) => state.auth.isPending);
 
-  const [form] = Form.useForm();
-
-  const [usernameOrEmail, setUsernameOrEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [form] = Form.useForm<FormValues>();
 
   useEffectOnce(() => {
     dispatch(clearAuthErrors());
   });
-
-  const onUsernameOrEmailChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setUsernameOrEmail(event.currentTarget.value);
-  };
-
-  const onPasswordChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setPassword(event.currentTarget.value);
-  };
 
   const onErrorsClose: React.MouseEventHandler<HTMLButtonElement> = (event) => {
     dispatch(clearAuthErrors());
   };
 
   const onFinish = async () => {
+    const { usernameOrEmail, password } = form.getFieldsValue();
     const action = await dispatch(login({ input: { usernameOrEmail, password } }));
     if (action.payload && 'user' in action.payload) {
       message.success(`logged in as ${action.payload.user.username}`);
@@ -53,11 +48,11 @@ export const Login: React.FC = () => {
         main={
           <>
             <Form form={form} onFinish={onFinish}>
-              <Form.Item name="username-or-email" rules={[{ required: true, message: 'username or email required' }]}>
-                <Input placeholder="username or email" value={usernameOrEmail} onChange={onUsernameOrEmailChange} />
+              <Form.Item name="usernameOrEmail" rules={[{ required: true, message: 'username or email required' }]}>
+                <Input placeholder="username or email" />
               </Form.Item>
               <Form.Item name="password" rules={[{ required: true, message: 'password is required' }]}>
-                <Input.Password placeholder="password" value={password} onChange={onPasswordChange} />
+                <Input.Password placeholder="password" />
               </Form.Item>
               <Form.Item>
                 <Button block type="primary" htmlType="submit" disabled={isAuthPending}>
