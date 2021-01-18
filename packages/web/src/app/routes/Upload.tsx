a0caa27e32e3810f97eef806cd749f55cf670cfc
Jared Johnson <jared@jaredjohnson.dev>
2021-01-18 10:18:41 -0500

    Simplify Upload component.

--- packages/web/src/app/routes/Upload.tsx
+++ packages/web/src/app/routes/Upload.tsx
@@ -31,21 +31,28 @@ const HideableFormItem = styled(Form.Item)<{ hidden: boolean }>`
 
 const enhance = compose(withLayout(Layout.DEFAULT));
 
-interface Props {}
+type Props = {};
+
+type Upload = File | Blob | undefined;
+
+type FormValues = {
+  video: Upload;
+  thumbnail: Upload;
+  songName: string;
+  artistName: string;
+};
 
 const Upload: React.FC<Props> = enhance(() => {
   const history = useHistory();
   const dispatch = useDispatch();
   const tags = useSelector<RootState, Tag[]>((state) => state.tag.tags);
-  const [form] = Form.useForm();
   const [stepNdx, setStepNdx] = useState(0);
   const [isNavigateAwayVisible, setNavigateAwayVisibility] = useState(false);
   const [pathname, setPathname] = useState('');
   const [selectedTagNames, setSelectedTagNames] = useState(new Array<string>());
-  const [video, setVideo] = useState<File | Blob | undefined>(undefined);
-  const [thumbnail, setThumbnail] = useState<File | Blob | undefined>(undefined);
-  const [songName, setSongName] = useState('');
-  const [artistName, setArtistName] = useState('');
+
+  const [form] = Form.useForm<FormValues>();
+  const { video, thumbnail, songName, artistName } = form.getFieldsValue();
 
   const shouldBlockNavigation = useRef(true);
   shouldBlockNavigation.current = selectedTagNames.length > 0 || !!video || !!thumbnail || !!songName || !!artistName;
@@ -59,31 +66,7 @@ const Upload: React.FC<Props> = enhance(() => {
     setStepNdx(stepNdx);
   };
 
-  const beforeVideoUpload = (file: RcFile) => {
-    setVideo(file);
-    return false;
-  };
-
-  const onVideoRemove = () => {
-    setVideo(undefined);
-  };
-
-  const beforeThumbnailUpload = (file: RcFile) => {
-    setThumbnail(file);
-    return false;
-  };
-
-  const onThumbnailRemove = () => {
-    setThumbnail(undefined);
-  };
-
-  const onSongNameChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setSongName(event.currentTarget.value);
-  };
-
-  const onArtistNameChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setArtistName(event.currentTarget.value);
-  };
+  const dontUpload = (file: RcFile) => false;
 
   const onConfirmNavigationOk = () => {
     shouldBlockNavigation.current = false;
@@ -152,51 +135,46 @@ const Upload: React.FC<Props> = enhance(() => {
               hidden={stepNdx !== 0}
               rules={[{ required: true, message: 'video required' }]}
             >
-              <Dragger multiple={false} listType="picture" onRemove={onVideoRemove} beforeUpload={beforeVideoUpload}>
+              <Dragger multiple={false} listType="picture" beforeUpload={dontUpload}>
                 <p className="ant-upload-drag-icon">
                   <VideoCameraOutlined />
                 </p>
                 <p className="ant-upload-text">drag and drop a video file to upload</p>
-                <p className="ant-upload-hint">your video will be private until you publish them</p>
-              </Dragger>
-            </HideableFormItem>
-
-            <HideableFormItem
-              hasFeedback
-              name="video"
-              hidden={stepNdx !== 1}
-              rules={[{ required: true, message: 'thumbnail required' }]}
-            >
-              <Dragger
-                multiple={false}
-                listType="picture"
-                onRemove={onThumbnailRemove}
-                beforeUpload={beforeThumbnailUpload}
-              >
-                <p className="ant-upload-drag-icon">
-                  <PictureOutlined />
-                </p>
-                <p className="ant-upload-text">drag and drop a thumbnail to upload</p>
-                <p className="ant-upload-hint">your video will be private until you publish them</p>
+                <p className="ant-upload-hint">your video will be private until you publish</p>
               </Dragger>
             </HideableFormItem>
 
             <HideableFormItem
               hasFeedback
               name="thumbnail"
-              hidden={stepNdx !== 2}
-              rules={[{ required: true, message: 'song name required' }]}
+              hidden={stepNdx !== 1}
+              rules={[{ required: true, message: 'thumbnail required' }]}
             >
-              <Input placeholder="song name" value={songName} onChange={onSongNameChange} />
+              <Dragger multiple={false} listType="picture" beforeUpload={dontUpload}>
+                <p className="ant-upload-drag-icon">
+                  <PictureOutlined />
+                </p>
+                <p className="ant-upload-text">drag and drop a thumbnail to upload</p>
+                <p className="ant-upload-hint">your thumbnail will be private until you publish</p>
+              </Dragger>
             </HideableFormItem>
 
             <HideableFormItem
               hasFeedback
-              name="artist-name"
+              name="songName"
+              hidden={stepNdx !== 2}
+              rules={[{ required: true, message: 'song name required' }]}
+            >
+              <Input placeholder="song name" />
+            </HideableFormItem>
+
+            <HideableFormItem
+              hasFeedback
+              name="artistName"
               hidden={stepNdx !== 2}
               rules={[{ required: true, message: 'artist name required' }]}
             >
-              <Input placeholder="artist name" value={artistName} onChange={onArtistNameChange} />
+              <Input placeholder="artist name" />
             </HideableFormItem>
 
             <HideableFormItem hidden={stepNdx !== 2}>
