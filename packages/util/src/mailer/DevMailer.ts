7e4a8b699a3528ce6d8b6639b1607a3f001d9bf5
Jared Johnson <jared@jaredjohnson.dev>
2021-01-20 21:50:46 -0500

    Wire in SES to DevMailer.

--- packages/util/src/mailer/DevMailer.ts
+++ packages/util/src/mailer/DevMailer.ts
@@ -1,5 +1,6 @@
 import { InternalError } from '@stringsync/common';
 import { inject, injectable } from '@stringsync/di';
+import { SES } from 'aws-sdk';
 import { createTransport, Transporter } from 'nodemailer';
 import { Logger } from '../logger';
 import { UtilConfig } from '../UTIL_CONFIG';
@@ -10,10 +11,14 @@ const TYPES = { ...UTIL_TYPES };
 
 @injectable()
 export class DevMailer implements Mailer {
+  ses: SES;
   transporter: Transporter;
 
   constructor(@inject(TYPES.Logger) public logger: Logger, @inject(TYPES.UtilConfig) public config: UtilConfig) {
-    this.transporter = createTransport({});
+    this.ses = new SES();
+    this.transporter = createTransport({
+      SES: this.ses,
+    });
   }
 
   async send(mail: Mail): Promise<void> {
