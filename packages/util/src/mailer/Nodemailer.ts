58359a4965ba55ee0aadb99031804a76f5ab395e
Jared Johnson <jared@jaredjohnson.dev>
2021-01-22 19:51:16 -0500

    Wire in SES into nodemailer.

--- packages/util/src/mailer/Nodemailer.ts
+++ packages/util/src/mailer/Nodemailer.ts
@@ -1,13 +1,21 @@
 import { injectable } from '@stringsync/di';
+import { SES } from 'aws-sdk';
 import { createTransport, Transporter } from 'nodemailer';
+import { UTIL_TYPES } from '../UTIL_TYPES';
 import { Mail, Mailer } from './types';
+
+const TYPES = { ...UTIL_TYPES };
 
 @injectable()
 export class Nodemailer implements Mailer {
+  ses: SES;
   transporter: Transporter;
 
   constructor() {
-    this.transporter = createTransport({});
+    this.ses = new SES();
+    this.transporter = createTransport({
+      SES: this.ses,
+    });
   }
 
   async send(mail: Mail): Promise<void> {
