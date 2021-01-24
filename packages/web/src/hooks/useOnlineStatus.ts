e2321b6218639b0a9eaa642d3ea81fa9f4ffa888
Jared Johnson <jared@jaredjohnson.dev>
2021-01-24 09:48:22 -0500

    Add useOnlineStatus hook.

---
+++ packages/web/src/hooks/useOnlineStatus.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+// from https://github.com/rehooks/online-status
+
+const getOnlineStatus = () => {
+  return typeof navigator !== 'undefined' && typeof navigator.onLine === 'boolean' ? navigator.onLine : true;
+};
+
+export const useOnlineStatus = () => {
+  const [onlineStatus, setOnlineStatus] = useState(getOnlineStatus());
+
+  const goOnline = () => setOnlineStatus(true);
+
+  const goOffline = () => setOnlineStatus(false);
+
+  useEffect(() => {
+    window.addEventListener('online', goOnline);
+    window.addEventListener('offline', goOffline);
+
+    return () => {
+      window.removeEventListener('online', goOnline);
+      window.removeEventListener('offline', goOffline);
+    };
+  }, []);
+
+  return onlineStatus;
+};
