aa0515b58bd5e963c8f0d4a557632eca168d5b5e
Jared Johnson <jared@jaredjohnson.dev>
2021-02-17 10:13:53 -0500

    Test getExpressServer return value.

--- modules/api/src/graphql/getExpressServer.test.ts
+++ modules/api/src/graphql/getExpressServer.test.ts
@@ -1,9 +1,19 @@
+import request from 'supertest';
 import { getServerConfig } from '../config';
 import { getExpressServer } from './getExpressServer';
 
 describe('getExpressServer', () => {
+  const config = getServerConfig();
+
   it('runs without crashing', () => {
-    const config = getServerConfig();
     expect(() => getExpressServer(config)).not.toThrow();
+  });
+
+  describe('GET /health', () => {
+    it('responds with 200', (done) => {
+      const server = getExpressServer(config);
+      const req = request(server);
+      req.get('/health').expect(200, done);
+    });
   });
 });
