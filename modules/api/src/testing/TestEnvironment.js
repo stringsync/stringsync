c4eda8f02afd62f47e9c3974447f67bdf5825c81
Jared Johnson <jared@jaredjohnson.dev>
2021-02-18 22:30:49 -0500

    Remove debugging code from globalSetup.

--- modules/api/src/testing/TestEnvironment.js
+++ modules/api/src/testing/TestEnvironment.js
@@ -1,51 +1,17 @@
 const NodeEnvironment = require('jest-environment-node');
-const { createNamespace } = require('cls-hooked');
-const { Sequelize } = require('sequelize');
 const { getWorkerDbName } = require('./workers');
-
-const namespace = createNamespace('transaction');
-Sequelize.useCLS(namespace);
 
 class TestEnvironment extends NodeEnvironment {
   async setup() {
     await super.setup();
 
-    const logLevel = (process.env.LOG_LEVEL || '').toUpperCase();
     const workerId = process.env.JEST_WORKER_ID || '0';
-
-    this.sequelize = new Sequelize({
-      dialect: 'postgres',
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      database: getWorkerDbName(workerId),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      logging: logLevel === 'DEBUG' ? console.log : false,
-    });
+    const workerDbName = getWorkerDbName(workerId);
 
     const ctx = this.getVmContext();
     if (ctx) {
-      ctx.process.env.DB_NAME = getWorkerDbName(workerId);
+      ctx.process.env.DB_NAME = workerDbName;
     }
-  }
-
-  async teardown() {
-    await this.sequelize.close();
-    await super.teardown();
-  }
-
-  async handleTestEvent(event, state) {
-    switch (event.name) {
-      case 'test_done':
-        await this.onTestDone();
-        break;
-      default:
-        break;
-    }
-  }
-
-  async onTestDone() {
-    // await this.sequelize.truncate({ logging: console.log });
   }
 }
 
--- modules/api/src/testing/globalSetup.js
+++ modules/api/src/testing/globalSetup.js
@@ -9,13 +9,12 @@ module.exports = async () => {
     database: process.env.DB_NAME,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    // logging: false,
+    logging: false,
   });
 
   // create worker dbs
   const dbName = sequelize.getDatabaseName();
   const numWorkers = parseInt(process.env.JEST_WORKERS || '1');
-  console.log('numWorkers', process.env.JEST_WORKERS);
   const workerDbNames = getWorkerDbNames(numWorkers);
   // only 1 user allowed to access main db at a time
   for (const workerDbName of workerDbNames) {
