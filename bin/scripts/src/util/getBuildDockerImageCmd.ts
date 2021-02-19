388e1463e1f299beacbf613a41cfd680dc6da176
Jared Johnson <jared@jaredjohnson.dev>
2021-02-19 08:23:56 -0500

    Remove bin dir.

--- bin/scripts/src/util/getBuildDockerImageCmd.ts
+++
@@ -1,3 +0,0 @@
-export const getBuildDockerImageCmd = (imageTagName: string, dockerfilePath: string, dockerContextPath: string) => {
-  return ['docker', 'build', '-t', imageTagName, '-f', dockerfilePath, dockerContextPath].join(' ');
-};
--- bin/scripts/src/util/types.ts
+++
@@ -1,1 +0,0 @@
-export type Project = 'main' | 'e2e' | 'api' | 'web';
