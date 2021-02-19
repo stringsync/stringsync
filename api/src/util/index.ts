3e75779b93a304315ebbb5bb5137f78c4b739acd
Jared Johnson <jared@jaredjohnson.dev>
2021-02-19 17:13:01 -0500

    Add repos types.

--- api/src/util/index.ts
+++ api/src/util/index.ts
@@ -1,5 +1,6 @@
 export * from './ctor';
 export * from './logger';
+export * from './pager';
 export * from './randInt';
 export * from './randStr';
 export * from './types';
---
+++ api/src/util/pager/types.ts
@@ -0,0 +1,39 @@
+export interface ConnectionArgs {
+  before?: string | null;
+  after?: string | null;
+  first?: number | null;
+  last?: number | null;
+}
+
+export interface NotationConnectionArgs extends ConnectionArgs {
+  query?: string | null;
+  tagIds?: string[] | null;
+}
+
+export interface UserConnectionArgs extends ConnectionArgs {}
+
+export enum PagingType {
+  FORWARD,
+  BACKWARD,
+}
+
+export interface PageInfo {
+  startCursor: string | null;
+  endCursor: string | null;
+  hasPreviousPage: boolean | null;
+  hasNextPage: boolean | null;
+}
+
+export interface Connection<T> {
+  edges: Array<Edge<T>>;
+  pageInfo: PageInfo;
+}
+
+export interface Edge<T> {
+  node: T;
+  cursor: string;
+}
+
+export type PagingMeta =
+  | { pagingType: PagingType.FORWARD; after: string | null; first: number | null }
+  | { pagingType: PagingType.BACKWARD; before: string | null; last: number | null };
