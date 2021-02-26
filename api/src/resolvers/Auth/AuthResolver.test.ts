9ed16cdac610573464e963ab0e3a8bdfda092f97
Jared Johnson <jared@jaredjohnson.dev>
2021-02-25 21:53:40 -0500

    Test confirmEmail sets confirmedAt for the logged in user.

--- api/src/resolvers/Auth/AuthResolver.test.ts
+++ api/src/resolvers/Auth/AuthResolver.test.ts
@@ -2,21 +2,40 @@ import { User, UserRole } from '../../domain';
 import { container } from '../../inversify.config';
 import { TYPES } from '../../inversify.constants';
 import { SessionUser } from '../../server';
-import { AuthService } from '../../services';
-import { EntityFactory, gql, LoginInput, Mutation, Query, resolve } from '../../testing';
+import { AuthService, UserService } from '../../services';
+import { ConfirmEmailInput, EntityFactory, gql, LoginInput, Mutation, Query, resolve } from '../../testing';
 import { randStr } from '../../util';
-import { generateSchema } from '../generateSchema';
+
+enum LoginStatus {
+  LOGGED_OUT = 'LOGGED_OUT',
+  LOGGED_IN = 'LOGGED_IN',
+}
 
 describe('AuthResolver', () => {
-  const schema = generateSchema();
-  let entityFactory: EntityFactory;
-
-  beforeEach(() => {
-    entityFactory = container.get<EntityFactory>(TYPES.EntityFactory);
-  });
+  const getSessionUser = (loginStatus: LoginStatus, user: User): SessionUser | undefined => {
+    switch (loginStatus) {
+      case LoginStatus.LOGGED_OUT:
+        return undefined;
+      case LoginStatus.LOGGED_IN:
+        return {
+          id: user.id,
+          isLoggedIn: true,
+          role: user.role,
+        };
+      default:
+        throw new Error(`unhandled login status: ${loginStatus}`);
+    }
+  };
 
   describe('whoami', () => {
-    const whoami = async (sessionUser?: SessionUser) => {
+    let user: User;
+
+    beforeEach(async () => {
+      const entityFactory = container.get<EntityFactory>(TYPES.EntityFactory);
+      user = await entityFactory.createRandUser();
+    });
+
+    const whoami = async (loginStatus: LoginStatus) => {
       return resolve<Query, 'whoami'>(
         gql`
           query {
@@ -26,27 +45,20 @@ describe('AuthResolver', () => {
           }
         `,
         {},
-        { sessionUser }
+        { sessionUser: getSessionUser(loginStatus, user) }
       );
     };
 
     it('returns null when logged out', async () => {
-      const { res } = await whoami();
+      const { res } = await whoami(LoginStatus.LOGGED_OUT);
 
       expect(res.data.whoami).toBeNull();
     });
 
-    it('returns the session user', async () => {
-      const user = await entityFactory.createRandUser();
+    it('returns the session user when logged in', async () => {
+      const { res, ctx } = await whoami(LoginStatus.LOGGED_IN);
 
-      const sessionUser: SessionUser = {
-        id: user.id,
-        isLoggedIn: true,
-        role: user.role,
-      };
-
-      const { res } = await whoami(sessionUser);
-
+      const sessionUser = ctx.getSessionUser();
       expect(res.data).not.toBeNull();
       expect(res.data.whoami).not.toBeNull();
       expect(res.data!.whoami!.id).toBe(sessionUser.id);
@@ -66,7 +78,7 @@ describe('AuthResolver', () => {
       user = await authService.signup(username, email, password);
     });
 
-    const login = async (input: LoginInput, sessionUser?: SessionUser) => {
+    const login = async (input: LoginInput, loginStatus: LoginStatus) => {
       return resolve<Mutation, 'login'>(
         gql`
           mutation login($input: LoginInput!) {
@@ -76,12 +88,12 @@ describe('AuthResolver', () => {
           }
         `,
         { input },
-        { sessionUser }
+        { sessionUser: getSessionUser(loginStatus, user) }
       );
     };
 
     it('logs the user in using username and password', async () => {
-      const { res, ctx } = await login({ usernameOrEmail: user.username, password });
+      const { res, ctx } = await login({ usernameOrEmail: user.username, password }, LoginStatus.LOGGED_OUT);
 
       expect(res.errors).toBeUndefined();
       expect(res.data.login).not.toBeNull();
@@ -94,7 +106,7 @@ describe('AuthResolver', () => {
     });
 
     it('logs the user in using email and password', async () => {
-      const { res, ctx } = await login({ usernameOrEmail: user.email, password });
+      const { res, ctx } = await login({ usernameOrEmail: user.email, password }, LoginStatus.LOGGED_OUT);
 
       expect(res.errors).toBeUndefined();
       expect(res.data.login).not.toBeNull();
@@ -108,7 +120,10 @@ describe('AuthResolver', () => {
 
     it('does not log the user in when wrong password', async () => {
       const wrongPassword = randStr(password.length + 1);
-      const { res, ctx } = await login({ usernameOrEmail: user.username, password: wrongPassword });
+      const { res, ctx } = await login(
+        { usernameOrEmail: user.username, password: wrongPassword },
+        LoginStatus.LOGGED_OUT
+      );
 
       expect(res.errors).toBeDefined();
 
@@ -119,10 +134,7 @@ describe('AuthResolver', () => {
     });
 
     it('returns errors when already logged in', async () => {
-      const { res, ctx } = await login(
-        { usernameOrEmail: user.username, password },
-        { id: user.id, isLoggedIn: true, role: user.role }
-      );
+      const { res, ctx } = await login({ usernameOrEmail: user.username, password }, LoginStatus.LOGGED_IN);
 
       expect(res.errors).toBeDefined();
 
@@ -146,20 +158,20 @@ describe('AuthResolver', () => {
       user = await authService.signup(username, email, password);
     });
 
-    const logout = (sessionUser?: SessionUser) => {
-      return resolve(
+    const logout = (loginStatus: LoginStatus) => {
+      return resolve<Mutation, 'logout'>(
         gql`
           mutation {
             logout
           }
         `,
         {},
-        { sessionUser }
+        { sessionUser: getSessionUser(loginStatus, user) }
       );
     };
 
     it('logs a user out', async () => {
-      const { res, ctx } = await logout({ id: user.id, isLoggedIn: true, role: user.role });
+      const { res, ctx } = await logout(LoginStatus.LOGGED_IN);
 
       expect(res.errors).toBeUndefined();
 
@@ -170,7 +182,7 @@ describe('AuthResolver', () => {
     });
 
     it('returns errors when already logged out', async () => {
-      const { res, ctx } = await logout();
+      const { res, ctx } = await logout(LoginStatus.LOGGED_OUT);
 
       expect(res.errors).toBeDefined();
 
@@ -178,6 +190,49 @@ describe('AuthResolver', () => {
       expect(sessionUser.isLoggedIn).toBeFalse();
       expect(sessionUser.id).toBeEmpty();
       expect(sessionUser.role).toBe(UserRole.STUDENT);
+    });
+  });
+
+  describe('confirmEmail', () => {
+    let user: User;
+    let password: string;
+
+    beforeEach(async () => {
+      const username = randStr(10);
+      const email = `${username}@domain.tld`;
+      password = randStr(10);
+
+      const authService = container.get<AuthService>(TYPES.AuthService);
+      user = await authService.signup(username, email, password);
+
+      expect(user.confirmationToken).not.toBeNull();
+    });
+
+    const confirmEmail = (input: ConfirmEmailInput, loginStatus: LoginStatus) => {
+      return resolve<Mutation, 'confirmEmail', { input: ConfirmEmailInput }>(
+        gql`
+          mutation confirmEmail($input: ConfirmEmailInput!) {
+            confirmEmail(input: $input) {
+              confirmedAt
+            }
+          }
+        `,
+        { input },
+        { sessionUser: getSessionUser(loginStatus, user) }
+      );
+    };
+
+    it('sets confirmed at for the logged in user', async () => {
+      const { res } = await confirmEmail({ confirmationToken: user.confirmationToken! }, LoginStatus.LOGGED_IN);
+
+      expect(res.errors).toBeUndefined();
+      expect(res.data.confirmEmail).not.toBeNull();
+
+      const userService = container.get<UserService>(TYPES.UserService);
+      const reloadedUser = await userService.find(user.id);
+      expect(reloadedUser).not.toBeNull();
+      expect(reloadedUser!.confirmedAt).not.toBeNull();
+      expect(res.data.confirmEmail!.confirmedAt).toBe(reloadedUser!.confirmedAt!.toISOString());
     });
   });
 });
