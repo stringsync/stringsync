82f9e890aed1f1d9262eb25ed8e2a08095038d20
Jared Johnson <jared@jaredjohnson.dev>
2021-07-06 16:21:43 -0400

    Add CI env var to test command in buildspec

--- aws/lib/constructs/ci.ts
+++ aws/lib/constructs/ci.ts
@@ -102,7 +102,7 @@ export class CI extends cdk.Construct {
               'docker tag stringsync:latest $APP_REPO_URI:latest',
               'DOCKERFILE=Dockerfile.worker DOCKER_TAG=stringsyncworker:latest ./bin/ss builddocker',
               'docker tag stringsyncworker:latest $WORKER_REPO_URI:latest',
-              './bin/ss testall',
+              'CI=true ./bin/ss testall',
             ],
             'on-failure': 'ABORT',
           },
