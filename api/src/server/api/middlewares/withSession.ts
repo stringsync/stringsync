6423ad6bae2b82399f2f7c9f206f38a46794600b
Jared Johnson <jared@jaredjohnson.dev>
2021-07-24 11:38:45 -0400

    Set lax SameSite cookie policy

--- api/src/server/api/middlewares/withSession.ts
+++ api/src/server/api/middlewares/withSession.ts
@@ -10,13 +10,10 @@ const MAX_AGE_MS = 14 * 24 * 60 * 60 * 1000; // 14 days
 export const withSession = (redis: RedisClient, config: Config): Handler => {
   const RedisStore = connectRedis(session);
   const store = new RedisStore({ client: redis });
-  const isProduction = config.NODE_ENV === 'production';
-  const secure = isProduction;
-  const sameSite = isProduction ? 'none' : undefined;
 
   return session({
     secret: config.SESSION_SECRET,
-    cookie: { httpOnly: true, maxAge: MAX_AGE_MS, secure, sameSite },
+    cookie: { httpOnly: true, maxAge: MAX_AGE_MS, secure: true, sameSite: 'lax' },
     genid: () => uuid.v4(),
     proxy: undefined,
     resave: false,
