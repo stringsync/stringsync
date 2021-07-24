94a7f3ed3668b659225275bdbef15fdbd93c8775
Jared Johnson <jared@jaredjohnson.dev>
2021-07-24 12:01:09 -0400

    Remove secure cookies

--- api/src/server/api/middlewares/withSession.ts
+++ api/src/server/api/middlewares/withSession.ts
@@ -13,7 +13,7 @@ export const withSession = (redis: RedisClient, config: Config): Handler => {
 
   return session({
     secret: config.SESSION_SECRET,
-    cookie: { httpOnly: true, maxAge: MAX_AGE_MS, secure: true, sameSite: 'lax' },
+    cookie: { httpOnly: true, maxAge: MAX_AGE_MS, secure: false, sameSite: 'lax' },
     genid: () => uuid.v4(),
     proxy: undefined,
     resave: false,
