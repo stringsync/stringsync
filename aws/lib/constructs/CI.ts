aae7d830819d3a8c9e8c2da952286ae0a1ad2e9a
Jared Johnson <jared@jaredjohnson.dev>
2021-07-25 08:01:41 -0400

    Install puppeteer dependencies in install phase of codebuild

--- aws/lib/constructs/CI.ts
+++ aws/lib/constructs/CI.ts
@@ -63,6 +63,7 @@ export class CI extends cdk.Construct {
       cache: codebuild.Cache.local(codebuild.LocalCacheMode.DOCKER_LAYER, codebuild.LocalCacheMode.CUSTOM),
       environment: {
         buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+        computeType: codebuild.ComputeType.LARGE,
         privileged: true,
         environmentVariables: {
           AWS_ACCOUNT_ID: {
@@ -110,7 +111,13 @@ export class CI extends cdk.Construct {
         },
         phases: {
           install: {
-            commands: ['yarn install', 'yarn --cwd web'],
+            commands: [
+              'yarn install',
+              'yarn --cwd web',
+              // Install puppeteer dependencies
+              'apt-get update',
+              'apt-get install -yq gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget',
+            ],
           },
           pre_build: {
             commands: [
