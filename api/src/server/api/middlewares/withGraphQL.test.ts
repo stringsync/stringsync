d65eb7ab2dd32e7a4b59f05ea23df986ba8a2d10
Jared Johnson <jared@jaredjohnson.dev>
2021-08-02 01:14:45 -0400

    Fix withGraphQL test

--- api/src/server/api/middlewares/withGraphQL.test.ts
+++ api/src/server/api/middlewares/withGraphQL.test.ts
@@ -1,9 +1,13 @@
+import { container } from '../../../inversify.config';
+import { TYPES } from '../../../inversify.constants';
 import { generateSchema } from '../../../resolvers';
+import { Logger } from '../../../util';
 import { withGraphQL } from './withGraphQL';
 
 describe('withGraphQL', () => {
   it('runs without crashing', () => {
+    const logger = container.get<Logger>(TYPES.Logger);
     const schema = generateSchema();
-    expect(() => withGraphQL(schema)).not.toThrow();
+    expect(() => withGraphQL(logger, schema)).not.toThrow();
   });
 });
