004ff18c501cdf30402a749bfc5ac5ea93d2818e
Jared Johnson <jared@jaredjohnson.dev>
2021-08-23 21:56:07 -0400

    Add hook for notation player settings

---
+++ web/src/components/routes/NotationPlayer/useNotationSettings.ts
@@ -0,0 +1,32 @@
+import { useLocalStorage } from '../../../hooks/useLocalStorage';
+import { NotationPlayerSettings } from './types';
+
+const NOTATION_SETTINGS_KEY = 'stringsyncNotationSettings';
+
+const DEFAULT_SETTINGS: NotationPlayerSettings = Object.freeze({
+  isFretboardVisible: false,
+});
+
+const isNotationSettings = (value: any): value is NotationPlayerSettings => {
+  if (typeof value !== 'object') {
+    return false;
+  }
+
+  for (const [k, v] of Object.entries(DEFAULT_SETTINGS)) {
+    if (typeof value[k] !== typeof v) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+export const useNotationPlayerSettings = () => {
+  const [notationPlayerSettings, setNotationPlayerSettings] = useLocalStorage<NotationPlayerSettings>(
+    NOTATION_SETTINGS_KEY,
+    DEFAULT_SETTINGS,
+    isNotationSettings
+  );
+
+  return [notationPlayerSettings, setNotationPlayerSettings];
+};
--- web/src/hooks/useLocalStorage.ts
+++ web/src/hooks/useLocalStorage.ts
@@ -1,25 +1,36 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { JsonValue } from 'type-fest';
 
+type TypeChecker<T> = (value: any) => value is T;
+
 // Adapted from https://usehooks.com/useLocalStorage/
-export const useLocalStorage = <T extends JsonValue>(key: string, initialValue: T) => {
+export const useLocalStorage = <T extends JsonValue>(
+  key: string,
+  initialValue: T,
+  isType: TypeChecker<T>
+): [T, (value: T) => void] => {
   const [storedValue, setStoredValue] = useState(() => {
     try {
       const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
+      const parsedValue = item ? JSON.parse(item) : initialValue;
+      return isType(parsedValue) ? parsedValue : initialValue;
     } catch (error) {
       console.log(error);
       return initialValue;
     }
   });
 
-  const setValue = (value: T) => {
-    try {
-      setStoredValue(value);
-      window.localStorage.setItem(key, JSON.stringify(value));
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const setValue = useCallback(
+    (value: T) => {
+      try {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [key]
+  );
+
   return [storedValue, setValue];
 };
