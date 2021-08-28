3385f3acb5e06a91da68a3c4a0067324ac23c44f
Jared Johnson <jared@jaredjohnson.dev>
2021-08-28 08:26:41 -0400

    Improve type naming in EventBus

--- web/src/lib/EventBus/EventBus.ts
+++ web/src/lib/EventBus/EventBus.ts
@@ -1,24 +1,24 @@
-type EventSpec = Record<string, any>;
+type PayloadByEventName = Record<string, any>;
 
 type EventCallback<P> = (payload: P) => void;
 
 type Subscriber<P> = { id: symbol; callback: EventCallback<P> };
 
-type Subscribers<S extends EventSpec, E extends EventNames<S>> = Array<Subscriber<Payload<S, E>>>;
+type Subscribers<T extends PayloadByEventName, E extends EventNames<T>> = Array<Subscriber<Payload<T, E>>>;
 
-type SubscribersByEventName<S extends EventSpec> = Partial<{ [E in EventNames<S>]: Subscribers<S, E> }>;
+type SubscribersByEventName<T extends PayloadByEventName> = Partial<{ [E in EventNames<T>]: Subscribers<T, E> }>;
 
-type EventNames<S extends EventSpec> = keyof S;
+type EventNames<T extends PayloadByEventName> = keyof T;
 
-type Payload<S extends EventSpec, E extends EventNames<S>> = S[E];
+type Payload<T extends PayloadByEventName, E extends EventNames<T>> = T[E];
 
-type EventNameById<S extends EventSpec> = { [key: symbol]: EventNames<S> };
+type EventNameById<T extends PayloadByEventName> = { [key: symbol]: EventNames<T> };
 
-export class EventBus<S extends EventSpec = {}> {
-  private subscribersByEvent: SubscribersByEventName<S> = {};
-  private eventNameById: EventNameById<S> = {};
+export class EventBus<T extends PayloadByEventName = {}> {
+  private subscribersByEvent: SubscribersByEventName<T> = {};
+  private eventNameById: EventNameById<T> = {};
 
-  dispatch<E extends EventNames<S>>(eventName: E, payload: Payload<S, E>) {
+  dispatch<E extends EventNames<T>>(eventName: E, payload: Payload<T, E>) {
     if (!this.hasSubscribers(eventName)) {
       return;
     }
@@ -28,7 +28,7 @@ export class EventBus<S extends EventSpec = {}> {
     }
   }
 
-  subscribe<E extends EventNames<S>>(eventName: E, callback: EventCallback<Payload<S, E>>): symbol {
+  subscribe<E extends EventNames<T>>(eventName: E, callback: EventCallback<Payload<T, E>>): symbol {
     const id = Symbol();
     this.addSubscriber(eventName, { id, callback });
     return id;
@@ -43,16 +43,16 @@ export class EventBus<S extends EventSpec = {}> {
     this.removeSubscriber(eventName, id);
   }
 
-  private hasSubscribers(eventName: EventNames<S>): boolean {
+  private hasSubscribers(eventName: EventNames<T>): boolean {
     return eventName in this.subscribersByEvent;
   }
 
-  private addSubscriber<E extends EventNames<S>>(eventName: E, subscriber: Subscriber<Payload<S, E>>) {
+  private addSubscriber<E extends EventNames<T>>(eventName: E, subscriber: Subscriber<Payload<T, E>>) {
     this.fetchSubscribers(eventName).push(subscriber);
     this.eventNameById[subscriber.id] = eventName;
   }
 
-  private removeSubscriber<E extends EventNames<S>>(eventName: E, id: symbol) {
+  private removeSubscriber<E extends EventNames<T>>(eventName: E, id: symbol) {
     const subscribers = this.fetchSubscribers(eventName);
     const nextSubscribers = subscribers.filter((subscriber) => subscriber.id !== id);
 
@@ -63,7 +63,7 @@ export class EventBus<S extends EventSpec = {}> {
     }
   }
 
-  private fetchSubscribers<E extends EventNames<S>>(eventName: E): Subscribers<S, E> {
+  private fetchSubscribers<E extends EventNames<T>>(eventName: E): Subscribers<T, E> {
     if (!this.hasSubscribers(eventName)) {
       this.subscribersByEvent[eventName] = [];
     }
