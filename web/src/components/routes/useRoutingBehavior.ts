dece7843c7f8a351af300561b330a536451e65ab
Jared Johnson <jared@jaredjohnson.dev>
2021-09-14 10:43:36 -0400

    Improve readability using Duration in useRoutingBehavior

--- web/src/components/routes/useRoutingBehavior.ts
+++ web/src/components/routes/useRoutingBehavior.ts
@@ -1,13 +1,16 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
+import { Duration } from '../../util/Duration';
 import { NumberRange } from '../../util/NumberRange';
 import { useRoutingLocalCache } from './useRoutingLocalCache';
 
 // Redirect the user if they last visited between 1 minute and 14 days.
 // This gives people a 1 minute grace period to try to visit the landing using a link
 // again.
-const REDIRECT_LANDING_TO_LIBRARY_TIME_MS_RANGE = NumberRange.from(60 * 1000).to(14 * 24 * 60 * 60 * 1000);
+const ONE_MINUTE = Duration.min(1);
+const TWO_WEEKS = Duration.day(14);
+const REDIRECT_LANDING_TO_LIBRARY_TIME_MS_RANGE = NumberRange.from(ONE_MINUTE.ms).to(TWO_WEEKS.ms);
 
 const getMsSinceEpoch = () => new Date().getTime();
 
