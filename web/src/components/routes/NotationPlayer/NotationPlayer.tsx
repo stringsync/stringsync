f496d90302c985c5d11a271d1baf7a77559dfe61
Jared Johnson <jared@jaredjohnson.dev>
2021-09-24 05:51:26 -0400

    Remove scale and styling from NotationPlayer

--- web/src/components/routes/NotationPlayer/NotationPlayer.tsx
+++ web/src/components/routes/NotationPlayer/NotationPlayer.tsx
@@ -226,13 +226,12 @@ const NotationPlayer: React.FC<Props> = enhance(() => {
 
             {settings.isFretboardVisible && (
               <Fretboard opts={fretboardOpts} tuning={tuning}>
-                <Fretboard.Scale root="D" type="major" style={{ stroke: 'lime' }} />
                 {measurePositions.map(({ string, fret }) => (
                   <Fretboard.Position
                     key={`measure-${string}-${fret}`}
                     string={string}
                     fret={fret}
-                    style={{ fill: '#aaa' }}
+                    style={{ fill: '#bbb' }}
                   />
                 ))}
                 {pressedPositions.map(({ string, fret }) => (
