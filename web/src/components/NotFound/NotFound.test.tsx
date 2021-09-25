dc8fe6eb7183d383023f5f167d82f064ee2ed705
Jared Johnson <jared@jaredjohnson.dev>
2021-09-24 21:43:47 -0400

    Test NotFound renders without crashing

--- web/src/components/NotFound/NotFound.test.tsx
+++ web/src/components/NotFound/NotFound.test.tsx
@@ -1,9 +1,14 @@
 import { render } from '@testing-library/react';
+import { Test } from '../../testing';
 import { NotFound } from './NotFound';
 
 describe('NotFound', () => {
   it('renders without crashing', () => {
-    const { container } = render(<NotFound />);
+    const { container } = render(
+      <Test>
+        <NotFound />
+      </Test>
+    );
     expect(container).toBeInTheDocument();
   });
 });
