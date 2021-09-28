76906a60b9ce86321b27759a4bf419133e287fcc
Jared Johnson <jared@jaredjohnson.dev>
2021-09-28 07:36:53 -0400

    Remove the graphql module

--- web/src/graphql/index.ts
+++ web/src/graphql/index.ts
@@ -1,7 +1,6 @@
-export * from './graphql';
-export * from './graphqlTypes';
-export * from './toUserRole';
-export * from './$gql'
-export * from './t';
+export * from './$gql';
 export * from './constants';
+export * from './graphqlTypes';
+export * from './t';
+export * from './toUserRole';
 export * as queries from './queries';
