4dc7d78091c2855285222b9ad8667d7c75fdb222
Jared Johnson <jared@jaredjohnson.dev>
2021-11-28 19:03:04 -0500

    Add sonar guitar and sonar sleep svgs

--- web/src/components/Sonar/index.ts
+++ web/src/components/Sonar/index.ts
@@ -1,1 +1,3 @@
+export { ReactComponent as SonarGuitar } from './SonarGuitar.svg';
 export { ReactComponent as SonarSearch } from './SonarSearch.svg';
+export { ReactComponent as SonarSleep } from './SonarSleep.svg';
