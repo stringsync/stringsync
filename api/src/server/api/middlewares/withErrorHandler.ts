fffc3c4d45ba128dd4b379c820a81c878636984c
Jared Johnson <jared@jaredjohnson.dev>
2021-12-07 11:16:15 -0500

    Add uuid to error handler to make it easier to find errors

--- api/src/server/api/middlewares/withErrorHandler.ts
+++ api/src/server/api/middlewares/withErrorHandler.ts
@@ -1,4 +1,5 @@
 import { ErrorRequestHandler } from 'express';
+import * as uuid from 'uuid';
 import { HttpStatus, UNKNOWN_ERROR_MSG } from '../../../errors';
 import { TYPES } from '../../../inversify.constants';
 import { Logger } from '../../../util';
@@ -8,9 +9,11 @@ export const withErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
   }
+  const id = uuid.v4();
   const ctx = Ctx.get(req);
   const container = ctx.getContainer();
   const logger = container.get<Logger>(TYPES.Logger);
-  logger.error(err.stack);
-  res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(UNKNOWN_ERROR_MSG);
+
+  logger.error(`${id}: ${err.stack}`);
+  res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(`${UNKNOWN_ERROR_MSG}: ${id}`);
 };
