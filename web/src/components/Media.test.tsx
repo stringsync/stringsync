1b58a812e92dd6f0e97946877e48401def333d0a
Jared Johnson <jared@jaredjohnson.dev>
2022-06-12 07:27:33 -0400

    Test Media and MusicSheet components

---
+++ web/src/components/Media.test.tsx
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { Test } from '../testing';
+import { Media } from './Media';
+
+describe('Media', () => {
+  it('renders without crashing', () => {
+    const onPlayerChange = jest.fn();
+
+    const { container } = render(
+      <Test>
+        <Media src={null} skeleton={false} video={false} onPlayerChange={onPlayerChange} />
+      </Test>
+    );
+    expect(container).toBeInTheDocument();
+  });
+});
---
+++ web/src/components/MusicSheet.test.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { DisplayMode } from '../lib/musicxml';
+import { Test } from '../testing';
+import { MusicSheet } from './MusicSheet';
+
+describe('MusicSheet', () => {
+  it('renders without crashing', () => {
+    const { container } = render(
+      <Test>
+        <MusicSheet displayMode={DisplayMode.NotesAndTabs} notation={null} skeleton={false} />
+      </Test>
+    );
+    expect(container).toBeInTheDocument();
+  });
+});
