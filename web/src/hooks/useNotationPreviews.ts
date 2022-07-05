c9741913aab3856a39aee8ac03720b19aa8b22b6
Jared Johnson <jared@jaredjohnson.dev>
2022-07-05 11:21:56 -0400

    Move notation previews type to hook

--- web/src/hooks/useNotationPreviews.ts
+++ web/src/hooks/useNotationPreviews.ts
@@ -1,25 +1,32 @@
 import { useCallback, useEffect, useState } from 'react';
 import * as graphql from '../lib/graphql';
-import * as library from '../lib/library';
+import { Notation, Tag, User } from '../lib/graphql';
 import * as pager from '../lib/pager';
 import * as queries from '../lib/queries';
 import { GqlReqStatus, useGql2 } from './useGql2';
 
+type Transcriber = Pick<User, 'id' | 'username' | 'role' | 'avatarUrl'>;
+
+type NotationPreview = Omit<Notation, 'createdAt' | 'updatedAt'> & {
+  transcriber: Transcriber;
+  tags: Tag[];
+  createdAt: string;
+  updatedAt: string;
+};
+
 type LoadPage = () => void;
 
-const toNotationPreviews = (
-  connection: graphql.DataOf<typeof queries.NOTATION_PREVIEWS>
-): library.NotationPreview[] => {
+const toNotationPreviews = (connection: graphql.DataOf<typeof queries.NOTATION_PREVIEWS>): NotationPreview[] => {
   return (connection?.edges || []).map((edge) => {
     const transcriber = { ...edge.node.transcriber };
-    return { ...edge.node, transcriber } as library.NotationPreview;
+    return { ...edge.node, transcriber } as NotationPreview;
   });
 };
 
 export const useNotationPreviews = (
   pageSize: number
-): [notations: library.NotationPreview[], pageInfo: pager.PageInfo, loadPage: LoadPage] => {
-  const [notations, setNotations] = useState(new Array<library.NotationPreview>());
+): [notations: NotationPreview[], pageInfo: pager.PageInfo, loadPage: LoadPage] => {
+  const [notations, setNotations] = useState(new Array<NotationPreview>());
   const [pageInfo, setPageInfo] = useState(pager.getInitialPageInfo);
 
   const [exec, res, cancel] = useGql2(queries.NOTATION_PREVIEWS);
