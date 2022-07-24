f6ddbb1dfdd46a4f6c9dd7c64f96a0290175e8e9
Jared Johnson <jared@jaredjohnson.dev>
2022-07-24 13:53:09 -0400

    Add go to player link to NExport

--- web/src/components/NExport.tsx
+++ web/src/components/NExport.tsx
@@ -1,11 +1,41 @@
+import { Button, Row } from 'antd';
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import { Layout, withLayout } from '../hocs/withLayout';
 import { compose } from '../util/compose';
+import { Box } from './Box';
+
+const Outer = styled.div`
+  margin-top: 48px;
+`;
+
+const Inner = styled.div`
+  margin: 0 auto;
+  max-width: 720px;
+`;
+
+type FormValues = {};
 
 const enhance = compose(withLayout(Layout.DEFAULT));
 
 export const NExport: React.FC = enhance(() => {
-  return <div>NExport</div>;
+  const params = useParams();
+  const notationId = params.id || '';
+
+  return (
+    <Outer data-testid="n-export">
+      <Inner>
+        <Box>
+          <Row justify="space-between">
+            <Link to={`/n/${notationId}`}>
+              <Button type="link">go to player</Button>
+            </Link>
+          </Row>
+        </Box>
+      </Inner>
+    </Outer>
+  );
 });
 
 export default NExport;
