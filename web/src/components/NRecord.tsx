da315c063d5b4feca7336a372ef8fb9a05a76f4c
Jared Johnson <jared@jaredjohnson.dev>
2022-07-25 08:26:14 -0400

    Render error message if record is opened through non-popup

--- web/src/components/NRecord.tsx
+++ web/src/components/NRecord.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { Layout, withLayout } from '../hocs/withLayout';
+import { Errors } from './Errors';
 
-export const NRecord: React.FC = () => {
-  return <div data-testid="n-record"></div>;
-};
+const POPUP_ERRORS = ['must open through exporter'];
+
+const ExportLink = styled.div`
+  text-align: center;
+  margin-top: 24px;
+`;
+
+const enhance = withLayout(Layout.NONE, { footer: false, lanes: false });
+
+export const NRecord: React.FC = enhance(() => {
+  // params
+  const params = useParams();
+  const notationId = params.id || '';
+
+  // window ctx
+  const isPopup = !!window.opener;
+
+  // render branches
+  const renderPopupError = !isPopup;
+
+  return (
+    <div data-testid="n-record">
+      {renderPopupError && (
+        <div>
+          <br />
+          <br />
+
+          <Errors errors={POPUP_ERRORS} />
+
+          <ExportLink>
+            <Link to={`/n/${notationId}/export`}>exporter</Link>
+          </ExportLink>
+        </div>
+      )}
+    </div>
+  );
+});
 
 export default NRecord;
