cd6b7e87b36469fc605d9a0d854f4d8897f099ec
Jared Johnson <jared@jaredjohnson.dev>
2023-01-22 20:49:06 -0500

    Improve flagslice tests

--- api/util/flagslice_test.go
+++ api/util/flagslice_test.go
@@ -1,8 +1,23 @@
 package util
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestFlagSlice(t *testing.T) {
+func TestFlagSlice_Set(t *testing.T) {
+	fs := &FlagSlice{}
+
+	fs.Set("foo")
+	fs.Set("bar")
+
+	want := FlagSlice{"foo", "bar"}
+	if !reflect.DeepEqual(*fs, want) {
+		t.Errorf("*fs = %v, want %v", *fs, want)
+	}
+}
+
+func TestFlagSlice_String(t *testing.T) {
 	fs := &FlagSlice{}
 
 	fs.Set("foo")
