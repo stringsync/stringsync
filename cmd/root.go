e2245260a768517afd9adc9601f3b9ab493fc6a7
Jared Johnson <jared@jaredjohnson.dev>
2023-01-28 16:42:37 -0500

    Wire in cobra command pwd

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,9 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path"
+	"path/filepath"
+	"runtime"
 
 	"github.com/spf13/cobra"
 )
@@ -12,9 +15,17 @@ var rootCmd = &cobra.Command{
 	Short: "stringsync is an application that teaches guitar",
 }
 
+// Execute runs the command.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+// RootDir calculates the root directory of the project.
+func RootDir() string {
+	_, b, _, _ := runtime.Caller(0)
+	d := path.Join(path.Dir(b))
+	return filepath.Dir(d)
 }
