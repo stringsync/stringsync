bf7b488d5f29bdda5e3d1f99166ff958ac677431
Jared Johnson <jared@jaredjohnson.dev>
2023-02-06 20:03:51 -0500

    Test ctx

---
+++ api/util/ctx_test.go
@@ -0,0 +1,20 @@
+package util
+
+import (
+	"context"
+	"testing"
+)
+
+func TestCtxKey(t *testing.T) {
+	key := CtxKey("foo")
+	value := "bar"
+
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, key, value)
+
+	want := value
+	got := ctx.Value(key)
+	if got != want {
+		t.Errorf("ctx.Value(%q) = %q, want %q", key, got, want)
+	}
+}
