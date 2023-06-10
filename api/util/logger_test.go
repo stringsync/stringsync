31626d8fd7d80d15c444a9c7bd7f83d6625cdd34
Jared Johnson <jared@jaredjohnson.dev>
2023-06-10 12:42:43 -0400

    Finish testing logger

--- api/util/logger_test.go
+++ api/util/logger_test.go
@@ -2,9 +2,12 @@ package util
 
 import (
 	"bytes"
+	"strings"
 	"testing"
 	"time"
 )
+
+var testTime = time.Date(2023, time.June, 10, 9, 25, 10, 0, time.UTC)
 
 type testWriter struct {
 	buf bytes.Buffer
@@ -25,29 +28,29 @@ func TestLogger_NewLogger(t *testing.T) {
 		{
 			name:      "text format",
 			formatter: FormatterText,
-			time:      time.Date(2023, time.June, 10, 9, 25, 10, 0, time.UTC),
-			log:       "hello world",
-			want:      "time=\"2023-06-10T09:25:10Z\" level=info msg=\"hello world\"\n",
+			time:      testTime,
+			log:       "foo",
+			want:      "time=\"2023-06-10T09:25:10Z\" level=info msg=foo\n",
 		}, {
 			name:      "json format",
 			formatter: FormatterJson,
-			time:      time.Date(2023, time.June, 10, 9, 25, 10, 0, time.UTC),
-			log:       "hello world",
-			want:      "{\"level\":\"info\",\"msg\":\"hello world\",\"time\":\"2023-06-10T09:25:10Z\"}\n",
+			time:      testTime,
+			log:       "foo",
+			want:      "{\"level\":\"info\",\"msg\":\"foo\",\"time\":\"2023-06-10T09:25:10Z\"}\n",
 		}, {
 			name:      "invalid format",
 			formatter: -1,
-			time:      time.Date(2023, time.June, 10, 9, 25, 10, 0, time.UTC),
-			log:       "hello world",
-			want:      "time=\"2023-06-10T09:25:10Z\" level=info msg=\"hello world\"\n",
+			time:      testTime,
+			log:       "foo",
+			want:      "time=\"2023-06-10T09:25:10Z\" level=info msg=foo\n",
 		},
 	} {
 		t.Run(test.name, func(t *testing.T) {
 			writer := &testWriter{}
-
 			logger := NewLogger(test.formatter)
 			logger.SetTime(test.time)
 			logger.SetOutput(writer)
+
 			logger.Infof(test.log)
 
 			if got := writer.buf.String(); got != test.want {
@@ -61,7 +64,7 @@ func TestLogger_NewChild(t *testing.T) {
 	writer := &testWriter{}
 
 	logger := NewLogger(FormatterText)
-	logger.SetTime(time.Date(2023, time.June, 10, 9, 25, 10, 0, time.UTC))
+	logger.SetTime(testTime)
 	logger.SetOutput(writer)
 
 	child1 := logger.NewChild()
@@ -79,6 +82,121 @@ func TestLogger_NewChild(t *testing.T) {
 	got2 := writer.buf.String()
 
 	if got1 == got2 {
-		t.Errorf("want got1 != got2, got got1 == got2 == %q", got1)
+		t.Errorf("want got1 != got2, got %q", got1)
+	}
+}
+
+func TestLogger_SetOutput(t *testing.T) {
+	writer := &testWriter{}
+	logger := NewLogger(FormatterText)
+
+	logger.SetOutput(writer)
+	logger.Infof("foo")
+
+	if got := writer.buf.Len(); got == 0 {
+		t.Errorf("writer.buf.Len() = %v, want > 0", got)
+	}
+}
+
+func TestLogger_SetTime(t *testing.T) {
+	writer := &testWriter{}
+	logger := NewLogger(FormatterText)
+	logger.SetOutput(writer)
+
+	logger.SetTime(testTime)
+	logger.Infof("foo")
+
+	if got, substr := writer.buf.String(), testTime.Format(time.RFC3339); !strings.Contains(got, substr) {
+		t.Errorf("strings.Contains(%q, %q) = false, want true", got, substr)
+	}
+}
+
+func TestLogger_SetGlobalField(t *testing.T) {
+	writer := &testWriter{}
+	logger := NewLogger(FormatterText)
+	logger.SetOutput(writer)
+
+	logger.SetGlobalField("foo", "bar")
+	logger.Infof("hello")
+
+	if got := writer.buf.String(); !strings.Contains(got, "foo=bar") {
+		t.Errorf("strings.Contains(%q, \"foo=bar\") = false, want true", got)
+	}
+}
+
+func TestLogger_SetGlobalField_ChildLogger(t *testing.T) {
+	writer := &testWriter{}
+	logger := NewLogger(FormatterText)
+	logger.SetOutput(writer)
+
+	logger.SetGlobalField("foo", "bar")
+	child := logger.NewChild()
+	child.Infof("hello")
+
+	if got := writer.buf.String(); !strings.Contains(got, "foo=bar") {
+		t.Errorf("strings.Contains(%q, \"foo=bar\") = false, want true", got)
+	}
+}
+
+func TestLogger_SetLocalField(t *testing.T) {
+	writer := &testWriter{}
+	logger := NewLogger(FormatterText)
+	logger.SetOutput(writer)
+
+	logger.SetLocalField("foo", "bar")
+	logger.Infof("hello")
+
+	if got := writer.buf.String(); !strings.Contains(got, "foo=bar") {
+		t.Errorf("strings.Contains(%q, \"foo=bar\") = false, want true", got)
+	}
+}
+
+func TestLogger_SetLocalField_ChildLogger(t *testing.T) {
+	writer := &testWriter{}
+	logger := NewLogger(FormatterText)
+	logger.SetOutput(writer)
+
+	logger.SetLocalField("foo", "bar")
+	child := logger.NewChild()
+	child.Infof("hello")
+
+	if got := writer.buf.String(); strings.Contains(got, "foo=bar") {
+		t.Errorf("strings.Contains(%q, \"foo=bar\") = true, want false", got)
+	}
+}
+
+func TestLogger_Infof(t *testing.T) {
+	writer := &testWriter{}
+	logger := NewLogger(FormatterText)
+	logger.SetOutput(writer)
+
+	logger.Infof("foo")
+
+	if got := writer.buf.String(); !strings.Contains(got, "level=info") {
+		t.Errorf("strings.Contains(%q, \"level=info\") = false, want true", got)
+	}
+}
+
+func TestLogger_Warnf(t *testing.T) {
+	writer := &testWriter{}
+	logger := NewLogger(FormatterText)
+	logger.SetOutput(writer)
+
+	logger.Warnf("foo")
+
+	if got := writer.buf.String(); !strings.Contains(got, "level=warn") {
+		t.Errorf("strings.Contains(%q, \"level=warn\") = false, want true", got)
+	}
+}
+
+func TestLogger_Errorf(t *testing.T) {
+	writer := &testWriter{}
+	logger := NewLogger(FormatterText)
+	logger.SetOutput(writer)
+
+	logger.Errorf("foo")
+
+	if got := writer.buf.String(); !strings.Contains(got, "level=error") {
+		t.Errorf("strings.Contains(%q, \"level=error\") = false, want true", got)
 	}
 }
