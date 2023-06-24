d4ff99ae295f4058a72cf79a2d7659f63d692a2b
Jared Johnson <jared@jaredjohnson.dev>
2023-06-24 11:32:34 -0400

    Test health service

--- services/healthservice_test.go
+++ services/healthservice_test.go
@@ -1,7 +1,9 @@
 package services
 
 import (
+	"context"
 	"stringsync/database"
+	"stringsync/util"
 	"testing"
 )
 
@@ -16,4 +18,37 @@ func TestNewHealthService(t *testing.T) {
 	}()
 
 	NewHealthService(db)
+}
+
+func TestHealthService_CheckDBHealth(t *testing.T) {
+	t.Run("returns true when DB can be pinged", func(t *testing.T) {
+		db, cleanup := database.CreateTestDBFromEnv()
+		defer cleanup()
+
+		logger := util.NewLogger(util.FormatterText)
+		ctx := context.Background()
+		ctx = util.LogCtxSlot.Put(ctx, logger)
+		healthService := NewHealthService(db)
+
+		if got, want := healthService.CheckDBHealth(ctx), true; got != want {
+			t.Errorf("healthService.CheckDBHealth(%v) = %v, want %v", ctx, got, want)
+		}
+	})
+
+	t.Run("returns false when DB cannot be pinged", func(t *testing.T) {
+		db, cleanup := database.CreateTestDBFromEnv()
+		defer cleanup()
+
+		logger := util.NewLogger(util.FormatterText)
+		ctx := context.Background()
+		ctx = util.LogCtxSlot.Put(ctx, logger)
+		healthService := NewHealthService(db)
+
+		// Close the DB connection to make pings fail.
+		db.Close()
+
+		if got, want := healthService.CheckDBHealth(ctx), false; got != want {
+			t.Errorf("healthService.CheckDBHealth(%v) = %v, want %v", ctx, got, want)
+		}
+	})
 }
